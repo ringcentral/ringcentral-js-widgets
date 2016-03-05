2c9885b33961433b293dbf23cf26ab5adb761caa
Colin Liu <colin198702@gmail.com>
2016-03-05 10:23:56 +0800

    Wrap RC SDK in sdk component and Add LoginService helper

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,22 +1,25 @@
 import AuthPanel from './components/auth-panel'
 import CallPanel from './components/call-panel'
 import DialPad from './components/dial-pad'
-import CallLog from './components/call-log'
 import AutoComplete from './components/auto-complete'
 import rcHelper from './helpers/helper'
+import sdk from './helpers/rc-sdk'
+import LoginService from './helpers/login-service'
 
 window.AuthPanel = AuthPanel;
 window.CallPanel = CallPanel;
 window.DialPad = DialPad;
-window.CallLog = CallLog;
 window.AutoComplete = AutoComplete;
 window.rcHelper = rcHelper;
+window.sdk = sdk;
+window.LoginService = LoginService;
 
 export {
     AuthPanel,
     CallPanel,
     DialPad,
-    CallLog,
     AutoComplete,
-    rcHelper
+    rcHelper,
+    sdk,
+    LoginService
 }
