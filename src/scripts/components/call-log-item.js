868042e9eec7bf6b2f6cb3b26f439af234be9e4d
Colin Liu <colin198702@gmail.com>
2016-03-05 10:35:07 +0800

    Add Call Log widget

---
+++ src/scripts/components/call-log-item.js
@@ -0,0 +1,16 @@
+import { Component, register } from '../component'
+
+var CallLogItem = register({
+    beforeUpdate: function(action) {
+        
+    },
+    afterUpdate: function(action) {
+        
+    },
+    methods: {
+        
+
+    }
+});
+
+export default CallLogItem;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,34 @@
 import AuthPanel from './components/auth-panel'
 import CallPanel from './components/call-panel'
 import DialPad from './components/dial-pad'
+import CallLog from './components/call-log'
+import CallLogItem from './components/call-log-item'
 import AutoComplete from './components/auto-complete'
 import rcHelper from './helpers/helper'
 import sdk from './helpers/rc-sdk'
 import LoginService from './helpers/login-service'
+import CallLogService from './helpers/call-log-service'
 
 window.AuthPanel = AuthPanel;
 window.CallPanel = CallPanel;
 window.DialPad = DialPad;
+window.CallLog = CallLog;
+window.CallLogItem = CallLogItem
 window.AutoComplete = AutoComplete;
 window.rcHelper = rcHelper;
 window.sdk = sdk;
 window.LoginService = LoginService;
+window.CallLogService = CallLogService;
 
 export {
     AuthPanel,
     CallPanel,
     DialPad,
+    CallLog,
+    CallLogItem,
     AutoComplete,
     rcHelper,
     sdk,
-    LoginService
+    LoginService,
+    CallLogService
 }
