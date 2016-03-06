2533b4163bf50c5fed568e9c64f6fe9555139d23
Colin Liu <colin198702@gmail.com>
2016-03-06 13:27:34 +0800

    Modify the code which generates content for CallLogItem

--- src/scripts/components/call-log.js
+++ src/scripts/components/call-log.js
@@ -6,6 +6,7 @@ var CallLog = register({
     afterUpdate:function(action, options){
         var allCallTab = this.props.dom.allCallTab;
         var missedCallTab = this.props.dom.missedCallTab;
+        var logs = this.props.dom.logs;
             
         if(action === 'enableAllCallTab'){
             if(allCallTab.classList.contains('active') === false){
@@ -37,16 +38,27 @@ var CallLog = register({
                 
                 var callLogItem = new CallLogItem({
                     template:'./template/call-log-item.html',
-                    beforeUpdate:function(action){
-                        
-                    },
                     afterUpdate: function(action){
                         if(action === 'mount'){
-                            //TODO: How to manipulate dom style
+                            if(item.result === "Missed"){
+                                this.props.dom.callResult.classList.add('call-missed');
+                            }
+                            
                             if(item.direction === 'Outbound'){
-                                this.props.dom.contact.innerHTML = item.to.name;
-                                this.props.dom.location.innerHTML = item.to.location;
+                                if(item.to.name){
+                                    this.props.dom.contact.innerHTML = item.to.name;
+                                }else{
+                                    this.props.dom.contact.innerHTML = item.to.phoneNumber;
+                                }
+                                
+                                if(item.to.location){
+                                    this.props.dom.location.innerHTML = item.to.location;
+                                }
                                 this.props.dom.time.innerHTML = item.startTime;
+                                
+                                if(item.result !== "Missed"){
+                                    this.props.dom.callResult.classList.add('call-outbound');
+                                }
                             }
                             else{
                                 if(item.from.name){
@@ -55,6 +67,10 @@ var CallLog = register({
                                     this.props.dom.contact.innerHTML = item.from.phoneNumber;
                                 }
                                 this.props.dom.time.innerHTML = item.startTime;
+                                
+                                if(item.result !== "Missed"){
+                                    this.props.dom.callResult.classList.add('call-inbound');
+                                }
                             }
                             
                             
