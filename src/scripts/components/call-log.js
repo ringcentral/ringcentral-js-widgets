32de2977a54797cf02b186f1797cdc6b90806d4d
Colin Liu <colin198702@gmail.com>
2016-03-06 11:37:25 +0800

    Modify UI update method when switching All/Missed tab

--- src/scripts/components/call-log.js
+++ src/scripts/components/call-log.js
@@ -1,33 +1,35 @@
 import { register, Component } from '../component'
 import CallLogItem from './call-log-item'
 
-function switchTab(tab1, tab2){
-    if(tab1.classList.contains('active') === false){
-        tab1.classList.add('active');
-        tab2.classList.remove('active');
-    }
-}
-
-function switchTabByClick(){
-    var callTab = this.props.dom.callTab;
-    var missedCallTab = this.props.dom.missedCallTab;
-    callTab.addEventListener('click', function(){
-        switchTab.call(this, callTab, missedCallTab);
-    });
-    
-    missedCallTab.addEventListener('click', function(){
-        switchTab.call(this, missedCallTab, callTab);
-    });
-}
-
 var CallLog = register({
     
     afterUpdate:function(action, options){
-        if(action === 'mount'){
-            switchTabByClick.call(this);
+        var allCallTab = this.props.dom.allCallTab;
+        var missedCallTab = this.props.dom.missedCallTab;
+            
+        if(action === 'enableAllCallTab'){
+            if(allCallTab.classList.contains('active') === false){
+                allCallTab.classList.add('active');
+                missedCallTab.classList.remove('active');
+            }
+        }else if(action === 'enableMissedCallTab'){
+            
+            if(missedCallTab.classList.contains('active') === false){
+                missedCallTab.classList.add('active');
+                allCallTab.classList.remove('active');
+            }
         }
     },
     methods: {
+        
+        enableAllCallTab: function(finish, event){
+             
+             return finish();          
+        },
+        enableMissedCallTab: function(finish, event){
+             
+             return finish();          
+        },
         logUpdated:function(logItems){
             
             var props = this.props;
@@ -40,16 +42,22 @@ var CallLog = register({
                     },
                     afterUpdate: function(action){
                         if(action === 'mount'){
+                            //TODO: How to manipulate dom style
                             if(item.direction === 'Outbound'){
                                 this.props.dom.contact.innerHTML = item.to.name;
                                 this.props.dom.location.innerHTML = item.to.location;
                                 this.props.dom.time.innerHTML = item.startTime;
                             }
                             else{
-                                this.props.dom.contact.innerHTML = item.from.name;
-                                //this.props.dom.location.innerHTML = item.from.location;
+                                if(item.from.name){
+                                    this.props.dom.contact.innerHTML = item.from.name;
+                                }else{
+                                    this.props.dom.contact.innerHTML = item.from.phoneNumber;
+                                }
                                 this.props.dom.time.innerHTML = item.startTime;
                             }
+                            
+                            
                         }
                     }
                     
