c85b8f3f4a68629fa7a09b5620267898223297f0
changbenny <changbenny0401@gmail.com>
2016-05-23 17:03:52 +0800

    minify js, html when compiling

--- compiler/index.js
+++ compiler/index.js
@@ -19,13 +19,14 @@ var watch = require('node-watch')
 var file = require('./file')
 var compile = require('./compile').compile
 var transform = require('./transform').transform
+var minify = require('./minify').minify
 const GLOBAL_PREFIX = '__w_widgets = {};'
 const GLOBAL_POSTFIX = ''
 
 function start() {
     console.log('compile')
+    var data = ''
     file.writeFile(GLOBAL_PREFIX, true)
-    // TODO
     file.readFiles(function(content, id) {
         var content = compile(content)
         return transform(content, {
@@ -34,8 +35,13 @@ function start() {
         }).then(output => {
             var prefix = `__w_widgets['${id}'] = `
             var postfix = ';\n'
-            file.writeFile(prefix + JSON.stringify(output) + postfix)
+            // file.writeFile()
+            console.log(id);
+            data += (prefix + JSON.stringify(output) + postfix)
         }).catch(e => console.error(e))
+    }, function() {
+        file.writeFile(minify(data))
+        console.log('finish');
     })
     file.writeFile(GLOBAL_POSTFIX, false)
     // file.removeFile('__w_temp.js')
