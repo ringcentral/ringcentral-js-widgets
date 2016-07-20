1aeeefaea673e9ff72ea84bfa70b7d5121426535
changbenny <changbenny0401@gmail.com>
2016-07-20 14:38:15 +0800

    update test for utils:connect

--- src/components/widgets/auth/container/UserAuthPanel.react.js
+++ src/components/widgets/auth/container/UserAuthPanel.react.js
@@ -1,6 +1,7 @@
 import AuthPanel from '../presentation/AuthPanel/AuthPanel.react';
 import { connect } from 'react-redux';
 import { connect as phoneConnect } from '../../../../utils/integration/';
+
 const withPhone = phoneConnect(phone => ({
   auth: phone.auth,
 }))(AuthPanel);
--- test/setup.js
+++ test/setup.js
@@ -16,5 +16,3 @@ Object.keys(document.defaultView).forEach((property) => {
 global.navigator = {
   userAgent: 'node.js'
 };
-
-documentRef = document;
