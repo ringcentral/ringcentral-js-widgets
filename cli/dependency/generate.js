1f6ccef47da13722adaba73736b40a9222305db1
changbenny <changbenny0401@gmail.com>
2016-07-28 15:47:29 +0800

    showcase will display dependency tree

--- cli/dependency/generate.js
+++ cli/dependency/generate.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const { analysis } = require('./analysis');
 const Handlebars = require('handlebars');
-
-const source = fs.readFileSync(path.resolve(__dirname + '/template.hbs'), 'utf-8');
+const data = analysis();
+const source = fs.readFileSync(path.resolve(__dirname + '/template/template.hbs'), 'utf-8');
+const children = fs.readFileSync(path.resolve(__dirname + '/template/children.hbs'), 'utf-8');
+Handlebars.registerPartial('children', children);
 const template = Handlebars.compile(source);
-const html = template(analysis());
+const html = template(data);
 console.log(html);
