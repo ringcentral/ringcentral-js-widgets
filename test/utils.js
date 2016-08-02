b5753d522620f1c41a7c039ec9cd649f8f3895a0
changbenny <changbenny0401@gmail.com>
2016-08-02 09:48:54 +0800

    fix test

--- test/utils.js
+++ test/utils.js
@@ -1,6 +1,7 @@
 /* global describe it */
 
 import React from 'react';
+import { createStore } from 'redux';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
 import { Provider, PhoneProvider, connect } from '../src/utils/integration';
@@ -12,20 +13,22 @@ describe('connect', () => {
     }
   }
   it('should have context in wrapped component', () => {
+    const store = createStore((state, action) => state);
     const Container = (props) => (<Passthrough {...props} />);
     const phone = {
       attr: 1,
     };
-    const Wrapper = connect(p => ({ attr: p.attr }))(Container);
+    const Wrapper = connect((state, props, p) => ({ attr: p.attr }))(Container);
     const tree = mount(
-    <Provider>
-      <PhoneProvider phone={phone}>
-        <Wrapper />
-      </PhoneProvider>);
-    <Provider>
-    expect(Wrapper.displayName).to.equal('PhoneConnect(Container)');
+      <Provider store={store}>
+        <PhoneProvider phone={phone}>
+          <Wrapper />
+        </PhoneProvider>
+      </Provider>
+    );
+    expect(Wrapper.displayName).to.equal('Connect(Container)');
     expect(Wrapper.contextTypes.phone).to.equal(React.PropTypes.object);
-    expect(tree.find('PhoneConnect(Container)')).to.have.length(1);
+    expect(tree.find('Connect(Container)')).to.have.length(1);
     expect(tree.find('Passthrough').props().attr).to.equal(1);
     // expect(tree.find(<Container />).props()).to.equal(phone);
   });
