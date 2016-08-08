9969131f3565abd06bb710719f59ed38a2b3e49c
Colin Liu <colin198702@gmail.com>
2016-08-08 20:58:32 +0800

    Add unit test (#43)
    Seems alright... I'll see if the are missing aspects that needs to be tested later too.

--- test/unit/enum.js
+++ test/unit/enum.js
@@ -4,39 +4,35 @@ import Enum from '../../src/lib/enum';
 
 /* global describe it */
 
-describe('Enum', () => {
+describe('enum', () => {
   it('should be a class function', () => {
     expect(Enum).to.be.a('function');
     const e = new Enum({ key: 'value' });
     expect(e).to.be.instanceOf(Enum);
   });
-  // TODO: expand the test to fully test enum classes
-  it('test', () => {
-    class MyEnum extends Enum {
-      constructor() {
-        super({
-          key1: 'value1',
-          key2: 'value2',
-          key3: 'value3',
-        });
-      }
+  class TestEnum extends Enum {
+    constructor() {
+      super({
+        key1: 'value1',
+        key2: 'value2',
+        key3: 'value3',
+      });
     }
-
-    const myEnum = new MyEnum();
-
-    expect(myEnum.key1).to.equal('value1');
-    expect(myEnum.key2).to.equal('value2');
-    expect(myEnum.key3).to.equal('value3');
+  }
+  const testEnum = new TestEnum();
+  it('should add keys as own properties to subclass', () => {
+    expect(testEnum).to.have.ownProperty('key1');
+    expect(testEnum).to.have.ownProperty('key2');
+    expect(testEnum).to.have.ownProperty('key3');
+  });
+  it('should add values as values of properties to subclass', () => {
+    expect(testEnum.key1).to.equal('value1');
+    expect(testEnum.key2).to.equal('value2');
+    expect(testEnum.key3).to.equal('value3');
+  });
+  it('should not allow values of properties modified from outside', () => {
     expect(() => {
-      myEnum.key2 = 'override';
+      testEnum.key2 = 'override';
     }).to.throw();
-
-    const result = [];
-    for (const key in myEnum) {
-      if (myEnum.hasOwnProperty(key)) {
-        result.push(key);
-      }
-    }
-    expect(result).to.deep.equal(['key1', 'key2', 'key3']);
   });
 });
