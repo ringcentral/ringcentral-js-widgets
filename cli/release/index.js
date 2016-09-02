3784f41859d747b0b12ff8a95992c729f67caaea
changbenny <changbenny0401@gmail.com>
2016-09-02 10:53:16 +0800

    fix release script

--- cli/release/index.js
+++ cli/release/index.js
@@ -3,19 +3,17 @@ const path = require('path');
 const dir = require('node-dir');
 
 function walk(src, root, callback) {
-  const results = {};
   dir.readFiles(src, { match: /index.js$/ },
     (err, content, filename, next) => {
       if (err) throw err;
       const dirName = filename.substring(0, filename.lastIndexOf('/'));
-      const name = dir.slice(dir.lastIndexOf('/'));
+      const name = dirName.slice(dirName.lastIndexOf('/'));
       callback(dirName, `${root}/${name}`, (error) => {
-        error && console.log(err);
+        if (error) throw error;
       });
-      // next();
+      next();
     }, (err, files) => {
       if (err) throw err;
-      fs.writeFile('test.json', JSON.stringify(results));
       // console.log('----finish----');
     }
   );
