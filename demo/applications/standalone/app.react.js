61814d4a5690960bb0accc795000ce327a6626cd
changbenny <changbenny0401@gmail.com>
2016-09-22 11:28:57 +0800

    react router

--- demo/applications/standalone/app.react.js
+++ demo/applications/standalone/app.react.js
@@ -1,32 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import Match from 'react-router/Match';
+import Redirect from 'react-router/Redirect';
+import Router from 'react-router/MemoryRouter';
+
 import WebPhone from './containers/webphone';
 import Auth from './containers/auth';
 
-import TabPanels from '../../../src/widgets/shared/TabPanels';
-import TabPanel from '../../../src/widgets/shared/TabPanel';
-
-import Header from '../../../src/widgets/shared/Header';
-
 import styles from './app.css';
 
-const App = function ({ loggedIn }) {
+const WelcomePage = ({ loggedIn }) => {
   return (
-    <div className={styles.app}>
-      <Header logo={'ringcentral.png'} />
-      {!loggedIn ?
-        <Auth /> :
-        <TabPanels displayedTab={0}>
-          <WebPhone />
-        </TabPanels>  
-      }
+    <div>
+      {loggedIn ?
+        <Redirect to={'/webphone'} /> :
+        <Redirect to={'/auth'} />}
     </div>
   );
 };
 
-App.propTypes = {
+const connectedWelcomePage = connect(
+  state => ({
+    loggedIn: state.common.auth.status === 'LOGGED_IN',
+  })
+)(WelcomePage);
+
+const App = function () {
+  return (
+    <Router>
+      <div className={styles.app}>
+        <Match exactly pattern="/" component={connectedWelcomePage} />
+        <Match pattern="/auth" render={(props) => <Auth {...props} mainPage={'/webphone'} />} />
+        <Match pattern="/webphone" component={WebPhone} />
+      </div>
+    </Router>
+  );
+};
+
+WelcomePage.propTypes = {
   loggedIn: React.PropTypes.bool,
 };
 
-export default connect(state => ({ loggedIn: state.common.auth.status === 'LOGGED_IN' }))(App);
+export default App;
