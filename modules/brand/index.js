e05c463d628f0bb6d2a66f1247ce050b6bea3c5b
RingCentral Integrations Team <integrations@ringcentral.com>
2016-10-13 08:01:11 +0000

    released at Thu Oct 13 08:01:11 UTC 2016

--- modules/brand/index.js
+++ modules/brand/index.js
@@ -43,7 +43,7 @@ var Brand = function (_RcModule) {
   function Brand(options) {
     (0, _classCallCheck3.default)(this, Brand);
 
-    var _this = (0, _possibleConstructorReturn3.default)(this, (Brand.__proto__ || (0, _getPrototypeOf2.default)(Brand)).call(this));
+    var _this = (0, _possibleConstructorReturn3.default)(this, (Brand.__proto__ || (0, _getPrototypeOf2.default)(Brand)).call(this, options));
 
     var id = options.id;
     var name = options.name;
