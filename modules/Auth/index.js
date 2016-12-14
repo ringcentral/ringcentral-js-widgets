0450a2736183c8f04bd4eab67beed2b0ffb26d1a
RingCentral Integrations Team <integrations@ringcentral.com>
2016-12-14 08:46:34 +0000

    released at Wed Dec 14 08:46:34 UTC 2016

--- modules/Auth/index.js
+++ modules/Auth/index.js
@@ -226,7 +226,7 @@ var Auth = function (_RcModule) {
           while (1) {
             switch (_context.prev = _context.next) {
               case 0:
-                if (!(_this3.pending && _this3._locale.ready)) {
+                if (!(_this3.status === _moduleStatus2.default.pending && _this3._locale.ready)) {
                   _context.next = 7;
                   break;
                 }
