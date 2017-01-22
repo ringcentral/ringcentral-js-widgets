9dd19c3790d56419b83be31e7aab924c75219a2e
RingCentral Integrations Team <integrations@ringcentral.com>
2017-01-22 15:41:25 +0800

    update release script to not expose tokens used in CI

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 #clone latest branch
-git clone "https://$RELEASE_USER:$RELEASE_TOKEN@github.com/$REPO" -b latest release
+git clone "https://$RELEASE_USER:$RELEASE_TOKEN@github.com/$REPO" -b latest release &> /dev/null
 npm run release
 
 cd release
 
 if [[ $(git status -s) != '' ]]
   then
-  git config user.email "integrations@ringcentral.com"
-  git config user.name "RingCentral Integrations Team"
-  git add --all .
-  git commit -m "released at $(date)"
-  git push origin latest -f
+  git config user.email "integrations@ringcentral.com" &> /dev/null
+  git config user.name "RingCentral Integrations Team" &> /dev/null
+  git add --all . &> /dev/null
+  git commit -m "released at $(date)" &> /dev/null
+  git push origin latest -f &> /dev/null
 fi
