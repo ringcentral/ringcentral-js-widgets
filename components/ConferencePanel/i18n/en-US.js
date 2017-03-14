99c9226b94a07b8eeccebe55cb132f561716d8d9
RingCentral Integrations Team <integrations@ringcentral.com>
2017-03-14 14:56:23 +0000

    released at Tue Mar 14 14:56:23 UTC 2017

---
+++ components/ConferencePanel/i18n/en-US.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  newConference: 'New Conference',
+  dialInNumber: 'Dial-in Number',
+  host: 'Host',
+  participants: 'Participants',
+  internationalParticipants: 'International participants',
+  internationalNumbersHeader: 'Select International Dial-in Numbers',
+  search: 'Search...',
+  inviteWithText: 'Invite with Text',
+  inviteText: 'Please join the RingCentral conference.\n\n' + 'Dial-In Numbers: {dialInNumber} \n\n' + '{internationals} \n' + 'Participant Access: {participantCode} \n\n' + 'Need an international dial-in phone number? Please visit http://www.ringcentral.com/conferencing \n\n' + 'This conference call is brought to you by RingCentral Conferencing.'
+};
+//# sourceMappingURL=en-US.js.map
---
+++ containers/ConferencePage/index.js
@@ -0,0 +1,102 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = exports.propTypes = exports.mapToProps = exports.mapToFunctions = undefined;
+
+var _reactRedux = require('react-redux');
+
+var _react = require('react');
+
+var _formatNumber = require('ringcentral-integration/lib/formatNumber');
+
+var _formatNumber2 = _interopRequireDefault(_formatNumber);
+
+var _Locale = require('ringcentral-integration/modules/Locale');
+
+var _Locale2 = _interopRequireDefault(_Locale);
+
+var _Conference = require('ringcentral-integration/modules/Conference');
+
+var _Conference2 = _interopRequireDefault(_Conference);
+
+var _RegionSettings = require('ringcentral-integration/modules/RegionSettings');
+
+var _RegionSettings2 = _interopRequireDefault(_RegionSettings);
+
+var _ComposeText = require('ringcentral-integration/modules/ComposeText');
+
+var _ComposeText2 = _interopRequireDefault(_ComposeText);
+
+var _RouterInteraction = require('../../modules/RouterInteraction');
+
+var _RouterInteraction2 = _interopRequireDefault(_RouterInteraction);
+
+var _ConferencePanel = require('../../components/ConferencePanel');
+
+var _ConferencePanel2 = _interopRequireDefault(_ConferencePanel);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function mapToProps(_, _ref) {
+  var conference = _ref.conference,
+      regionSettings = _ref.regionSettings,
+      locale = _ref.locale;
+
+  return {
+    conferenceNumbers: conference.conferenceNumbers,
+    countryCode: regionSettings.countryCode,
+    areaCode: regionSettings.areaCode,
+    currentLocale: locale.currentLocale
+  };
+}
+
+function mapToFunctions(_, _ref2) {
+  var composeText = _ref2.composeText,
+      router = _ref2.router;
+
+  return {
+    inviteWithText: function inviteWithText(text) {
+      composeText.updateMessageText(text);
+      router.history.push('/composeText');
+    },
+    formatInternational: function formatInternational(phoneNumber, callingCode) {
+      if (phoneNumber.indexOf(callingCode === 1)) {
+        return '+' + callingCode + ' ' + phoneNumber.replace('+', '').replace(callingCode, '');
+      }
+      return phoneNumber;
+    },
+    formatPin: function formatPin(number) {
+      if (!number) {
+        return '';
+      }
+      return number.replace(/(\d{3})/g, '$1-').replace(/-$/, '');
+    },
+    formatPhone: function formatPhone(phoneNumber, countryCode, areaCode) {
+      return (0, _formatNumber2.default)({
+        phoneNumber: phoneNumber,
+        countryCode: countryCode,
+        areaCode: areaCode || ''
+      });
+    }
+  };
+}
+
+var ConferencePage = (0, _reactRedux.connect)(mapToProps, mapToFunctions)(_ConferencePanel2.default);
+
+var propTypes = {
+  conference: _react.PropTypes.instanceOf(_Conference2.default),
+  regionSettings: _react.PropTypes.instanceOf(_RegionSettings2.default),
+  locale: _react.PropTypes.instanceOf(_Locale2.default),
+  composeText: _react.PropTypes.instanceOf(_ComposeText2.default),
+  router: _react.PropTypes.instanceOf(_RouterInteraction2.default)
+};
+
+ConferencePage.propTypes = propTypes;
+
+exports.mapToFunctions = mapToFunctions;
+exports.mapToProps = mapToProps;
+exports.propTypes = propTypes;
+exports.default = ConferencePage;
+//# sourceMappingURL=index.js.map
