043f0c65fa30d7c8ca618a814afd3f44d310b27c
caffffe <sophie.wu@rcoffice.ringcentral.com>
2017-03-24 10:19:49 +0800

    update-alert-message

--- src/components/MessageSenderAlert/i18n/en-US.js
+++ src/components/MessageSenderAlert/i18n/en-US.js
@@ -17,8 +17,8 @@ export default {
   [messageSenderMessages.internalError]: 'Cannot connect due to internal errors. Please try again later.',
   [messageSenderMessages.notAnExtension]: 'The extension number does not exist.',
   [messageSenderMessages.networkError]: 'Cannot connect due to network issues. Please try again later.',
-  [messageSenderMessages.senderNumberInvalids]: `You don't have valid phone number to send SMS 
-    from. Please contact your account administrator.`,
+  [messageSenderMessages.senderNumberInvalids]: `A valid Phone Number is required to send text message to
+    recipients outside of your company, Please contact your Administrator to add a direct number to your account.`,
   [messageSenderMessages.notSmsToExtension]: `Cannot send To a extension number with main phone number.
     If you want to sent to a extension Number,
     please just enter extension Number.`,
