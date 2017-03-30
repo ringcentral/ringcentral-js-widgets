5b0502547a7ce90f5862c91bc6580819a84fb9fc
caffffe <sophie.wu@rcoffice.ringcentral.com>
2017-03-30 09:02:37 +0800

    add spaces

--- src/containers/ConversationPage/index.js
+++ src/containers/ConversationPage/index.js
@@ -114,10 +114,9 @@ function mapDispatchToProps(dispatch, props) {
   if (props.contactMatcher && props.contactMatcher.ready) {
     getMatcherContactList = (phoneNumber) => {
       const matcherNames = props.contactMatcher.dataMapping[phoneNumber];
-
       if (matcherNames && matcherNames.length > 0) {
         return matcherNames.map(matcher =>
-          `${matcher.name} ${matcher.phoneNumbers[0].phoneType}`
+          `${matcher.name}|${matcher.phoneNumbers[0].phoneType}`
         );
       }
       return [];
