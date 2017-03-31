3fa0d8c6821f89cf2d040640200327198275ba7f
caffffe <sophie.wu@rcoffice.ringcentral.com>
2017-03-31 10:26:56 +0800

    delete wrong translation

--- src/components/ComposeTextPanel/i18n/en-CA.js
+++
@@ -1,9 +0,0 @@
-export default {
-  from: 'From',
-  to: 'To',
-  enterNameOrNumber: 'Enter Number or Name...',
-  typeMessage: 'Type message...',
-  send: 'Send',
-  noSMSSenderNumber: `You don't have valid phone number to send SMS from. 
-    Please contact your account administrator.`,
-};
