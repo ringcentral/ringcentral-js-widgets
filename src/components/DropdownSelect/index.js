6f1669ee231868376df17c55e14778f1e4efa413
embbnux <embbnux@embbnux.com>
2017-04-01 09:05:53 +0800

    add dropdown select

---
+++ src/components/DropdownSelect/index.js
@@ -0,0 +1,132 @@
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
+import styles from './styles.scss';
+import dynamicsFont from '../../assets/DynamicsFont/DynamicsFont.scss';
+
+class DropdownSelect extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+    this.mounted = true;
+
+    this.toggleShowDropdown = () => {
+      if (this.props.disabled) {
+        return;
+      }
+      this.setState(preState => ({
+        open: !preState.open,
+      }));
+    };
+
+    this.onChange = (e, option) => {
+      e.stopPropagation();
+      this.props.onChange(option);
+      this.toggleShowDropdown();
+    };
+
+    this._handleDocumentClick = (e) => {
+      if (!this.mounted) {
+        return;
+      }
+      if (this.wrapper && this.wrapper.contains(e.target)) {
+        return;
+      }
+      this.setState({
+        open: false,
+      });
+    };
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    window.addEventListener('click', this._handleDocumentClick, false);
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+    window.removeEventListener('click', this._handleDocumentClick, false);
+  }
+
+  render() {
+    const label = this.props.label ?
+      (
+        <label>
+          {this.props.label}
+        </label>
+      ) : null;
+    const iconClassName = classnames(
+      styles.icon,
+      this.state.open ? styles.iconUp : null,
+    );
+    const containerClassName = classnames(
+      styles.root,
+      this.props.className,
+      this.props.disabled ? styles.disabled : null,
+      this.state.open ? styles.open : null,
+    );
+    return (
+      <div
+        className={containerClassName}
+        ref={(ref) => { this.wrapper = ref; }}
+      >
+        <button className={styles.button} onClick={this.toggleShowDropdown}>
+          {label}
+          <span className={styles.selectedValue}>
+            {this.props.renderValue(this.props.value)}
+          </span>
+          <span className={iconClassName}>
+            <i className={dynamicsFont.arrow} />
+          </span>
+        </button>
+        <ul className={styles.dropdown}>
+          {
+            this.props.options.map((option, idx) => {
+              const currentValue = this.props.valueFunction(option, idx);
+              const className = classnames(
+                styles.dropdownItem,
+                this.props.value === currentValue ? styles.selected : null,
+              );
+              return (
+                <li
+                  key={currentValue}
+                  className={className}
+                  value={currentValue}
+                  onClick={e => this.onChange(e, option)}
+                >
+                  {this.props.renderFunction(option, idx)}
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+DropdownSelect.propTypes = {
+  className: PropTypes.string,
+  value: PropTypes.string,
+  label: PropTypes.string,
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
+  options: PropTypes.arrayOf(PropTypes.any).isRequired,
+  valueFunction: PropTypes.func,
+  renderFunction: PropTypes.func,
+  renderValue: PropTypes.func,
+};
+
+DropdownSelect.defaultProps = {
+  className: null,
+  value: null,
+  label: null,
+  onChange: undefined,
+  disabled: false,
+  valueFunction: option => option,
+  renderFunction: option => option,
+  renderValue: option => option,
+};
+
+export default DropdownSelect;
