e746c99a824341dbc08d5975fb88284c40c9e34a
embbnux <embbnux@embbnux.com>
2017-04-19 14:08:22 +0800

    add badge and draggable components

---
+++ src/components/Badge/index.js
@@ -0,0 +1,34 @@
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+import styles from './styles.scss';
+
+function Badge({
+  className,
+  name,
+  children,
+  onClick,
+}) {
+  return (
+    <a
+      href={`#${name}-badge`}
+      className={classnames(styles.root, className)}
+      onClick={onClick} >
+      {children}
+    </a>
+  );
+}
+
+Badge.propTypes = {
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+Badge.defaultProps = {
+  className: null,
+  name: null,
+  onClick: () => null,
+};
+
+export default Badge;
