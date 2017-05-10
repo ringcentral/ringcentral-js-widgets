c287b95e6ec08bc76481c81553025d7bddf53ccb
Alvita Huang <alvita.huang@ringcentral.com>
2017-05-10 15:28:49 +0800

    bump version

--- src/components/RolesAndPermissionsAlert/i18n/en-US.js
+++ src/components/RolesAndPermissionsAlert/i18n/en-US.js
@@ -2,5 +2,5 @@ import permissionMessages from
   'ringcentral-integration/modules/RolesAndPermissions/permissionsMessages';
 
 export default {
-  [permissionMessages.invalidTier]: 'Your edition does not support {application} CRM integration. Please contact your account representative to upgrade your {brand} edition.',
+  [permissionMessages.invalidTier]: 'Your edition does not support {application} integration. Please contact your account representative to upgrade your {brand} edition.',
 };
