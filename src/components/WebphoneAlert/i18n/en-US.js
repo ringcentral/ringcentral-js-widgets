88425e3c90cae1e950800b73e1221b4d38fab533
embbnux <embbnux@embbnux.com>
2017-05-24 15:37:41 +0800

    fix typo

--- src/components/WebphoneAlert/i18n/en-US.js
+++ src/components/WebphoneAlert/i18n/en-US.js
@@ -1,7 +1,7 @@
 import webphoneErrors from 'ringcentral-integration/modules/Webphone/webphoneErrors';
 
 export default {
-  [webphoneErrors.connectFailed]: 'Connect with web phone server faild.',
+  [webphoneErrors.connectFailed]: 'Connect with web phone server failed.',
   [webphoneErrors.browserNotSupported]: 'Calling with browser is only supported on Chrome.',
   [webphoneErrors.webphoneCountOverLimit]: 'A maximum of 5 web phones could be registered.',
   [webphoneErrors.notOutboundCallWithoutDL]: `Your extension is not allowed to
