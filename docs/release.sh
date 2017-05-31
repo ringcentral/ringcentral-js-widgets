b94b1810a2776fe825186ef67406c9e6b1a8da3d
embbnux <embbnux@embbnux.com>
2017-05-31 09:44:09 +0800

    release.sh

--- docs/release.sh
+++ docs/release.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 rm -rf gh-pages
-git clone git@github.com:embbnux/ringcentral-js-widget.git -b gh-pages gh-pages &> /dev/null
+git clone git@github.com:ringcentral/ringcentral-js-widget.git -b gh-pages gh-pages &> /dev/null
+rm -r gh-pages/*
+rm -r gh-pages/.babelrc
+rm -r gh-pages/.gitignore
+rm -r gh-pages/.jscsrc
+
 npm run build
 
 cd gh-pages
