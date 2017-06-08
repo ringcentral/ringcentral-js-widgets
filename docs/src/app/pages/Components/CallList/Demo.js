c805bf5812cbf4360c70cc8f6e057f9ab11d6905
Alvita Huang <alvita.huang@ringcentral.com>
2017-06-08 21:21:32 +0800

    add showcase Modal, Alert, CallList

--- docs/src/app/pages/Components/CallList/Demo.js
+++ docs/src/app/pages/Components/CallList/Demo.js
@@ -4,10 +4,40 @@ import CallList from 'ringcentral-widget/components/CallList';
 
 const props = {};
 props.currentLocale = 'en-US';
-props.calls = [];
+props.calls = [{
+  "id":"6fdBlmrIQGm1n.Wt.dZ4KwtYIZEJxesw",
+  "direction":"Outbound",
+  "from":{"phoneNumber":"+16507411889"},
+  "to":{"phoneNumber":"+12055439065"},
+  "telephonyStatus":"CallConnected",
+  "sipData":{"toTag":"aY8JwGA-xpRinSKQGl6BJzOLCWsmyEPm","fromTag":"10.13.22.253-5070-e2b88f40181740",
+  "remoteUri":"sip:+12055439065@ringcentral.com",
+  "localUri":"sip:+16507411889@ringcentral.com"},
+  "sessionId":"156530009020",
+  "startTime":1496914055959,
+  "fromMatches":[],
+  "toMatches":[],
+  "activityMatches":[],
+  "duration":20
+},{
+  "id":"DPKm0zIrgo8RJic",
+  "sessionId":"156529999020",
+  "startTime":1496914031914,
+  "duration":11,
+  "type":"Voice",
+  "direction":"Outbound",
+  "action":"VoIP Call",
+  "result":"Call connected",
+  "to":{"phoneNumber":"+12055439065","name":"WARRIOR      AL",
+  "location":"Warrior, AL"},
+  "from":{"phoneNumber":"+16507411889","name":"Alvita Huang"},
+  "fromMatches":[],
+  "toMatches":[],
+  "activityMatches":[]
+}];
 props.areaCode = 'test string';
 props.countryCode = 'test string';
-props.dateTimeFormatter = () => null;
+props.dateTimeFormatter = () => '12:00';
 
 /**
  * A example of `CallList`
