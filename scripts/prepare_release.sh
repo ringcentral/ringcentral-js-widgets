68c78c25295d5510bc2a81cad22edc02cd957ad2
embbnux <embbnux@embbnux.com>
2017-06-14 22:08:57 +0800

    remove comment

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 #clone latest branch
-# git clone "https://$RELEASE_USER:$RELEASE_TOKEN@github.com/$REPO" -b latest release &> /dev/null
-# npm run release
+git clone "https://$RELEASE_USER:$RELEASE_TOKEN@github.com/$REPO" -b latest release &> /dev/null
+npm run release
 
 cd docs
 rm -rf gh-pages
