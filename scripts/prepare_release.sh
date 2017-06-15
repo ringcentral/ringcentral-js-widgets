fcd29c9f4f6898565861b859fa533dc8c99e9c7b
embbnux <embbnux@embbnux.com>
2017-06-15 10:10:17 +0800

    no repeat build

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 #clone latest branch
-git clone "https://$RELEASE_USER:$RELEASE_TOKEN@github.com/$REPO" -b latest release &> /dev/null
-npm run release
+if [ ! -d release ]; then
+  git clone "https://$RELEASE_USER:$RELEASE_TOKEN@github.com/$REPO" -b latest release &> /dev/null
+  echo 'start release build'
+  npm run release
+fi
+
 
 cd docs
-rm -rf gh-pages
-yarn
-npm run build
+if [ ! -d gh-pages ]; then
+  echo 'start docs elease build'
+  yarn
+  npm run build
+fi
 cd ../
