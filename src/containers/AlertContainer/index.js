eca0acd109a5764836d71dbaa96b2fde94e77d56
Alvita Huang <alvita@users.noreply.github.com>
2017-07-11 10:25:16 +0800

    move getRenderer to Dynamics (#285)

--- src/containers/AlertContainer/index.js
+++ src/containers/AlertContainer/index.js
@@ -2,21 +2,9 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 import Alert from 'ringcentral-integration/modules/Alert';
-import Brand from 'ringcentral-integration/modules/Brand';
 import Locale from 'ringcentral-integration/modules/Locale';
-import RateLimiter from 'ringcentral-integration/modules/RateLimiter';
 
 import AlertDisplay from '../../components/AlertDisplay';
-
-import AuthAlert from '../../components/AuthAlert';
-import CallAlert from '../../components/CallAlert';
-import CallingSettingsAlert from '../../components/CallingSettingsAlert';
-import RegionSettingsAlert from '../../components/RegionSettingsAlert';
-import MessageSenderAlert from '../../components/MessageSenderAlert';
-import RateExceededAlert from '../../components/RateExceededAlert';
-import ConnectivityAlert from '../../components/ConnectivityAlert';
-import WebphoneAlert from '../../components/WebphoneAlert';
-import RolesAndPermissionsAlert from '../../components/RolesAndPermissionsAlert';
 
 const AlertContainer = connect((state, {
   locale,
@@ -25,73 +13,10 @@ const AlertContainer = connect((state, {
   currentLocale: locale.currentLocale,
   messages: alert.messages,
 }), (dispatch, {
-  rateLimiter,
-  brand,
+  getRenderer,
   alert,
 }) => ({
-  getRenderer: (message) => {
-    if (AuthAlert.handleMessage(message)) {
-      return AuthAlert;
-    }
-    if (CallAlert.handleMessage(message)) {
-      return props => (
-        <CallAlert
-          {...props}
-          regionSettingsUrl="/settings/region" />
-      );
-    }
-    if (CallingSettingsAlert.handleMessage(message)) {
-      return props => (
-        <CallingSettingsAlert
-          {...props}
-          brand={brand.fullName}
-          callingSettingsUrl="/settings/calling" />
-      );
-    }
-
-    if (RegionSettingsAlert.handleMessage(message)) {
-      return props => (
-        <RegionSettingsAlert
-          {...props}
-          regionSettingsUrl="/settings/region" />
-      );
-    }
-
-    if (MessageSenderAlert.handleMessage(message)) {
-      return props => (
-        <MessageSenderAlert
-          {...props}
-          regionSettingsUrl="/settings/region" />
-      );
-    }
-
-    if (RateExceededAlert.handleMessage(message)) {
-      return props => (
-        <RateExceededAlert
-          {...props}
-          timestamp={rateLimiter.timestamp}
-          duration={rateLimiter._throttleDuration} />
-      );
-    }
-
-    if (ConnectivityAlert.handleMessage(message)) {
-      return ConnectivityAlert;
-    }
-
-    if (WebphoneAlert.handleMessage(message)) {
-      return WebphoneAlert;
-    }
-    if (RolesAndPermissionsAlert.handleMessage(message)) {
-      return props => (
-        <RolesAndPermissionsAlert
-          {...props}
-          brand={brand.fullName}
-          application={brand.application} />
-      );
-    }
-
-    return undefined;
-  },
+  getRenderer,
   dismiss: (id) => {
     alert.dismiss(id);
   },
@@ -99,9 +24,8 @@ const AlertContainer = connect((state, {
 
 AlertContainer.propTypes = {
   alert: PropTypes.instanceOf(Alert).isRequired,
-  brand: PropTypes.instanceOf(Brand).isRequired,
+  getRenderer: PropTypes.func.isRequired,
   locale: PropTypes.instanceOf(Locale).isRequired,
-  rateLimiter: PropTypes.instanceOf(RateLimiter).isRequired,
 };
 
 export default AlertContainer;
