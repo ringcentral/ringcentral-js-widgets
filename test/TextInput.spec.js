290ddf6f649f00a9166459c4c9a5c7935e83242c
Tyler Long <tyler4long@gmail.com>
2017-07-19 13:47:35 +0800

    Make test work again

---
+++ test/TextInput.spec.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Textinput from '../src/components/TextInput';
+
+describe('<Textinput />', () => {
+  it('should render correct', () => {
+    const wrapper = shallow(<Textinput />);
+    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('input').length).toEqual(1);
+  });
+
+  it('props received', () => {
+    const onChange = jest.fn();
+    const onKeyDown = jest.fn();
+    const wrapper = shallow(<Textinput
+      className={'helloClass'}
+      invalid={false}
+      onChange={onChange}
+      placeholder={'hello placeholder'}
+      disabled={false}
+      readOnly={false}
+      pattern={'hello pattern'}
+      name={'hello name'}
+      maxLength={20}
+      value={'hello value'}
+      defaultValue={'hello default value'}
+      onKeyDown={onKeyDown}
+      />
+    );
+    const div = wrapper.find('div').first();
+
+    expect(div.props().className).toBeDefined();
+    expect(div.props().className).toEqual('helloClass');
+
+    const input = wrapper.find('input').first();
+
+    expect(input.props().onChange).toBeDefined();
+    expect(input.props().placeholder).toBeDefined();
+    expect(input.props().disabled).toBeDefined();
+    expect(input.props().readOnly).toBeDefined();
+    expect(input.props().pattern).toBeDefined();
+    expect(input.props().name).toBeDefined();
+    expect(input.props().maxLength).toBeDefined();
+    expect(input.props().defaultValue).toBeDefined();
+    expect(input.props().onKeyDown).toBeDefined();
+
+    expect(input.props().placeholder).toEqual('hello placeholder');
+    expect(input.props().disabled).toEqual(false);
+    expect(input.props().readOnly).toEqual(false);
+    expect(input.props().pattern).toEqual('hello pattern');
+    expect(input.props().name).toEqual('hello name');
+    expect(input.props().maxLength).toEqual(20);
+    expect(input.props().defaultValue).toEqual('hello default value');
+  });
+
+  it('onChange event bind success', () => {
+    const onChangeSpy = jest.fn();
+    const wrapper = mount(<Textinput onChange={onChangeSpy} />);
+    wrapper.find('input').simulate('change', { target: { value: 'hello' } });
+    expect(onChangeSpy.mock.calls.length).toEqual(1);
+  });
+
+  it('onKeyDown event bind success', () => {
+    const onKeyDown = jest.fn();
+    const wrapper = mount(<Textinput onKeyDown={onKeyDown} />);
+    wrapper.find('input').simulate('keydown', { which: 'a' });
+    expect(onKeyDown.mock.calls.length).toEqual(1);
+  });
+});
---
+++ test/setup.js
@@ -0,0 +1,8 @@
+function mockedMatchMedia() {
+  return {
+    matches: false,
+    addListener() {},
+    removeListener() {}
+  };
+}
+window.matchMedia = window.matchMedia || mockedMatchMedia;
