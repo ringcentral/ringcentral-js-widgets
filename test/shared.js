e387286f3bef8921a7928c4e9c8a3d1a8e78a933
Tyler Long <tyler4long@gmail.com>
2017-07-20 18:28:18 +0800

    Use dotenv for test

--- test/shared.js
+++ test/shared.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import Perf from 'react-addons-perf';
 import { createMockStore } from 'redux-logic-test';
 
 import Phone from '../dev-server/Phone';
 import App from '../dev-server/containers/App';
-import apiConfig from '../dev-server/api-config';
 import brandConfig from '../dev-server/brandConfig';
 import version from '../dev-server/version';
 import prefix from '../dev-server/prefix';
 
-window.Perf = Perf;
+require('dotenv').config();
+
+const apiConfig = process.env;
 
 const phone = new Phone({
   apiConfig,
