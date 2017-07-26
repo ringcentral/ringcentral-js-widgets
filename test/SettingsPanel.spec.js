442fbcc0343982b61b446c90b8452065d3a153d3
Tyler Long <tyler4long@gmail.com>
2017-07-26 23:39:35 +0800

    More tests for settings page

--- test/SettingsPanel.spec.js
+++ test/SettingsPanel.spec.js
@@ -5,6 +5,8 @@ import { mount } from 'enzyme';
 import { getPhone } from './shared';
 import SettingsPage from '../src/containers/SettingsPage';
 import LinkLine from '../src/components/LinkLine';
+import PresenceSettingSection from '../src/components/PresenceSettingSection';
+import Eula from '../src/components/Eula';
 
 let wrapper = null;
 beforeEach(() => {
@@ -33,5 +35,10 @@ describe('settings panel', () => {
     expect(linkLines.length).toBeGreaterThan(1);
     expect(linkLines.at(0).props().children).toEqual('calling');
     expect(linkLines.at(1).props().children).toEqual('region');
+
+    expect(wrapper.find(PresenceSettingSection).length).toBe(1);
+    expect(wrapper.find(Eula).length).toBe(1);
+    expect(wrapper.find('span.logout').length).toBe(1);
+    expect(wrapper.find('div.versionContainer').length).toBe(1);
   });
 });
