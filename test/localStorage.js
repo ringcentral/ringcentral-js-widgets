f1dd6369046abe9ea15a21f1f7ec77c924957655
Tyler Long <tyler4long@gmail.com>
2017-07-28 18:21:48 +0800

    Add localStorage back

---
+++ test/localStorage.js
@@ -0,0 +1,36 @@
+import storage from './storage.json';
+
+export default class LocalStorage {
+  constructor() {
+    this.store = storage;
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value;
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+
+  key(index) {
+    const keys = Object.keys(this.store);
+    return keys[index] || null;
+  }
+
+  get length() {
+    return Object.keys(this.store).length;
+  }
+
+  toString() {
+    return '[object Storage]';
+  }
+}
