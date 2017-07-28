b42db906fe408f086e2794426f4ea7a91ffbb81c
Tyler Long <tyler4long@gmail.com>
2017-07-28 16:00:05 +0800

    Fix loadLocacle issue

--- test/__mocks__/loadLocale.js
+++ test/__mocks__/loadLocale.js
@@ -1,5 +1,5 @@
 module.exports = {
   process(src, filename, config, options) {
-    return "const enUS =require('./en-US'); module.exports = async function loadLocale() { return enUS; };";
+    return "const enUS = require('./en-US').default; module.exports = async function loadLocale() { return enUS; };";
   },
 };
