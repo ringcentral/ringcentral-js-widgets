09cb9dca36b7258743e5fc47fbbf0c52d63a40d1
Embbnux Ji <embbnux@embbnux.com>
2017-08-02 15:09:41 +0800

    add save button in setting pages (#318)

--- src/components/RegionSettingsPanel/i18n/en-US.js
+++ src/components/RegionSettingsPanel/i18n/en-US.js
@@ -1,4 +1,5 @@
 export default {
+  save: 'Save',
   title: 'Region',
   NAOnlyMessage: 'Please set your area code. This will be used for local dialing.',
   MultiWithNAMessage: 'Please set the country and area code for your region. This will be used for local dialing and phone number formatting.',
