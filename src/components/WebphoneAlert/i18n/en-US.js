f8de01ea5f4d49707ae57ce0c432dad690ec891a
embbnux <embbnux@embbnux.com>
2017-08-02 15:58:03 +0800

    show spinner when connecting webphone

--- src/components/WebphoneAlert/i18n/en-US.js
+++ src/components/WebphoneAlert/i18n/en-US.js
@@ -2,6 +2,7 @@ import webphoneErrors from 'ringcentral-integration/modules/Webphone/webphoneErr
 
 export default {
   [webphoneErrors.connectFailed]: 'Connect with web phone server failed.',
+  [webphoneErrors.connected]: 'Web phone registered.',
   [webphoneErrors.browserNotSupported]: 'Calling with browser is only supported on Chrome.',
   [webphoneErrors.webphoneCountOverLimit]: 'A maximum of 5 web phones could be registered.',
   [webphoneErrors.notOutboundCallWithoutDL]: 'Your extension is not allowed to make outbound calls with browser currently, please contact your account representative for an upgrade.',
