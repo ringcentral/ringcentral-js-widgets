85256e274e7c705e0ca13fe902fd84d86c3f69cc
ele828 <ele828@gmail.com>
2017-08-03 11:16:02 +0800

    show messages when recording is disabled

--- src/components/WebphoneAlert/index.js
+++ src/components/WebphoneAlert/index.js
@@ -37,5 +37,6 @@ WebphoneAlert.handleMessage = ({ message }) => (
   (message === webphoneErrors.muteError) ||
   (message === webphoneErrors.holdError) ||
   (message === webphoneErrors.flipError) ||
-  (message === webphoneErrors.recordError)
+  (message === webphoneErrors.recordError) ||
+  (message === webphoneErrors.recordDisabled)
 );
