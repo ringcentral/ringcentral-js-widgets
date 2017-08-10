ac1310266aba611922045b1eba787da9c7e458ef
Tyler Long <tyler4long@gmail.com>
2017-08-10 10:53:12 +0800

    Test click log button

--- test/history/Calls.spec.js
+++ test/history/Calls.spec.js
@@ -2,6 +2,7 @@ import { getWrapper, timeout } from '../shared';
 import NavigationBar from '../../src/components/NavigationBar';
 import CallsPanel from '../../src/components/CallsPanel';
 import CallItem from '../../src/components/CallItem';
+import LogButton from '../../src/components/LogButton';
 
 let wrapper = null;
 let panel = null;
@@ -22,6 +23,16 @@ describe('history', () => {
     await timeout(20); // because there is a setTimeout in CallItem implementation
     for (let i = 0; i < callItems.length; i += 1) {
       expect(callItems.at(i).text().trim().length > 0).toEqual(true);
+    }
+  });
+
+  test('click log button', async () => {
+    const callItems = panel.find(CallItem);
+    await timeout(20); // because there is a setTimeout in CallItem implementation
+    if (callItems.length > 0) {
+      const callItem = callItems.at(callItems.length - 1); // last item
+      const logButton = callItem.find(LogButton).first();
+      expect(logButton.props().disableLinks).toBe(false);
     }
   });
 });
