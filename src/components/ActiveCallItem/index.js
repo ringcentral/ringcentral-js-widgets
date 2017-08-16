a73b7d8d84f46a27b2d0f86e9762326a95b13113
embbnux <embbnux@embbnux.com>
2017-08-16 10:03:27 +0800

    adjust ActiveCallItem

--- src/components/ActiveCallItem/index.js
+++ src/components/ActiveCallItem/index.js
@@ -3,9 +3,18 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import sessionStatus from 'ringcentral-integration/modules/Webphone/sessionStatus';
 import callDirections from 'ringcentral-integration/enums/callDirections';
-
+import {
+  isInbound,
+  isRinging,
+  isMissed,
+} from 'ringcentral-integration/lib/callLogHelpers';
 import dynamicsFont from '../../assets/DynamicsFont/DynamicsFont.scss';
 import DurationCounter from '../DurationCounter';
+import ContactDisplay from '../ContactDisplay';
+import Button from '../Button';
+import CircleButton from '../CircleButton';
+import EndIcon from '../../assets/images/End.svg';
+import AnswerIcon from '../../assets/images/Answer.svg';
 
 import styles from './styles.scss';
 
@@ -22,7 +31,12 @@ function CallIcon({
   missed,
   active,
   ringing,
+  inboundTitle,
+  outboundTitle,
+  missedTitle,
 }) {
+  const title = missed ? missedTitle :
+    ((direction === callDirections.inbound) ? inboundTitle : outboundTitle);
   return (
     <div className={styles.callIcon}>
       <span
@@ -31,135 +45,327 @@ function CallIcon({
           active && styles.activeCall,
           ringing && styles.ringing,
           missed && styles.missed,
-        )} />
+        )}
+        title={title}
+      />
     </div>
   );
 }
+
 CallIcon.propTypes = {
   direction: PropTypes.string.isRequired,
   missed: PropTypes.bool,
   active: PropTypes.bool,
   ringing: PropTypes.bool,
+  inboundTitle: PropTypes.string,
+  outboundTitle: PropTypes.string,
+  missedTitle: PropTypes.string,
 };
+
 CallIcon.defaultProps = {
   missed: false,
   active: false,
   ringing: false,
+  inboundTitle: undefined,
+  outboundTitle: undefined,
+  missedTitle: undefined,
 };
 
-export default class CallItem extends Component {
+function ClickToSmsButton({
+  className,
+  onClickToSms,
+  disableLinks,
+  phoneNumber,
+  title,
+}) {
+  return (
+    <Button
+      className={classnames(styles.sms, className)}
+      onClick={onClickToSms}
+      disabled={disableLinks || !phoneNumber} >
+      <span
+        className={dynamicsFont.composeText}
+        title={title}
+      />
+    </Button>
+  );
+}
+
+ClickToSmsButton.propTypes = {
+  className: PropTypes.string,
+  onClickToSms: PropTypes.func,
+  disableLinks: PropTypes.bool,
+  phoneNumber: PropTypes.string,
+};
+ClickToSmsButton.defaultProps = {
+  className: undefined,
+  onClickToSms: undefined,
+  disableLinks: false,
+  phoneNumber: undefined,
+};
+
+function AddContactButton({
+  className,
+  onAddContact,
+  disableLinks,
+  phoneNumber,
+  title,
+}) {
+  return (
+    <Button
+      className={classnames(styles.addContact, className)}
+      onClick={onAddContact}
+      disabled={disableLinks || !phoneNumber} >
+      <span
+        className={dynamicsFont.add2}
+        title={title}
+      />
+    </Button>
+  );
+}
+
+function ExtendIcon() {
+  return (
+    <div className={styles.extendIcon}>
+      <div className={styles.extendInner} />
+      <div className={styles.extendInnerIcon} />
+    </div>
+  );
+}
+
+export default class ActiveCallItem extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selected: 0,
+      isLogging: false,
+      extended: false,
+    };
+    this._userSelection = false;
+
+    this.toggleExtended = () => {
+      console.log('clicked');
+      this.setState(preState => ({
+        extended: !preState.extended,
+      }));
+    };
+  }
+
+  onSelectContact = (value, idx) => {
+    const nameMatches = this.getContactMatches();
+    const selected = nameMatches.findIndex(
+      match => match.id === value.id
+    );
+    this._userSelection = true;
+    this.setState({
+      selected,
+    });
+    if (
+      this.props.call.activityMatches.length > 0 &&
+      this.props.autoLog
+    ) {
+      this.logCall({ redirect: false, selected });
+    }
+  }
+
+  getFallbackContactName() {
+    return isInbound(this.props.call) ?
+      (this.props.call.from.name) :
+      (this.props.call.to.name);
+  }
+
+  getSelectedContact = (selected = this.state.selected) => {
+    const contactMatches = this.getContactMatches();
+    return (selected > -1 && contactMatches[selected]) ||
+      (contactMatches.length === 1 && contactMatches[0]) ||
+      null;
+  }
+
+  getContactMatches(nextProps = this.props) {
+    return isInbound(nextProps.call) ?
+      nextProps.call.fromMatches :
+      nextProps.call.toMatches;
+  }
+
+  getPhoneNumber() {
+    return isInbound(this.props.call) ?
+      (this.props.call.from.phoneNumber || this.props.call.from.extensionNumber) :
+      (this.props.call.to.phoneNumber || this.props.call.to.extensionNumber);
+  }
+
+  getMyPhoneNumber() {
+    return isInbound(this.props.call) ?
+      (this.props.call.to.phoneNumber || this.props.call.to.extensionNumber) :
+      (this.props.call.from.phoneNumber || this.props.call.from.extensionNumber);
+  }
+
+  getWebphoneButtons() {
+    const {
+      call: {
+        webphoneSession,
+      },
+      webphoneAnswer,
+      webphoneReject,
+      webphoneHangup,
+      webphoneResume,
+    } = this.props;
+    if (!webphoneSession) {
+      return null;
+    }
+    let hangupFunc = webphoneHangup;
+    let resumeFunc = webphoneResume;
+    if (
+      webphoneSession.direction === callDirections.inbound &&
+      webphoneSession.callStatus === sessionStatus.connecting
+    ) {
+      hangupFunc = webphoneReject;
+      resumeFunc = webphoneAnswer;
+    }
+    const onHangupFunc = (e) => {
+      e.stopPropagation();
+      hangupFunc(webphoneSession.id);
+    };
+    const onResumeFunc = (e) => {
+      e.stopPropagation();
+      resumeFunc(webphoneSession.id);
+    };
+    return (
+      <div className={styles.webphoneButtons}>
+        <CircleButton
+          className={styles.rejectButton}
+          onClick={onHangupFunc}
+          icon={EndIcon}
+          showBorder={false}
+        />
+        <CircleButton
+          className={styles.answerButton}
+          onClick={onResumeFunc}
+          icon={AnswerIcon}
+          showBorder={false}
+        />
+      </div>
+    );
+  }
+
+  getCallInfo() {
+    const {
+      call: {
+        telephonyStatus,
+        startTime,
+        webphoneSession,
+      },
+      disableLinks,
+      currentLocale,
+      formatPhone,
+    } = this.props;
+    const myPhoneNumber = this.getMyPhoneNumber();
+    if (webphoneSession) {
+      return (
+        <div className={styles.callDetail}>
+          <span className={styles.label}>
+            {
+              isInbound(this.props.call) ?
+                i18n.getString('to', currentLocale) :
+                i18n.getString('from', currentLocale)
+            }:
+          </span>
+          {formatPhone(myPhoneNumber)}
+        </div>
+      );
+    }
+    return (
+      <div className={styles.callDetail}>
+        {
+          disableLinks ?
+            i18n.getString('unavailable', currentLocale) :
+            <DurationCounter startTime={startTime} />
+        }
+        <span className={styles.split}>|</span>
+        { i18n.getString(telephonyStatus, currentLocale) }
+      </div>
+    );
   }
 
   render() {
     const {
       call: {
         direction,
-        telephonyStatus,
-        result,
-        startTime,
-        duration,
-        activityMatches,
-        webphoneSession,
       },
+      disableLinks,
       currentLocale,
-      webphoneAnswer,
-      webphoneReject,
-      webphoneHangup,
-      webphoneResume,
+      areaCode,
+      countryCode,
+      outboundSmsPermission,
+      internalSmsPermission,
+      enableContactFallback,
+      isLogging,
+      brand,
+      showContactDisplayPlaceholder,
     } = this.props;
+    const phoneNumber = this.getPhoneNumber();
+    const contactMatches = this.getContactMatches();
+    const fallbackContactName = this.getFallbackContactName();
+    const ringing = isRinging(this.props.call);
+    const webphoneEl = this.getWebphoneButtons();
+    const callDetail = this.getCallInfo();
 
-    const durationEl = disableLinks ?
-        i18n.getString('unavailable', currentLocale) :
-        <DurationCounter startTime={startTime} />;
-    let dateEl;
-    let statusEl;
-    if (active) {
-      statusEl = i18n.getString(result || telephonyStatus, currentLocale);
-    }
-    let webphoneEl;
-    if (webphoneSession) {
-      let hangupFunc = webphoneHangup;
-      let resumeFunc = webphoneResume;
-      if (
-        webphoneSession.direction === callDirections.inbound &&
-        webphoneSession.callStatus === sessionStatus.connecting
-      ) {
-        hangupFunc = webphoneReject;
-        resumeFunc = webphoneAnswer;
-      }
-      webphoneEl = (
-        <div className={styles.webphoneButtons}>
-          <Button
-            className={classnames(styles.webphoneButton, styles.rejectWebphoneButton)}
-            onClick={() => hangupFunc(webphoneSession.id)}
-          >
-            <i className={dynamicsFont.missed} />
-          </Button>
-          <Button
-            className={styles.webphoneButton}
-            onClick={() => resumeFunc(webphoneSession.id)}
-          >
-            <i className={dynamicsFont.call} />
-          </Button>
-        </div>
-      );
-    }
     return (
-      <div className={styles.root}>
-        <CallIcon
-          direction={direction}
-          ringing={ringing}
-          active={active}
-          missed={missed}
-        />
-        <ContactDisplay
-          className={classnames(
-            styles.contactDisplay,
-            missed && styles.missed,
-            active && styles.active,
-          )}
-          contactMatches={contactMatches}
-          selected={this.state.selected}
-          onSelectContact={this.onSelectContact}
-          disabled={disableLinks}
-          isLogging={isLogging || this.state.isLogging}
-          fallBackName={fallbackContactName}
-          enableContactFallback={enableContactFallback}
-          areaCode={areaCode}
-          countryCode={countryCode}
-          phoneNumber={phoneNumber}
-          currentLocale={currentLocale}
-        />
-        <div className={styles.details} >
-          {durationEl} | {dateEl}{statusEl}
+      <div className={styles.root} onClick={this.toggleExtended}>
+        <div className={styles.callInfo}>
+          <CallIcon
+            direction={direction}
+            ringing={ringing}
+            active
+            missed={false}
+            inboundTitle={i18n.getString('inboundCall', currentLocale)}
+            outboundTitle={i18n.getString('outboundCall', currentLocale)}
+            missedTitle={i18n.getString('missedCall', currentLocale)}
+          />
+          <ContactDisplay
+            className={styles.contactDisplay}
+            contactMatches={contactMatches}
+            selected={this.state.selected}
+            onSelectContact={this.onSelectContact}
+            disabled={disableLinks}
+            isLogging={isLogging || this.state.isLogging}
+            fallBackName={fallbackContactName}
+            enableContactFallback={enableContactFallback}
+            areaCode={areaCode}
+            countryCode={countryCode}
+            phoneNumber={phoneNumber}
+            currentLocale={currentLocale}
+            brand={brand}
+            showPlaceholder={showContactDisplayPlaceholder}
+            showType={false}
+            stopPropagation
+          />
+          {callDetail}
+          {webphoneEl}
         </div>
-        <ActionMenu
-          currentLocale={currentLocale}
-          onLog={onLogCall && this.logCall}
-          onViewEntity={onViewContact && this.viewSelectedContact}
-          onCreateEntity={onCreateContact && this.createSelectedContact}
-          hasEntity={!!contactMatches.length}
-          onClickToDial={onClickToDial && this.clickToDial}
-          onClickToSms={
-            showClickToSms ?
-              () => this.clickToSms({ countryCode, areaCode })
-              : undefined
-          }
-          phoneNumber={phoneNumber}
-          disableLinks={disableLinks}
-          disableClickToDial={disableClickToDial}
-          isLogging={isLogging || this.state.isLogging}
-          isLogged={activityMatches.length > 0}
-          isCreating={this.state.isCreating}
-        />
+        <div
+          className={classnames(
+            styles.actionMenu,
+            this.state.extended ? styles.extended : null
+          )}
+        >
+          <ClickToSmsButton
+            className={styles.smsButton}
+            disableLinks={disableLinks}
+            phoneNumber={phoneNumber}
+          />
+          <AddContactButton
+            className={styles.addContactButton}
+            disableLinks={disableLinks}
+            phoneNumber={phoneNumber}
+          />
+        </div>
+        <ExtendIcon />
       </div>
     );
   }
 }
 
-CallItem.propTypes = {
+ActiveCallItem.propTypes = {
   call: PropTypes.shape({
     direction: PropTypes.string.isRequired,
     telephonyStatus: PropTypes.string,
@@ -182,27 +388,24 @@ CallItem.propTypes = {
   areaCode: PropTypes.string.isRequired,
   countryCode: PropTypes.string.isRequired,
   currentLocale: PropTypes.string.isRequired,
-  onLogCall: PropTypes.func,
-  onViewContact: PropTypes.func,
-  onCreateContact: PropTypes.func,
-  onClickToDial: PropTypes.func,
-  onClickToSms: PropTypes.func,
-  isLoggedContact: PropTypes.func,
   disableLinks: PropTypes.bool,
-  disableClickToDial: PropTypes.bool,
+  isLoggedContact: PropTypes.func,
   outboundSmsPermission: PropTypes.bool,
   internalSmsPermission: PropTypes.bool,
-  active: PropTypes.bool.isRequired,
   dateTimeFormatter: PropTypes.func.isRequired,
   isLogging: PropTypes.bool,
-  // webphoneAnswer: PropTypes.func,
-  // webphoneReject: PropTypes.func,
-  // webphoneHangup: PropTypes.func,
-  // webphoneResume: PropTypes.func,
+  webphoneAnswer: PropTypes.func,
+  webphoneReject: PropTypes.func,
+  webphoneHangup: PropTypes.func,
+  webphoneResume: PropTypes.func,
   enableContactFallback: PropTypes.bool,
+  autoLog: PropTypes.bool,
+  brand: PropTypes.string,
+  showContactDisplayPlaceholder: PropTypes.bool,
+  formatPhone: PropTypes.func.isRequired,
 };
 
-CallItem.defaultProps = {
+ActiveCallItem.defaultProps = {
   onLogCall: undefined,
   onClickToDial: undefined,
   onClickToSms: undefined,
@@ -214,9 +417,12 @@ CallItem.defaultProps = {
   outboundSmsPermission: false,
   internalSmsPermission: false,
   disableLinks: false,
-  // webphoneAnswer: () => null,
-  // webphoneReject: () => null,
-  // webphoneHangup: () => null,
-  // webphoneResume: () => null,
+  webphoneAnswer: () => null,
+  webphoneReject: () => null,
+  webphoneHangup: () => null,
+  webphoneResume: () => null,
   enableContactFallback: undefined,
+  autoLog: false,
+  brand: 'RingCentral',
+  showContactDisplayPlaceholder: true,
 };
--- src/components/ActiveCallsPanel/index.js
+++ src/components/ActiveCallsPanel/index.js
@@ -5,15 +5,31 @@ import SpinnerOverlay from '../SpinnerOverlay';
 import ActiveCallItem from '../ActiveCallItem';
 
 import styles from './styles.scss';
+import i18n from './i18n';
 
 export default function ActiveCallsPanel({
   calls,
   showSpinner,
   className,
   currentLocale,
+  areaCode,
+  countryCode,
+  dateTimeFormatter,
+  brand,
+  showContactDisplayPlaceholder,
+  formatPhone,
 }) {
   if (showSpinner) {
     return (<SpinnerOverlay />);
+  }
+  if (calls.length === 0) {
+    return (
+      <div className={classnames(styles.root, className)}>
+        <p className={styles.noCalls}>
+          {i18n.getString('noActiveCalls', currentLocale)}
+        </p>
+      </div>
+    );
   }
   return (
     <div className={classnames(styles.root, className)}>
@@ -23,6 +39,12 @@ export default function ActiveCallsPanel({
             call={call}
             key={call.id}
             currentLocale={currentLocale}
+            areaCode={areaCode}
+            countryCode={countryCode}
+            dateTimeFormatter={dateTimeFormatter}
+            brand={brand}
+            showContactDisplayPlaceholder={showContactDisplayPlaceholder}
+            formatPhone={formatPhone}
           />
         ))
       }
@@ -32,7 +54,19 @@ export default function ActiveCallsPanel({
 
 ActiveCallsPanel.propTypes = {
   currentLocale: PropTypes.string.isRequired,
-  className: PropTypes.string.isRequired,
+  className: PropTypes.string,
   calls: PropTypes.array.isRequired,
   showSpinner: PropTypes.bool.isRequired,
+  areaCode: PropTypes.string.isRequired,
+  countryCode: PropTypes.string.isRequired,
+  brand: PropTypes.string,
+  showContactDisplayPlaceholder: PropTypes.bool,
+  dateTimeFormatter: PropTypes.func.isRequired,
+  formatPhone: PropTypes.func.isRequired,
+};
+
+ActiveCallsPanel.defaultProps = {
+  className: undefined,
+  brand: 'RingCentral',
+  showContactDisplayPlaceholder: true,
 };
