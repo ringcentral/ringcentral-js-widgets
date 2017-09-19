8f91965cddd8da4881bd7f21222da2930a9beb26
Jack Tzu-Chieh Huang <u9520107@gmail.com>
2017-09-19 15:28:52 +0800

    add AdapterCore

---
+++ src/lib/AdapterCore/baseMessageTypes.js
@@ -0,0 +1,11 @@
+import Enum from 'ringcentral-integration/lib/Enum';
+
+export default new Enum([
+  'syncClosed',
+  'syncMinimized',
+  'syncSize',
+  'syncPosition',
+  'syncPresence',
+  'syncAdapterState',
+  'presenceClicked',
+], 'adapter');
