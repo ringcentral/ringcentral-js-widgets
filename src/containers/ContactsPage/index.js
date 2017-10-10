150d3c76ed011fdbccb34cb718d884c778fba341
Michael Lin <unadlib@gmail.com>
2017-10-10 16:16:09 +0800

    update code linting

--- src/containers/ContactsPage/index.js
+++ src/containers/ContactsPage/index.js
@@ -35,12 +35,13 @@ function mapToFunctions(_, {
       return presence;
     },
     onItemSelect: async ({ type, id }) => {
-      const searchSource = contacts[`${type}Contacts`] || []
-      const isInsure = searchSource.map(({id})=>id.toString()).includes(id)
+      const searchSource = contacts[`${type}Contacts`] || [];
+      const isInsure = searchSource.map(({id}) => id.toString()).includes(id);
       if (!isInsure) {
         const searchCriteria = JSON.parse(JSON.stringify(contactSearch.state.searchCriteria));
         await contacts.showAlert();
-        await contactSearch.searchPlus({...contactSearch.state.searchCriteria,searchString: undefined});
+        const resetSearchCriteria= { ...contactSearch.state.searchCriteria, searchString: undefined };
+        await contactSearch.searchPlus(resetSearchCriteria);
         await contactSearch.searchPlus(searchCriteria);
       } else {
         router.push(`/contacts/${type}/${id}`);
