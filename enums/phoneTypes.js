29bf6e187c53793fd32fd910f3c7440c8fae7d91
RingCentral Integrations Team <integrations@ringcentral.com>
2017-12-05 07:53:13 +0000

    released at Tue Dec  5 07:53:13 UTC 2017, commit: 129dd3e0aa39351177167e02c585e8869a361c21

--- enums/phoneTypes.js
+++ enums/phoneTypes.js
@@ -10,5 +10,5 @@ var _Enum2 = _interopRequireDefault(_Enum);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = new _Enum2.default(['business', 'extension', 'home', 'mobile', 'phone', 'unknown']);
+exports.default = new _Enum2.default(['business', 'extension', 'home', 'mobile', 'phone', 'unknown', 'company', 'direct']);
 //# sourceMappingURL=phoneTypes.js.map
