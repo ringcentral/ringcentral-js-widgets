57f8774b89ccbd146162b4fab1ee43dcdd49a8b1
Kevin Zeng <zengfenfei@gmail.com>
2017-12-06 11:11:07 +0800

    Conference for google V4 (#521)
    * Conference dial in number dropdown select
    
    * Format conference dial in phone numbers
    
    * Conference form fields
    
    * Conference help commands
    
    * Conference action buttons
    
    * Conference show additional dial in numbers control
    
    * Conference show and hide all dial in numbers for selection
    
    * Select and deselect additional numbers
    
    * Select number list styles
    
    * Use color variables
    
    * Connect with real data
    
    * Conference additional dial in numbers
    
    * Conference invite with SMS
    
    * Conference join as host
    
    * Conference support additional action buttons
    
    * Add background color for conference action buttons
    
    * Update conference link
    
    * Conference allow join before host
    
    * Fix joinAsHost
    
    * Conference help commands link
    
    * Add key for conference additional buttons
    
    * Fix lint
    
    * Update commons
    
    * Use style variables

--- src/components/ConferencePanel/i18n/en-US.js
+++ src/components/ConferencePanel/i18n/en-US.js
@@ -1,11 +1,14 @@
 export default {
   newConference: 'New Conference',
   dialInNumber: 'Dial-in Number',
-  host: 'Host',
-  participants: 'Participants',
-  internationalParticipants: 'International participants',
-  internationalNumbersHeader: 'Select International Dial-in Numbers',
-  search: 'Search...',
+  hostAccess: 'Host Access',
+  participantsAccess: 'Participants Access',
+  addinalDialInNumbers: 'Additional Dial-in Numbers',
+  selectNumbers: 'Select Numbers',
+  enableJoinBeforeHost: 'Enable join before Host',
+  conferenceCommands: 'Conference Commands',
+  inviteWithGCalendar: 'Invite with Google Calendar',
   inviteWithText: 'Invite with Text',
+  joinAsHost: 'Join as Host',
   inviteText: 'Please join the RingCentral conference.\n\nDial-In Numbers: {dialInNumber} \n\n{internationals} \nParticipant Access: {participantCode} \n\nNeed an international dial-in phone number? Please visit http://www.ringcentral.com/conferencing \n\nThis conference call is brought to you by RingCentral Conferencing.'
 };
