f2668fd9e2cff250e23ca6dfff8de714be352522
caffffe <1223840780@qq.com>
2017-12-08 11:07:37 +0800

    Voicemail delete (#524)
    * voice mail delete
    
    * discard changes in package.json
    
    * discard changes in yarn.lock
    
    * disable delete button when confirm to delte
    
    * add messageStoreAlert
    
    * codacy

---
+++ src/components/MessageStoreAlert/i18n/en-US.js
@@ -0,0 +1,5 @@
+import messageStoreErrors from 'ringcentral-integration/modules/MessageStore/messageStoreErrors';
+
+export default {
+  [messageStoreErrors.deleteFailed]: 'Cannot delete the voicemail due to internal server error.',
+};
---
+++ src/components/MessageStoreAlert/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import messageStoreErrors from 'ringcentral-integration/modules/MessageStore/messageStoreErrors';
+import FormattedMessage from '../FormattedMessage';
+import i18n from './i18n';
+
+export default function MessageStoreAlert(props) {
+  const { message } = props.message;
+  let view = (<span>{i18n.getString(message, props.currentLocale)}</span>);
+  // Handle call record error
+  if (message === messageStoreErrors.deleteFailed) {
+    view = (
+      <FormattedMessage
+        message={i18n.getString(message, props.currentLocale)}
+      />
+    );
+  }
+  return view;
+}
+
+MessageStoreAlert.propTypes = {
+  currentLocale: PropTypes.string.isRequired,
+  message: PropTypes.shape({
+    message: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+MessageStoreAlert.handleMessage = ({ message }) => (
+  (message === messageStoreErrors.deleteFailed)
+);
