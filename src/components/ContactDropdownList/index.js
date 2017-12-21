f535dc81db5daa07532b57fb1a00a6aaa8c7473d
embbnux <embbnux@embbnux.com>
2017-12-21 17:23:21 +0800

    fix ContactDropdownList error when node is null

--- src/components/ContactDropdownList/index.js
+++ src/components/ContactDropdownList/index.js
@@ -65,12 +65,12 @@ ContactItem.defaultProps = {
 
 class ContactDropdownList extends Component {
   componentWillReceiveProps(nextProps) {
-    if (!nextProps.visibility) {
+    if (!nextProps.visibility || nextProps.items.length === 0) {
       return;
     }
     if (nextProps.scrollDirection === 'ArrowDown') {
       if (nextProps.selectedIndex < nextProps.items.length) {
-        if (nextProps.selectedIndex > 4) {
+        if (nextProps.selectedIndex > 4 && this.node) {
           this.node.scrollTop += 53;
           this.node.scrollTop = Math.floor(this.node.scrollTop / 53) * 53;
         }
@@ -78,7 +78,7 @@ class ContactDropdownList extends Component {
     }
     if (nextProps.scrollDirection === 'ArrowUp') {
       if (nextProps.selectedIndex > -1) {
-        if (nextProps.selectedIndex < nextProps.items.length - 4) {
+        if ((nextProps.selectedIndex < nextProps.items.length - 4) && this.node) {
           this.node.scrollTop -= 53;
           this.node.scrollTop = Math.floor(this.node.scrollTop / 53) * 53;
         }
