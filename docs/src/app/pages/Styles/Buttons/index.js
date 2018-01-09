bebfc6ae35d19d0a03a636cc92a10b4392bdfdb8
embbnux <embbnux@embbnux.com>
2018-01-09 17:34:44 +0800

    update buttons ui of docs

--- docs/src/app/pages/Styles/Buttons/index.js
+++ docs/src/app/pages/Styles/Buttons/index.js
@@ -3,250 +3,247 @@ import React from 'react';
 import Markdown from '../../../components/Markdown';
 import styles from './styles.scss';
 
-function ButtonItem({
-  text,
-  position,
-  fillColor,
-  fillColorName,
-  height,
-  cornerRadius,
-  textSize,
-  textColor,
-  textColorName,
-  borderColor,
-  borderColorName,
-}) {
+function ButtonsWrapper(props) {
   const style = {
-    background: fillColor,
-    height: `${height}px`,
-    lineHeight: `${height}px`,
-    borderRadius: `${cornerRadius}px`,
-    color: textColor,
-    fontSize: `${textSize}px`,
+    width: `${props.width}px`,
+    height: `${props.height}px`,
+    lineHeight: `${props.height}px`,
+    borderRadius: `${props.cornerRadius}px`,
+    color: props.textColor,
+    fontSize: `${props.textSize}px`,
   };
-  let borderText;
-  if (borderColor) {
-    style.border = `solid 1px ${borderColor}`;
-    borderText = <p>Border: {borderColorName} {borderColor}</p>;
-  }
-  if (position === 'popup') {
-    style.width = '65px';
-  }
+  const buttons = ['normal', 'hover', 'pressed', 'disable'].map((type) => {
+    let buttonStyle = { ...style };
+    if (props[`${type}Style`]) {
+      buttonStyle = {
+        ...style,
+        ...(props[`${type}Style`])
+      };
+    }
+    return (
+      <div
+        key={type}
+        className={styles.button}
+        style={buttonStyle}
+      >
+        {type.charAt(0).toUpperCase() + type.slice(1)}
+      </div>
+    );
+  });
+  const fillDesriptions = ['normal', 'hover', 'pressed', 'disable'].map((type) => {
+    const typeStyle = props[`${type}Style`];
+    const name = type.charAt(0).toUpperCase() + type.slice(1);
+    return (
+      <div>
+        <div className={styles.descriptionLine} key={type}>
+          {name} fill color: {typeStyle.backgroundName} {typeStyle.background}
+        </div>
+        {
+          typeStyle.borderColorName && (
+            <div className={styles.descriptionLine}>
+              {name} border color: {typeStyle.borderColorName}
+            </div>
+          )
+        }
+        {
+          typeStyle.colorName && (
+            <div className={styles.descriptionLine}>
+              {name} Text: {props.textSize} {typeStyle.colorName} {typeStyle.color}
+            </div>
+          )
+        }
+      </div>
+    );
+  });
   return (
-    <div className={styles.buttonItem}>
-      <div className={styles.button} style={style}>{text}</div>
-      <div className={styles.styleText}>
-        <p>Fill Color: {fillColorName} {fillColor}</p>
-        {borderText}
-        <p>Height: {height}</p>
-        <p>Corner radius: {cornerRadius}</p>
-        <p>Text: {textSize} {textColorName} {textColor}</p>
+    <div className={styles.buttonsWrapperRoot}>
+      <div className={styles.wrapperTitle}>{props.title}</div>
+      <div className={styles.wrapperButtonList}>
+        {buttons}
+      </div>
+      <div className={styles.description}>
+        <div className={styles.descriptionLine}>
+          Minumum width: {props.width}
+        </div>
+        <div className={styles.descriptionLine}>
+          Height: {props.height}
+        </div>
+        <div className={styles.descriptionLine}>
+          Corner radius: {props.cornerRadius}
+        </div>
+        {
+          props.textColor && (
+            <div className={styles.descriptionLine}>
+              Text: {props.textSize} {props.textColorName} {props.textColor}
+            </div>
+          )
+        }
+        { fillDesriptions }
       </div>
     </div>
   );
 }
 
-const pagePrimaryButtons = [{
-  text: 'Normal',
-  position: 'page',
-  fillColor: '#0684BD',
-  fillColorName: 'RC Bule',
+const primaryButtonInPage = {
+  title: 'Primary button in the page',
+  width: 260,
   height: 35,
   cornerRadius: 100,
   textSize: 13,
   textColor: '#FFFFFF',
   textColorName: 'Regular Snow',
-}, {
-  text: 'Hover',
-  position: 'page',
-  fillColor: '#389DCA',
-  fillColorName: 'Sea',
-  height: 35,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#FFFFFF',
-  textColorName: 'Regular Snow',
-}, {
-  text: 'Pressed',
-  position: 'page',
-  fillColor: '#0570A1',
-  fillColorName: 'Marine',
-  height: 35,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#FFFFFF',
-  textColorName: 'Regular Snow',
-}, {
-  text: 'Disable',
-  position: 'page',
-  fillColor: '#C7C7C7',
-  fillColorName: 'Smoke',
-  height: 35,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#FFFFFF',
-  textColorName: 'Regular Snow',
-}];
+  normalStyle: {
+    background: '#0684BD',
+    backgroundName: 'RC Blue'
+  },
+  hoverStyle: {
+    background: '#389DCA',
+    backgroundName: 'Sea'
+  },
+  pressedStyle: {
+    background: '#0570A1',
+    backgroundName: 'Marine'
+  },
+  disableStyle: {
+    background: '#C7C7C7',
+    backgroundName: 'Smoke'
+  }
+};
 
-const pageSecondaryButtons = [{
-  text: 'Normal',
-  position: 'page',
-  fillColor: '#FFFFFF',
-  fillColorName: 'Regular Snow',
-  borderColor: '#9BCEE5',
-  borderColorName: 'Water',
+const secondaryButtonInPage = {
+  title: 'Secondary button in the page',
+  width: 260,
   height: 35,
   cornerRadius: 100,
   textSize: 13,
-  textColor: '#0684BD',
-  textColorName: 'Regular RC Bule',
-}, {
-  text: 'Hover',
-  position: 'page',
-  fillColor: '#389DCA',
-  fillColorName: 'Sea',
-  height: 35,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#FFFFFF',
-  textColorName: 'Regular Snow',
-}, {
-  text: 'Pressed',
-  position: 'page',
-  fillColor: '#0570A1',
-  fillColorName: 'Marine',
-  height: 35,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#FFFFFF',
-  textColorName: 'Regular Snow',
-}, {
-  text: 'Disable',
-  position: 'page',
-  fillColor: '#FFFFFF',
-  fillColorName: 'Regular Snow',
-  height: 35,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#C7C7C7',
-  textColorName: 'Regular Smoke',
-  borderColor: '#C7C7C7',
-  borderColorName: 'Smoke',
-}];
+  normalStyle: {
+    background: 'transparent',
+    color: '#0684BD',
+    border: 'solid 1px rgba(6, 132, 189, 0.5)',
+    borderColorName: '50% RC Blue #0684BD'
+  },
+  hoverStyle: {
+    color: '#FFFFFF',
+    background: '#389DCA',
+    backgroundName: 'Sea'
+  },
+  pressedStyle: {
+    color: '#FFFFFF',
+    background: '#0570A1',
+    backgroundName: 'Marine'
+  },
+  disableStyle: {
+    background: '#FFFFFF',
+    backgroundName: 'Snow',
+    color: '#C7C7C7',
+    colorName: 'Smoke',
+    border: 'solid 1px #C7C7C7',
+    borderColorName: '50% Smoke #C7C7C7'
+  }
+};
 
-const popupPrimaryButtons = [{
-  text: 'Normal',
-  position: 'popup',
-  fillColor: '#0684BD',
-  fillColorName: 'RC Bule',
+const primaryButtonInPopup = {
+  title: 'Primary button in the popup',
+  width: 70,
   height: 28,
   cornerRadius: 100,
   textSize: 13,
   textColor: '#FFFFFF',
   textColorName: 'Regular Snow',
-}, {
-  text: 'Hover',
-  position: 'popup',
-  fillColor: '#389DCA',
-  fillColorName: 'Sea',
-  height: 28,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#FFFFFF',
-  textColorName: 'Regular Snow',
-}, {
-  text: 'Pressed',
-  position: 'popup',
-  fillColor: '#0570A1',
-  fillColorName: 'Marine',
-  height: 28,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#FFFFFF',
-  textColorName: 'Regular Snow',
-}, {
-  text: 'Disable',
-  position: 'popup',
-  fillColor: '#C7C7C7',
-  fillColorName: 'Smoke',
-  height: 28,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#FFFFFF',
-  textColorName: 'Regular Snow',
-}];
+  normalStyle: {
+    background: '#0684BD',
+    backgroundName: 'RC Blue'
+  },
+  hoverStyle: {
+    background: '#389DCA',
+    backgroundName: 'Sea'
+  },
+  pressedStyle: {
+    background: '#0570A1',
+    backgroundName: 'Marine'
+  },
+  disableStyle: {
+    background: '#C7C7C7',
+    backgroundName: 'Smoke'
+  }
+};
 
-const popupSecondaryButtons = [{
-  text: 'Normal',
-  position: 'popup',
-  fillColor: '#FFFFFF',
-  fillColorName: 'Regular Snow',
-  borderColor: '#9BCEE5',
-  borderColorName: 'Water',
+const secondaryButtonInPopup = {
+  title: 'Secondary button in the popup',
+  width: 70,
   height: 28,
   cornerRadius: 100,
   textSize: 13,
-  textColor: '#0684BD',
-  textColorName: 'Regular RC Bule',
-}, {
-  text: 'Hover',
-  position: 'popup',
-  fillColor: '#389DCA',
-  fillColorName: 'Sea',
-  height: 28,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#FFFFFF',
-  textColorName: 'Regular Snow',
-}, {
-  text: 'Pressed',
-  position: 'popup',
-  fillColor: '#0570A1',
-  fillColorName: 'Marine',
-  height: 28,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#FFFFFF',
-  textColorName: 'Regular Snow',
-}, {
-  text: 'Disable',
-  position: 'popup',
-  fillColor: '#FFFFFF',
-  fillColorName: 'Regular Snow',
-  height: 28,
-  cornerRadius: 100,
-  textSize: 13,
-  textColor: '#C7C7C7',
-  textColorName: 'Regular Smoke',
-  borderColor: '#C7C7C7',
-  borderColorName: 'Smoke',
-}];
+  normalStyle: {
+    background: 'transparent',
+    color: '#0684BD',
+    colorName: 'RC Blue',
+    border: 'solid 1px rgba(6, 132, 189, 0.5)',
+    borderColorName: '50% RC Blue #0684BD'
+  },
+  hoverStyle: {
+    color: '#FFFFFF',
+    background: '#389DCA',
+    backgroundName: 'Sea'
+  },
+  pressedStyle: {
+    color: '#FFFFFF',
+    background: '#0570A1',
+    backgroundName: 'Marine'
+  },
+  disableStyle: {
+    background: '#FFFFFF',
+    backgroundName: 'Snow',
+    color: '#C7C7C7',
+    colorName: 'Smoke',
+    border: 'solid 1px #C7C7C7'
+  }
+};
+
 function ButtonsPage() {
   return (
     <div className={styles.root}>
-      <Markdown
-        text={'# Buttons\n'}
-      />
-      <div className={styles.header}>Page</div>
+      <div className={styles.header}>
+        Button
+      </div>
+      <div className={styles.subHeader}>Size & Specs</div>
       <div className={styles.buttonListGroup}>
         <div className={styles.buttonList}>
-          <div className={styles.subHeader}>Primary Button</div>
-          {pagePrimaryButtons.map(button => <ButtonItem {...button} key={button.text} />)}
+          <ButtonsWrapper {...primaryButtonInPage} />
         </div>
         <div className={styles.buttonList}>
-          <div className={styles.subHeader}>Secondary Button</div>
-          {pageSecondaryButtons.map(button => <ButtonItem {...button} key={button.text} />)}
+          <ButtonsWrapper {...secondaryButtonInPage} />
         </div>
       </div>
-      <div className={styles.header}>Popup</div>
+      <div className={styles.clearLine} />
       <div className={styles.buttonListGroup}>
         <div className={styles.buttonList}>
-          <div className={styles.subHeader}>Primary Button</div>
-          {popupPrimaryButtons.map(button => <ButtonItem {...button} key={button.text} />)}
+          <div
+            className={styles.button}
+            style={{
+              width: 260,
+              height: 35,
+              lineHeight: 35,
+              borderRadius: 100,
+              fontSize: 13,
+              color: '#FFFFFF',
+              background: '#0684BD',
+            }}
+          >
+            Normal
+          </div>
         </div>
         <div className={styles.buttonList}>
-          <div className={styles.subHeader}>Secondary Button</div>
-          {popupSecondaryButtons.map(button => <ButtonItem {...button} key={button.text} />)}
+          Test
+        </div>
+      </div>
+      <div className={styles.clearLine} />
+      <div className={styles.buttonListGroup}>
+        <div className={styles.buttonList}>
+          <ButtonsWrapper {...primaryButtonInPopup} />
+        </div>
+        <div className={styles.buttonList}>
+          <ButtonsWrapper {...secondaryButtonInPopup} />
         </div>
       </div>
     </div>
