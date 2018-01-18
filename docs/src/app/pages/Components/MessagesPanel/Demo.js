8414d24cdab7f12ed3bc14a112553c07fc806450
embbnux <embbnux@embbnux.com>
2018-01-18 15:10:53 +0800

    update docs

--- docs/src/app/pages/Components/MessagesPanel/Demo.js
+++ docs/src/app/pages/Components/MessagesPanel/Demo.js
@@ -4,18 +4,62 @@ import MessagesPanel from 'ringcentral-widgets/components/MessagesPanel';
 
 const props = {};
 props.currentLocale = 'en-US';
-props.composeText = () => null;
+props.goToComposeText = () => null;
 props.readVoicemail = () => null;
+props.unmarkVoicemail = () => null;
+props.markVoicemail = () => null;
 props.showConversationDetail = () => null;
+props.onClickToDial = () => null;
+props.onViewContact = () => null;
+props.onCreateContact = () => null;
+props.brand = 'RingCentral';
 props.textUnreadCounts = 0;
 props.voiceUnreadCounts = 0;
-
+props.conversations = [{
+  id: 1,
+  conversationId: '1',
+  subject: 'subject text',
+  correspondents: [{
+    phoneNumber: '123456789',
+  }],
+  correspondentMatches: [],
+  conversationMatches: [],
+  unreadCounts: 0,
+  type: 'SMS',
+  creationTime: '2018-01-17T08:59:02.000Z',
+}, {
+  id: 2,
+  conversationId: '2',
+  subject: 'subject text2',
+  correspondents: [{
+    phoneNumber: '123456788',
+  }],
+  correspondentMatches: [],
+  conversationMatches: [],
+  unreadCounts: 1,
+  type: 'SMS',
+  creationTime: '2018-01-16T08:59:02.000Z',
+}];
+props.countryCode = '1';
+props.areaCode = '657';
+props.typeFilter = 'All';
+props.dateTimeFormatter = ({ utcTimestamp }) => {
+  const time = new Date(utcTimestamp);
+  return `${time.getMonth() + 1}/${time.getDate()}/${time.getFullYear()}`;
+};
 /**
  * A example of `MessagesPanel`
  */
 const MessagesPanelDemo = () => (
-  <MessagesPanel
-    {...props}
-  />
+  <div style={{
+    position: 'relative',
+    height: '500px',
+    width: '300px',
+    border: '1px solid #f3f3f3',
+  }}>
+    <MessagesPanel
+      {...props}
+    />
+  </div>
 );
 export default MessagesPanelDemo;
