cc1686ca6fb47240e078f1c09f74ec959a59a8c1
RingCentral Integrations Team <integrations@ringcentral.com>
2018-02-03 09:32:52 +0000

    released at Sat Feb  3 09:32:52 UTC 2018, commit: 86a68862a5a5342a99ad3b380613eb952cb75f76

--- modules/ProxyFrameOAuth/index.js
+++ modules/ProxyFrameOAuth/index.js
@@ -41,6 +41,10 @@ var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstru
 
 var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
 
+var _get2 = require('babel-runtime/helpers/get');
+
+var _get3 = _interopRequireDefault(_get2);
+
 var _inherits2 = require('babel-runtime/helpers/inherits');
 
 var _inherits3 = _interopRequireDefault(_inherits2);
@@ -190,10 +194,61 @@ var ProxyFrameOAuth = (_dec = (0, _di.Module)({
     _this._defaultProxyRetry = defaultProxyRetry;
 
     _this._reducer = (0, _getProxyFrameOAuthReducer2.default)(_this.actionTypes);
+
+    _this._loggedIn = false;
     return _this;
   }
 
   (0, _createClass3.default)(ProxyFrameOAuth, [{
+    key: '_onStateChange',
+    value: function _onStateChange() {
+      (0, _get3.default)(ProxyFrameOAuth.prototype.__proto__ || (0, _getPrototypeOf2.default)(ProxyFrameOAuth.prototype), '_onStateChange', this).call(this);
+      if (this._auth.loggedIn === this._loggedIn) {
+        return;
+      }
+      this._loggedIn = this._auth.loggedIn;
+      if (this._loggedIn && this._auth.isImplicit) {
+        console.log('new login, start refresh token timeout');
+        this._createImplicitRefreshTimeout();
+      }
+      if (!this._loggedIn && this._auth.isImplicit) {
+        this._clearImplicitRefreshIframe();
+        if (this._implicitRefreshTimeoutId) {
+          clearTimeout(this._implicitRefreshTimeoutId);
+        }
+      }
+    }
+  }, {
+    key: '_handleCallbackUri',
+    value: function () {
+      var _ref4 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee2(options) {
+        return _regenerator2.default.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.next = 2;
+                return (0, _get3.default)(ProxyFrameOAuth.prototype.__proto__ || (0, _getPrototypeOf2.default)(ProxyFrameOAuth.prototype), '_handleCallbackUri', this).call(this, options);
+
+              case 2:
+                if (this._auth.isImplicit && this._auth.loggedIn) {
+                  this._createImplicitRefreshTimeout();
+                }
+
+              case 3:
+              case 'end':
+                return _context2.stop();
+            }
+          }
+        }, _callee2, this);
+      }));
+
+      function _handleCallbackUri(_x2) {
+        return _ref4.apply(this, arguments);
+      }
+
+      return _handleCallbackUri;
+    }()
+  }, {
     key: '_retrySetupProxyFrame',
     value: function _retrySetupProxyFrame() {
       this._retryTimeoutId = null;
@@ -215,10 +270,10 @@ var ProxyFrameOAuth = (_dec = (0, _di.Module)({
   }, {
     key: 'setupOAuth',
     value: function () {
-      var _ref4 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee2() {
-        return _regenerator2.default.wrap(function _callee2$(_context2) {
+      var _ref5 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee3() {
+        return _regenerator2.default.wrap(function _callee3$(_context3) {
           while (1) {
-            switch (_context2.prev = _context2.next) {
+            switch (_context3.prev = _context3.next) {
               case 0:
                 if (!this._proxyFrame) {
                   this.store.dispatch({
@@ -229,14 +284,14 @@ var ProxyFrameOAuth = (_dec = (0, _di.Module)({
 
               case 1:
               case 'end':
-                return _context2.stop();
+                return _context3.stop();
             }
           }
-        }, _callee2, this);
+        }, _callee3, this);
       }));
 
       function setupOAuth() {
-        return _ref4.apply(this, arguments);
+        return _ref5.apply(this, arguments);
       }
 
       return setupOAuth;
@@ -244,10 +299,10 @@ var ProxyFrameOAuth = (_dec = (0, _di.Module)({
   }, {
     key: 'destroyOAuth',
     value: function () {
-      var _ref5 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee3() {
-        return _regenerator2.default.wrap(function _callee3$(_context3) {
+      var _ref6 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee4() {
+        return _regenerator2.default.wrap(function _callee4$(_context4) {
           while (1) {
-            switch (_context3.prev = _context3.next) {
+            switch (_context4.prev = _context4.next) {
               case 0:
                 if (this._proxyFrame) {
                   if (this._retryTimeoutId) {
@@ -262,14 +317,14 @@ var ProxyFrameOAuth = (_dec = (0, _di.Module)({
 
               case 1:
               case 'end':
-                return _context3.stop();
+                return _context4.stop();
             }
           }
-        }, _callee3, this);
+        }, _callee4, this);
       }));
 
       function destroyOAuth() {
-        return _ref5.apply(this, arguments);
+        return _ref6.apply(this, arguments);
       }
 
       return destroyOAuth;
@@ -282,6 +337,77 @@ var ProxyFrameOAuth = (_dec = (0, _di.Module)({
           oAuthUri: this.oAuthUri
         }, '*');
       }
+    }
+  }, {
+    key: '_createImplicitRefreshIframe',
+    value: function _createImplicitRefreshIframe() {
+      var _this3 = this;
+
+      this._clearImplicitRefreshIframe();
+      this._implicitRefreshFrame = document.createElement('iframe');
+      this._implicitRefreshFrame.src = this.implictRefreshOAuthUri;
+      this._implicitRefreshFrame.style.display = 'none';
+      document.body.appendChild(this._implicitRefreshFrame);
+      // eslint-disable-next-line
+      this._implictitRefreshCallBack = function (_ref7) {
+        var origin = _ref7.origin,
+            data = _ref7.data;
+        var refreshCallbackUri = data.refreshCallbackUri;
+
+        if (refreshCallbackUri && _this3._auth.loggedIn) {
+          _this3._handleCallbackUri(refreshCallbackUri, true);
+          _this3._clearImplicitRefreshIframe();
+        }
+      };
+      window.addEventListener('message', this._implictitRefreshCallBack);
+    }
+  }, {
+    key: '_clearImplicitRefreshIframe',
+    value: function _clearImplicitRefreshIframe() {
+      if (this._implicitRefreshFrame) {
+        document.body.removeChild(this._implicitRefreshFrame);
+        this._implicitRefreshFrame = null;
+        window.removeEventListener('message', this._implictitRefreshCallBack);
+        this._callbackHandler = null;
+      }
+    }
+
+    // create a time out to refresh implicit flow token
+
+  }, {
+    key: '_createImplicitRefreshTimeout',
+    value: function _createImplicitRefreshTimeout() {
+      var _this4 = this;
+
+      if (this._implicitRefreshTimeoutId) {
+        clearTimeout(this._implicitRefreshTimeoutId);
+      }
+      var authData = this._auth.token;
+      var refreshTokenExpiresIn = authData.expiresIn;
+      var expireTime = authData.expireTime;
+
+      if (!refreshTokenExpiresIn || !expireTime) {
+        return;
+      }
+      // set refresh time to (token exposre time) / 3
+      var refreshTokenTimeoutTime = parseInt(refreshTokenExpiresIn, 10) * 1000 / 3;
+      if (refreshTokenTimeoutTime + Date.now() > expireTime) {
+        refreshTokenTimeoutTime = expireTime - Date.now() - 5000;
+        if (refreshTokenTimeoutTime < 0) {
+          return;
+        }
+      }
+      this._implicitRefreshTimeoutId = setTimeout(function () {
+        if (!_this4._auth.loggedIn) {
+          return;
+        }
+        if (_this4._tabManager && !_this4._tabManager.active) {
+          _this4._createImplicitRefreshTimeout();
+          return;
+        }
+        _this4._createImplicitRefreshIframe();
+        _this4._implicitRefreshTimeoutId = null;
+      }, refreshTokenTimeoutTime);
     }
   }, {
     key: 'name',
