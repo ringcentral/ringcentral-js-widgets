fd86c3e9fdd045f32c5752de032c9c95362603ba
Embbnux Ji <embbnux@embbnux.com>
2018-03-15 16:15:48 +0800

    add noInternational alert message (#659)

--- src/components/CallAlert/i18n/en-US.js
+++ src/components/CallAlert/i18n/en-US.js
@@ -8,6 +8,7 @@ export default {
   [callErrors.internalError]: 'Cannot connect due to internal errors. Please try again later.',
   [callErrors.notAnExtension]: 'The extension number does not exist.',
   [callErrors.networkError]: 'Cannot connect due to network issues. Please try again later.',
+  [callErrors.noInternational]: "You don't have permissions to make international calls. Please contact your {brand} account administrator for an upgrade.",
   [callErrors.noRingoutEnable]: 'Your extension is allowed to make calls with desktop app.\n    If you wish to switch to other calling options\n    please contact your account administrator for an upgrade.',
   areaCode: 'area code',
   telus911: 'Emergency dialing is not supported.'
