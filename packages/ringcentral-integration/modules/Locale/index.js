54cc084b85f607465f35630b54647dbf880a8762
embbnux <embbnux@embbnux.com>
2018-03-29 09:28:23 +0800

    update locale

--- packages/ringcentral-integration/modules/Locale/index.js
+++ packages/ringcentral-integration/modules/Locale/index.js
@@ -118,7 +118,7 @@ export default class Locale extends RcModule {
    * @property {String} currentLocale
    */
   get currentLocale() {
-    return (this.proxyState && (this.proxyState.proxyLocale || this._defaultLocale)) ||
+    return (this._transport && this.proxyState && (this.proxyState.proxyLocale || this._defaultLocale)) ||
       this.state.currentLocale ||
       this._defaultLocale;
   }
