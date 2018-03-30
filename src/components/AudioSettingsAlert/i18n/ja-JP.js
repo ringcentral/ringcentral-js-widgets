53f5102608faedb1ed852e4dbcb029b594bf67bf
Lex. Huang <531971909@qq.com>
2018-03-30 10:04:48 +0800

    fix(AudioSettingsAlert): change incorrect docwrittings (#672)

--- src/components/AudioSettingsAlert/i18n/ja-JP.js
+++
@@ -1,7 +0,0 @@
-import audioSettingsErrors from 'ringcentral-integration/modules/AudioSettings/audioSettingsErrors';
-
-export default {
-  [audioSettingsErrors.userMediaPermission]: 'オーディオへのアクセスを{brandName} for Googleに許可してください\u3002',
-};
-
-// @key: @#@"[audioSettingsErrors.userMediaPermission]"@#@ @source: @#@"Please grant {brandName} for Google to access your audio."@#@
