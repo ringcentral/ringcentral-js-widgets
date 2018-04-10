701804f09ca6f197be008010ef25411668b7e477
Rulee Chen <rulee@live.cn>
2018-04-10 12:32:20 +0800

    fix messageSenderMessages.recipientNumberInvalids in ja-JP

--- src/components/MessageSenderAlert/i18n/ja-JP.js
+++ src/components/MessageSenderAlert/i18n/ja-JP.js
@@ -11,7 +11,7 @@ export default {
   [messageSenderMessages.recipientsEmpty]: '有効な受信者番号を入力してください\u3002',
   [messageSenderMessages.textTooLong]: 'テキストが長すぎます\u3002上限は1,000文字です',
   [messageSenderMessages.multipartTextTooLong]: 'テキストが長すぎます\u3002上限は5,000文字です',
-  [messageSenderMessages.recipientNumberInvalids]: 'Veuillez entrer un numéro de téléphone valide.',
+  [messageSenderMessages.recipientNumberInvalids]: '有効な電話番号を入力してください\u3002',
   [messageSenderMessages.noAreaCode]: '7桁の国内電話番号を使用するには\u3001{areaCodeLink}を設定してください\u3002',
   [messageSenderMessages.specialNumber]: '緊急サービスまたは特別なサービスの番号へのテキストの送信はサポートされていません。',
   [messageSenderMessages.connectFailed]: '接続に失敗しました\u3002後でもう一度やり直してください\u3002',
