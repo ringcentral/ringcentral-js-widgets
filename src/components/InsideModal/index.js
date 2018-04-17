0017f3422dc0c7d660a85b779d19076d05920b1e
Michael Lin <unadlib@gmail.com>
2018-04-18 00:59:14 +0800

    implement call log section

---
+++ src/components/InsideModal/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from '../Modal';
+import styles from './styles.scss';
+import InsideModalClose from '../../assets/images/InsideModalClose.svg';
+
+export function CloseBtn({ onClick }) {
+  return (
+    <div
+      className={styles.closeBtn}
+      onClick={onClick}>
+      <InsideModalClose />
+    </div>
+  );
+}
+
+CloseBtn.propTypes = {
+  onClick: PropTypes.func,
+};
+
+CloseBtn.defaultProps = {
+  onClick: undefined,
+};
+
+export default function InsideModal({
+  show,
+  onClose,
+  appendDOM,
+  children,
+  title,
+}) {
+  const closeBtn = (<CloseBtn onClick={onClose} />);
+  return (
+    <Modal
+      title={title}
+      headerClassName={styles.title}
+      className={styles.container}
+      maskClassName={styles.mask}
+      modalClassName={styles.modal}
+      contentClassName={styles.content}
+      closeBtn={closeBtn}
+      show={show}
+      appendDOM={appendDOM}>
+      {children}
+    </Modal>
+  );
+}
+
+InsideModal.propTypes = {
+  show: PropTypes.bool,
+  onClose: PropTypes.func,
+  appendDOM: PropTypes.object,
+  children: PropTypes.node,
+  title: PropTypes.string,
+};
+
+InsideModal.defaultProps = {
+  title: null,
+  show: undefined,
+  onClose: undefined,
+  appendDOM: undefined,
+  children: undefined,
+};
---
+++ src/components/LogBasicInfo/index.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import dynamicsFont from 'ringcentral-widgets/assets/DynamicsFont/DynamicsFont.scss';
+import callDirections from 'ringcentral-integration/enums/callDirections';
+import callResults from 'ringcentral-integration/enums/callResults';
+import telephonyStatuses from 'ringcentral-integration/enums/telephonyStatus';
+import {
+  isMissed
+} from 'ringcentral-integration/lib/callLogHelpers';
+import i18n from './i18n';
+import styles from './styles.scss';
+
+const callIconMap = {
+  [callResults.missed]: dynamicsFont.missed,
+  [callDirections.inbound]: dynamicsFont.inbound,
+  [callDirections.outbound]: dynamicsFont.outbound,
+  [telephonyStatuses.ringing]: dynamicsFont.callHover
+};
+const colorStatusMap = {
+  green: [
+    telephonyStatuses.callConnected,
+    telephonyStatuses.ringing,
+    callResults.callAccepted,
+    callResults.accepted,
+  ],
+  red: [
+    callResults.missed,
+    callResults.voicemail,
+    callResults.rejected,
+    callResults.blocked,
+    callResults.noAnswer,
+    callResults.busy,
+    callResults.hangUp,
+    callResults.declined
+  ],
+  orange: [
+    telephonyStatuses.onHold,
+    telephonyStatuses.parkedCall
+  ],
+};
+
+export default function LogBasicInfo(props) {
+  const {
+    currentLog: {
+      call,
+      logName,
+    },
+    formatPhone,
+    currentLocale,
+  } = props;
+  if (!call) return null;
+  const {
+    direction,
+    to,
+    from,
+    duration,
+    result,
+    telephonyStatus
+  } = call;
+  const number = direction === callDirections.outbound ?
+    (to && (to.phoneNumber || to.extensionNumber)) :
+    (from && (from.phoneNumber || from.extensionNumber));
+  const formatNumber = formatPhone(number);
+  const status = result || telephonyStatus;
+  const active = (!duration && duration !== 0);
+  const missed = isMissed(call);
+  const green = colorStatusMap.green.includes(status);
+  const red = colorStatusMap.red.includes(status);
+  const orange = colorStatusMap.orange.includes(status);
+  const isRinging = status === telephonyStatuses.ringing;
+  const title = missed ? i18n.getString(callResults.missed, currentLocale) :
+    i18n.getString(direction, currentLocale);
+  const statusI18n = i18n.getString(status, currentLocale);
+  const iconClassName = classnames(
+    styles.icon,
+    isRinging && styles.ringing,
+    isRinging && dynamicsFont.callHover,
+    !isRinging && !missed && callIconMap[direction],
+    missed && styles.missed,
+    missed && callIconMap[callResults.missed],
+    !isRinging && active && styles.active
+  );
+  const statusClassName = classnames(
+    styles.status,
+    green && styles.green,
+    red && styles.red,
+    orange && styles.orange
+  );
+  return (
+    <div className={styles.callInfo}>
+      <div
+        className={styles.callIcon}
+        title={title}>
+        <span
+          className={iconClassName}
+        />
+      </div>
+      <ul className={styles.callDisplay}>
+        <li
+          className={styles.contact}
+          title={logName}>
+          {logName}
+        </li>
+        <li>
+          <span
+            className={styles.number}
+            title={formatNumber}>
+            {formatNumber}
+          </span>
+          <span className={styles.separator}>|</span>
+          <span
+            className={statusClassName}
+            title={statusI18n}>
+            {statusI18n}
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+LogBasicInfo.propTypes = {
+  currentLocale: PropTypes.string.isRequired,
+  formatPhone: PropTypes.func,
+  currentLog: PropTypes.object,
+};
+
+LogBasicInfo.defaultProps = {
+  formatPhone: value => value,
+  currentLog: {},
+};
