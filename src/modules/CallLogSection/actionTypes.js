c2ef93585633a0acd5f140041e9bd7a580d544bb
Michael Lin <unadlib@gmail.com>
2018-04-18 10:17:49 +0800

    implement CallLogSection

---
+++ src/modules/CallLogSection/actionTypes.js
@@ -0,0 +1,10 @@
+import Enum from 'ringcentral-integration/lib/Enum';
+import moduleActionTypes from 'ringcentral-integration/enums/moduleActionTypes';
+
+export default new Enum([
+  ...Object.keys(moduleActionTypes),
+  'update',
+  'cleanUp',
+  'showLogSection',
+  'hideLogSection',
+], 'callLogSection');
