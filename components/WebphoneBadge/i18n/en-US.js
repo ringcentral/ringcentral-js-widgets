9593f1e18ee9fe719fc7a96465ce767d6948962d
RingCentral Integrations Team <integrations@ringcentral.com>
2018-04-19 08:01:01 +0000

    released at Thu Apr 19 08:01:01 UTC 2018, commit: e2cc53987667a6b48db47e006f61865851e2cc1f

--- components/WebphoneBadge/i18n/en-US.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = {
-  webphoneUnavailable: 'Web Phone Unavailable'
-};
-//# sourceMappingURL=en-US.js.map
--- containers/CallBadgeContainer/i18n/en-US.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = {
-  activeCall: 'Active Call'
-};
-//# sourceMappingURL=en-US.js.map
