078c815989a689477ca73f208c1c6c72abd74c08
Lucy <382783004@qq.com>
2018-05-08 17:14:46 +0800

    Date picker (#741)
    * Date-Picker Widgets
    
    * no message

---
+++ packages/ringcentral-widgets/components/DatePicker/index.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import Moment from 'moment';
+import momentLocalizer from 'react-widgets-moment';
+import DateTimePicker from 'react-widgets/lib/DateTimePicker';
+import 'react-widgets/dist/css/react-widgets.css';
+import styles from './styles.scss';
+
+export default class DatePicker extends Component {
+  constructor(props) {
+    super(props);
+    Moment.locale(this.props.currentLocale);
+    momentLocalizer();
+  }
+  onClickFunc = () => {
+    const open = !!document.querySelector('.rw-open');
+    if (!open) {
+      this.date.inner.toggle();
+    } else {
+      this.date.inner.close();
+    }
+  }
+  render() {
+    const {
+      currentLocale,
+    } = this.props;
+    const dueDate = new Date().toISOString();
+    return (
+      <div className={classnames(styles.datePicker, this.props.datePickerClassName)}>
+        <DateTimePicker
+          culture={currentLocale}
+          time={false}
+          value={new Date(this.props.date)}
+          onChange={(currentStartTime) => {
+            if (currentStartTime) {
+              const date = new Date(dueDate);
+              date.setFullYear(currentStartTime.getFullYear());
+              date.setMonth(currentStartTime.getMonth());
+              date.setDate(currentStartTime.getDate());
+              this.props.onChange(date);
+            }
+          }}
+          ref={(ref) => { this.date = ref; }}
+          format="MM/DD/YY"
+          min={new Date()}
+        />
+        <div
+          onClick={() => this.onClickFunc()}
+          className={classnames(styles.dateTimeText, styles.dateText, this.props.dateTextClassName)}>
+          {this.props.label}:{Moment(this.props.date).format('MM/DD/YY')}
+        </div>
+      </div>
+    );
+  }
+}
+DatePicker.propTypes = {
+  currentLocale: PropTypes.string,
+  label: PropTypes.string,
+  date: PropTypes.instanceOf(Date),
+  onChange: PropTypes.func,
+  datePickerClassName: PropTypes.string,
+  dateTextClassName: PropTypes.string,
+};
+DatePicker.defaultProps = {
+  currentLocale: 'en-US',
+  label: '',
+  date: new Date(),
+  onChange: undefined,
+  datePickerClassName: '',
+  dateTextClassName: ''
+};
