8358447daaea41499514b4b14b9018877ad0e102
Jack Tzu-Chieh Huang <u9520107@gmail.com>
2018-05-30 13:33:32 +0800

    [feature] Apply infinite scrolling list to contacts

--- packages/ringcentral-integration/lib/contactHelper.js
+++ packages/ringcentral-integration/lib/contactHelper.js
@@ -1,3 +1,4 @@
+import { reduce, sort } from 'ramda';
 import isBlank from './isBlank';
 
 export const AllContactSourceName = 'all';
@@ -19,61 +20,57 @@ export function addPhoneToContact(contact, phone, type) {
   }
 }
 
-export function uniqueContactItems(result) {
-  let items = result || [];
-  // remove duplicated referencing
-  items = items.filter((value, index, arr) =>
-    arr.indexOf(value) === index
+export function uniqueContactItems(input = []) {
+  const map = {};
+  return reduce(
+    (result, item) => {
+      const itemId = `${item.type}${item.id}`;
+      if (!map[itemId]) {
+        map[itemId] = true;
+        result.push(item);
+      }
+      return result;
+    },
+    [],
+    input,
   );
-  // remove duplicated items by id
-  const hash = {};
-  const unique = [];
-  items.forEach((item) => {
-    const itemId = `${item.type}${item.id}`;
-    if (!hash[itemId]) {
-      hash[itemId] = 1;
-      unique.push(item);
-    }
-  });
-  return unique;
 }
 
 const NON_ALPHABET_RE = /[^a-z]/i;
-export function sortContactItemsByName(result) {
-  const items = result || [];
-  items.sort((a, b) => {
-    const name1 = (a.name || '').toLowerCase().replace(/^\s\s*/, ''); // trim start
-    const name2 = (b.name || '').toLowerCase().replace(/^\s\s*/, ''); // trim start
-    const isNumber1 = /^[0-9]/.test(name1);
-    const isNumber2 = /^[0-9]/.test(name2);
-    // Empty string should be put at the end
-    if (name1.length <= 0 || name2.length <= 0) {
-      return -name1.localeCompare(name2);
-    }
-    if (isNumber1 && isNumber2) {
-      return name1.localeCompare(name2);
-    }
-    if (isNumber1 || isNumber2) {
-      // put number name at last
-      return -name1.localeCompare(name2);
-    }
-    if (NON_ALPHABET_RE.test(name1[0]) && !NON_ALPHABET_RE.test(name2[0])) {
-      return 1;
-    }
-    if (!NON_ALPHABET_RE.test(name1[0]) && NON_ALPHABET_RE.test(name2[0])) {
-      return -1;
-    }
+function byName(a, b) {
+  const name1 = (a.name || '').toLowerCase().replace(/^\s\s*/, ''); // trim start
+  const name2 = (b.name || '').toLowerCase().replace(/^\s\s*/, ''); // trim start
+  const isNumber1 = /^[0-9]/.test(name1);
+  const isNumber2 = /^[0-9]/.test(name2);
+  // Empty string should be put at the end
+  if (name1.length <= 0 || name2.length <= 0) {
+    return -name1.localeCompare(name2);
+  }
+  if (isNumber1 && isNumber2) {
     return name1.localeCompare(name2);
-  });
-  return items;
+  }
+  if (isNumber1 || isNumber2) {
+    // put number name at last
+    return -name1.localeCompare(name2);
+  }
+  if (NON_ALPHABET_RE.test(name1[0]) && !NON_ALPHABET_RE.test(name2[0])) {
+    return 1;
+  }
+  if (!NON_ALPHABET_RE.test(name1[0]) && NON_ALPHABET_RE.test(name2[0])) {
+    return -1;
+  }
+  return name1.localeCompare(name2);
+}
+export function sortContactItemsByName(input = []) {
+  return input.sort(byName);
 }
 
 const POUND_SIGN = '#';
-export function groupByFirstLetterOfName(contactItems) {
+export function groupByFirstLetterOfName(input = []) {
   const groups = [];
-  if (contactItems && contactItems.length) {
+  if (input && input.length) {
     let group;
-    contactItems.forEach((contact) => {
+    input.forEach((contact) => {
       const name = (contact.name || '').replace(/^\s\s*/, ''); // trim start
       let letter = null;
       if (name.length <= 0 || NON_ALPHABET_RE.test(name[0])) {
