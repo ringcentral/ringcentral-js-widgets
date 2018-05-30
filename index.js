be98c54a5042cdf1256a8b29bb47b470d250927b
embbnux <embbnux@embbnux.com>
2018-05-30 17:05:35 +0800

    phone number release

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.formatTypes = exports.parse = exports.detect = exports.format = undefined;
+
+var _format = require('./lib/format');
+
+var _format2 = _interopRequireDefault(_format);
+
+var _detect = require('./lib/detect');
+
+var _detect2 = _interopRequireDefault(_detect);
+
+var _parse = require('./lib/parse');
+
+var _parse2 = _interopRequireDefault(_parse);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.format = _format2.default;
+exports.detect = _detect2.default;
+exports.parse = _parse2.default;
+exports.formatTypes = _format.formatTypes;
+//# sourceMappingURL=index.js.map
