3ecebb3e53fe02516650fad06b5be2077d53e5e6
embbnux <embbnux@embbnux.com>
2018-05-30 17:11:24 +0800

    add script and job to release i18n,locale-loader,phone-number

--- scripts/prepareRelease.js
+++ scripts/prepareRelease.js
@@ -1,7 +1,19 @@
 const prepareRelease = require('../packages/monorepo-tools/lib/releasePackage').prepareRelease;
 
-const packageNames = ['ringcentral-integration', 'ringcentral-widgets'];
-const releaseBranchNames = ['commons-release', 'widgets-release'];
+const packageNames = [
+  'ringcentral-integration',
+  'ringcentral-widgets',
+  '@ringcentral-integration/i18n',
+  '@ringcentral-integration/locale-loader',
+  '@ringcentral-integration/phone-number'
+];
+const releaseBranchNames = [
+  'commons-release',
+  'widgets-release',
+  'i18n-release',
+  'locale-loader-release',
+  'phone-number-release'
+];
 
 const args = process.argv.slice(2);
 const packageName = args[0];
