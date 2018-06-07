4c556e2b998833a5b8d710fc3b17a225feae675e
ele828 <ele828@gmail.com>
2018-06-07 16:17:02 +0800

    fix lint error

--- packages/ringcentral-widgets/components/SlideoutBar/index.js
+++ packages/ringcentral-widgets/components/SlideoutBar/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import CloseIcon from '../../assets/images/CloseIcon.svg';
