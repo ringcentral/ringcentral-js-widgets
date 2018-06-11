286b34c8910677dd7e53b29bed0d04f6f7b07294
RingCentral Integrations Team <integrations@ringcentral.com>
2018-06-11 10:24:53 +0000

    released at Mon Jun 11 10:24:53 UTC 2018, commit: a9fe43f2a106ebb7769337e33b476ad0366d3ce7

--- components/AuthAlert/i18n/index.js
+++ components/AuthAlert/i18n/index.js
@@ -4,9 +4,9 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _I18n = require('ringcentral-integration/lib/I18n');
+var _i18n = require('@ringcentral-integration/i18n');
 
-var _I18n2 = _interopRequireDefault(_I18n);
+var _i18n2 = _interopRequireDefault(_i18n);
 
 var _loadLocale = require('./loadLocale');
 
@@ -14,5 +14,5 @@ var _loadLocale2 = _interopRequireDefault(_loadLocale);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = new _I18n2.default(_loadLocale2.default);
+exports.default = new _i18n2.default(_loadLocale2.default);
 //# sourceMappingURL=index.js.map
