52c3cbfc4edb04a1adcd8b1a8db24ab14a83ff99
RingCentral Integrations Team <integrations@ringcentral.com>
2018-06-29 12:26:24 +0000

    released at Fri Jun 29 12:26:24 UTC 2018, commit: d83ad63bc46c7eaf6c33eb277a86d3a8c3d512e8

--- modules/Storage/index.js
+++ modules/Storage/index.js
@@ -5,6 +5,14 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = undefined;
 
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
 var _extends2 = require('babel-runtime/helpers/extends');
 
 var _extends3 = _interopRequireDefault(_extends2);
@@ -51,7 +59,7 @@ var _moduleStatuses2 = _interopRequireDefault(_moduleStatuses);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var DEFAULT_ALLOW_INACTIVE_TABS_WRITE = false;
+var DEFAULT_DISABLE_ALLOW_INACTIVE_TABS_WRITE = false;
 
 /**
  * @class
@@ -72,7 +80,7 @@ var Storage = (_dec = (0, _di.Module)({
    */
   function Storage(_ref) {
     var _ref$disableAllowInac = _ref.disableAllowInactiveTabsWrite,
-        disableAllowInactiveTabsWrite = _ref$disableAllowInac === undefined ? DEFAULT_ALLOW_INACTIVE_TABS_WRITE : _ref$disableAllowInac,
+        disableAllowInactiveTabsWrite = _ref$disableAllowInac === undefined ? DEFAULT_DISABLE_ALLOW_INACTIVE_TABS_WRITE : _ref$disableAllowInac,
         auth = _ref.auth,
         tabManager = _ref.tabManager,
         options = (0, _objectWithoutProperties3.default)(_ref, ['disableAllowInactiveTabsWrite', 'auth', 'tabManager']);
@@ -94,65 +102,112 @@ var Storage = (_dec = (0, _di.Module)({
       var _this2 = this;
 
       var storedData = null;
-      this.store.subscribe(function () {
-        if (_this2._auth.loginStatus === _loginStatus2.default.loggedIn && !_this2.ready) {
-          var storageKey = (_this2.prefix ? _this2.prefix + '-' : '') + 'storage-' + _this2._auth.ownerId;
-          _this2._storage = new _this2._StorageProvider({
-            storageKey: storageKey
-          });
-          storedData = _this2._storage.getData();
-          for (var key in storedData) {
-            if (!_this2._reducers[key]) {
-              delete storedData[key];
-              _this2._storage.removeItem(key);
-            }
-          }
-          _this2.store.dispatch({
-            type: _this2.actionTypes.initSuccess,
-            storageKey: storageKey,
-            data: storedData
-          });
-          _this2._storageHandler = function (_ref2) {
-            var key = _ref2.key,
-                value = _ref2.value;
+      this.store.subscribe((0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee() {
+        var storageKey, key, currentData, _key;
 
-            if (_this2.ready) {
-              storedData[key] = value;
-              _this2.store.dispatch({
-                type: _this2.actionTypes.sync,
-                key: key,
-                value: value
-              });
+        return _regenerator2.default.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                if (!(_this2._auth.loginStatus === _loginStatus2.default.loggedIn && !_this2.ready)) {
+                  _context.next = 20;
+                  break;
+                }
+
+                storageKey = (_this2.prefix ? _this2.prefix + '-' : '') + 'storage-' + _this2._auth.ownerId;
+
+                _this2._storage = new _this2._StorageProvider({
+                  storageKey: storageKey
+                });
+                _context.next = 5;
+                return _this2._storage.getData();
+
+              case 5:
+                storedData = _context.sent;
+                _context.t0 = _regenerator2.default.keys(storedData);
+
+              case 7:
+                if ((_context.t1 = _context.t0()).done) {
+                  _context.next = 15;
+                  break;
+                }
+
+                key = _context.t1.value;
+
+                if (_this2._reducers[key]) {
+                  _context.next = 13;
+                  break;
+                }
+
+                delete storedData[key];
+                _context.next = 13;
+                return _this2._storage.removeItem(key);
+
+              case 13:
+                _context.next = 7;
+                break;
+
+              case 15:
+                _this2.store.dispatch({
+                  type: _this2.actionTypes.initSuccess,
+                  storageKey: storageKey,
+                  data: storedData
+                });
+                _this2._storageHandler = function (_ref3) {
+                  var key = _ref3.key,
+                      value = _ref3.value;
+
+                  if (_this2.ready) {
+                    storedData[key] = value;
+                    _this2.store.dispatch({
+                      type: _this2.actionTypes.sync,
+                      key: key,
+                      value: value
+                    });
+                  }
+                };
+                _this2._storage.on('storage', _this2._storageHandler);
+                _context.next = 21;
+                break;
+
+              case 20:
+                if (_this2._auth.loginStatus === _loginStatus2.default.notLoggedIn && _this2.ready) {
+                  _this2.store.dispatch({
+                    type: _this2.actionTypes.reset
+                  });
+                  if (_this2._storageHandler) {
+                    _this2._storage.off('storage', _this2._storageHandler);
+                    _this2._storageHandler = null;
+                  }
+                  if (_this2._storage) {
+                    _this2._storage.destroy();
+                    _this2._storage = null;
+                  }
+                  _this2.store.dispatch({
+                    type: _this2.actionTypes.resetSuccess
+                  });
+                }
+
+              case 21:
+                if (_this2.status === _moduleStatuses2.default.ready && (!_this2._disableAllowInactiveTabsWrite || !_this2._tabManager || _this2._tabManager.active)) {
+                  // save new data to storage when changed
+                  currentData = _this2.data;
+
+                  for (_key in currentData) {
+                    if (storedData[_key] !== currentData[_key]) {
+                      _this2._storage.setItem(_key, currentData[_key]);
+                      storedData[_key] = currentData[_key];
+                    }
+                  }
+                }
+
+              case 22:
+              case 'end':
+                return _context.stop();
             }
-          };
-          _this2._storage.on('storage', _this2._storageHandler);
-        } else if (_this2._auth.loginStatus === _loginStatus2.default.notLoggedIn && _this2.ready) {
-          _this2.store.dispatch({
-            type: _this2.actionTypes.reset
-          });
-          if (_this2._storageHandler) {
-            _this2._storage.off('storage', _this2._storageHandler);
-            _this2._storageHandler = null;
           }
-          if (_this2._storage) {
-            _this2._storage.destroy();
-            _this2._storage = null;
-          }
-          _this2.store.dispatch({
-            type: _this2.actionTypes.resetSuccess
-          });
-        }
-        if (_this2.status === _moduleStatuses2.default.ready && (!_this2._disableAllowInactiveTabsWrite || !_this2._tabManager || _this2._tabManager.active)) {
-          // save new data to storage when changed
-          var currentData = _this2.data;
-          for (var _key in currentData) {
-            if (storedData[_key] !== currentData[_key]) {
-              _this2._storage.setItem(_key, currentData[_key]);
-              storedData[_key] = currentData[_key];
-            }
-          }
-        }
-      });
+        }, _callee, _this2);
+      })));
     }
   }]);
   return Storage;
