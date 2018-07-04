31daa3d6bb2f6e599fd4bbe14d45070c2a3e43e6
RingCentral Integrations Team <integrations@ringcentral.com>
2018-07-04 07:44:18 +0000

    released at Wed Jul  4 07:44:18 UTC 2018, commit: deaae99585431888cb47823df0519feb09afd187

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.formatTypes = exports.parse = exports.detect = exports.format = undefined;
+exports.isSameLocalNumber = exports.isValidNumber = exports.formatTypes = exports.parse = exports.detect = exports.format = undefined;
+
+var _libphonenumberJs = require('libphonenumber-js');
 
 var _format = require('./lib/format');
 
@@ -17,10 +19,16 @@ var _parse = require('./lib/parse');
 
 var _parse2 = _interopRequireDefault(_parse);
 
+var _isSameLocalNumber = require('./lib/isSameLocalNumber');
+
+var _isSameLocalNumber2 = _interopRequireDefault(_isSameLocalNumber);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.format = _format2.default;
 exports.detect = _detect2.default;
 exports.parse = _parse2.default;
 exports.formatTypes = _format.formatTypes;
+exports.isValidNumber = _libphonenumberJs.isValidNumber;
+exports.isSameLocalNumber = _isSameLocalNumber2.default;
 //# sourceMappingURL=index.js.map
