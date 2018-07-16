af1163135c58790b1b3b5f438195f8b4f6d25f75
Rulee Chen <rulee@live.cn>
2018-07-16 23:54:22 +0800

    1) optimize matching between calls and sessions 2) implement the new "Current Call" definition

--- packages/ringcentral-integration/modules/CallMonitor/getCallMonitorReducer.js
+++ packages/ringcentral-integration/modules/CallMonitor/getCallMonitorReducer.js
@@ -13,9 +13,36 @@ export function getCallMatchedReducer(types) {
     return state;
   };
 }
+
+/**
+ * HACK: for conference call merging glitch
+ */
+export function getCachedCallsFromPresenceReducer(types) {
+  return (state = null, { type, call }) => {
+    switch (type) {
+      case types.updateCallsCaching:
+        if (Array.isArray(state)) {
+          if (state.find(cachedCall => cachedCall.id === call.id)) {
+            return state;
+          }
+          call.cached = true;
+          return [...state, call];
+        }
+        call.cached = true;
+        return [call];
+      case types.clearCallsCaching:
+      case types.reset:
+        return null;
+      default:
+        return state;
+    }
+  };
+}
+
 /* istanbul ignore next: unnecessary to test getModuleStatusReducer */
 export default function getCallMonitorReducer(types) {
   return combineReducers({
     status: getModuleStatusReducer(types),
+    cachedCallsFromPresence: getCachedCallsFromPresenceReducer(types),
   });
 }
