0ba7a666ec23667f462f4175928f459cb598352c
Rulee Chen <rulee@live.cn>
2018-07-19 15:27:52 +0800

    code formal

--- packages/ringcentral-widgets/components/ActiveCallPanel/MergeInfo.js
+++ packages/ringcentral-widgets/components/ActiveCallPanel/MergeInfo.js
@@ -6,24 +6,32 @@ import i18n from './i18n';
 import CallAvatar from '../CallAvatar';
 
 function MergeInfo({
-  timeCounter, currentCall, currentLocale, lastTo
+  timeCounter,
+  currentCall,
+  currentLocale,
+  lastTo,
 }) {
-  const isConference = lastTo && lastTo.calleeType === calleeTypes.conference ? i18n.getString('conferenceCall', currentLocale) : i18n.getString('unknow', currentLocale);
+  const isConference = (lastTo && lastTo.calleeType === calleeTypes.conference)
+    ? i18n.getString('conferenceCall', currentLocale)
+    : i18n.getString('unknow', currentLocale);
   return lastTo ? (
     <div className={styles.mergeInfo}>
       <div className={styles.merge_item}>
         <div className={styles.callee_avatar}>
           {
             lastTo.calleeType === calleeTypes.conference
-            ? <CallAvatar
-              avatarUrl={lastTo.avatarUrl}
-              extraNum={lastTo.extraNum}
-              isOnConferenceCall />
-            : <CallAvatar avatarUrl={lastTo.avatarUrl} />
+              ? <CallAvatar
+                avatarUrl={lastTo.avatarUrl}
+                extraNum={lastTo.extraNum}
+                isOnConferenceCall
+              />
+              : <CallAvatar
+                avatarUrl={lastTo.avatarUrl}
+              />
           }
         </div>
         <div className={styles.callee_name}>
-          { lastTo.calleeType === calleeTypes.contacts ? lastTo.name : isConference }
+          {lastTo.calleeType === calleeTypes.contacts ? lastTo.name : isConference}
         </div>
         <div className={styles.callee_status}>
           {i18n.getString('onHold', currentLocale)}
@@ -31,15 +39,15 @@ function MergeInfo({
       </div>
       <div className={styles.merge_item_active}>
         <div className={styles.callee_avatar_active} >
-          { !currentCall.nameMatches.length || !currentCall.avatarUrl
+          {!currentCall.nameMatches.length || !currentCall.avatarUrl
             ? <CallAvatar avatarUrl={null} />
             : <CallAvatar avatarUrl={currentCall.avatarUrl} />
           }
         </div>
         <div className={styles.callee_name_active}>
-          { currentCall.nameMatches.length
-              ? currentCall.nameMatches[0].name
-              : currentCall.fallBackName
+          {currentCall.nameMatches.length
+            ? currentCall.nameMatches[0].name
+            : currentCall.fallBackName
           }
         </div>
         <div className={styles.callee_status_active}>
@@ -49,15 +57,18 @@ function MergeInfo({
     </div>
   ) : (<span />);
 }
+
 MergeInfo.propTypes = {
-  timeCounter: PropTypes.element,
-  currentCall: PropTypes.object,
-  currentLocale: PropTypes.string,
-  lastTo: PropTypes.object
+  currentLocale: PropTypes.string.isRequired,
+  currentCall: PropTypes.object.isRequired,
+  timeCounter: PropTypes.element.isRequired,
+  lastTo: PropTypes.object,
 };
+
 MergeInfo.defaultProps = {
   lastTo: {
-    calleeTypes: 'unknow'
-  }
+    calleeTypes: 'unknow',
+  },
 };
+
 export default MergeInfo;
