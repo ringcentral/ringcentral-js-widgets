3f6f471c24b998209770b2968006ec2fa8b3e4b8
Rulee Chen <rulee@live.cn>
2018-07-20 16:11:57 +0800

    include 3way calling little updates (#918)

--- packages/ringcentral-widgets/components/ActiveCallsPanel/i18n/en-US.js
+++ packages/ringcentral-widgets/components/ActiveCallsPanel/i18n/en-US.js
@@ -4,4 +4,8 @@ export default {
   ringCall: 'Ringing Call',
   onHoldCall: 'Call on Hold',
   otherDeviceCall: 'Ongoing calls on my other devices',
+  confirmation: 'Confirmation',
+  confirmMergeToConference: 'Add to conference call?',
+  mergeToConference: 'Merge',
+  conferenceCall: 'Conference Call',
 };
