760b061fd457964233f239365bb0aa344ef9c097
Lex. Huang <531971909@qq.com>
2018-07-24 12:32:29 +0800

    refactor(CallCtrlPage, dev-server): remove router.replace() when merging
    On file CllCtrlPage/index.js, we used "routerInteraction.replace(`${routerInteraction.currentPath}/${sessionId}`);" to avoid flickering when merging. now the issue has gone, so remove it.
    
    BREAKING CHANGE:
    * Phone.js: indexOf->=== for conferece call routers
    * App:
      * /calls/active(/:sessionId) -> /calls/active
      * /conferenceCall/mergeCtrl(/:sessionId) -> /conferenceCall/mergeCtrl

--- packages/ringcentral-widgets-demo/dev-server/containers/App/index.js
+++ packages/ringcentral-widgets-demo/dev-server/containers/App/index.js
@@ -157,7 +157,7 @@ export default function App({
                   </DialerAndCallsTabContainer>
                 )} />
               <Route
-                path="/calls/active(/:sessionId)"
+                path="/calls/active"
                 component={() => (
                   <CallCtrlPage
                     showContactDisplayPlaceholder={false}
@@ -269,7 +269,7 @@ export default function App({
                     }} />
                 )} />
               <Route
-                path="/conferenceCall/mergeCtrl(/:sessionId)"
+                path="/conferenceCall/mergeCtrl"
                 component={() => (
                   <ConferenceCallMergeCtrlPage
                     showContactDisplayPlaceholder={false}
