d82ca89a205fa2064bde7a557c86a3ebbe60ef72
JackZong <jinzhanglin@hotmail.com>
2018-08-02 12:29:45 +0800

    Rcint 7871 (#968)
    feat(Conference, Webphone): RCINT-7871:Cannot add people to call while the call is being recorded

---
+++ packages/ringcentral-widgets/components/AddCallAlert/i18n/en-US.js
@@ -0,0 +1,5 @@
+import recordStatus from 'ringcentral-integration/modules/Webphone/recordStatus';
+
+export default {
+  [recordStatus.recording]: 'Call recording in progress. Please stop recording and try again.',
+};
