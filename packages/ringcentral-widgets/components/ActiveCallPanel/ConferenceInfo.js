afc9ff5fbcdd03eed817a61c29c6456fa7e3cb2f
Lex. Huang <531971909@qq.com>
2018-08-02 17:26:05 +0800

    refactor(ActiveCallPanel): use <CallAvatar/> as conference info
    using <CallAvatar/> component instead of DIV and background image

--- packages/ringcentral-widgets/components/ActiveCallPanel/ConferenceInfo.js
+++ packages/ringcentral-widgets/components/ActiveCallPanel/ConferenceInfo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import CallAvatar from '../CallAvatar';
 import dynamicsFont from '../../assets/DynamicsFont/DynamicsFont.scss';
 import styles from './styles.scss';
 import i18n from './i18n';
@@ -36,14 +37,8 @@ function ConferenceInfo({
                 displayedProfiles.map(({ avatarUrl, toUserName }, idx) => (
                   <div
                     key={`${toUserName}_${idx}`}
-                    className={styles.avatar}
-                    style={avatarUrl
-                      ? { backgroundImage: `url(${avatarUrl})` }
-                      : { backgroundColor: '#fff' }
-                    }>
-                    {avatarUrl
-                      ? null
-                      : <i className={classnames(dynamicsFont.portrait, styles.icon)} />}
+                    className={styles.avatar}>
+                    <CallAvatar avatarUrl={avatarUrl} />
                   </div>
                 )
                 )
