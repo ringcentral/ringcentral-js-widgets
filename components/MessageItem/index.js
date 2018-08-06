4d4f381e4a262ada603b2b5a002341c4519877f2
RingCentral Integrations Team <integrations@ringcentral.com>
2018-08-06 07:37:09 +0000

    released at Mon Aug  6 07:37:09 UTC 2018, commit: 39f01fdb887686ea45150cee5eb254f1b693b697

--- components/MessageItem/index.js
+++ components/MessageItem/index.js
@@ -635,7 +635,7 @@ var MessageItem = function (_Component) {
             onLog: isVoicemail || isFax || extraButton ? undefined : onLogConversation && this.logConversation,
             onViewEntity: onViewContact && this.viewSelectedContact,
             onCreateEntity: onCreateContact && this.createSelectedContact,
-            hasEntity: correspondents.length === 1 && !!correspondentMatches.length,
+            hasEntity: correspondents.length === 1 && !!correspondentMatches.length && this.state.selected >= 0,
             onClickToDial: !isFax ? onClickToDial && this.clickToDial : undefined,
             onClickToSms: isVoicemail ? onClickToSms && this.onClickToSms : undefined,
             phoneNumber: phoneNumber,
