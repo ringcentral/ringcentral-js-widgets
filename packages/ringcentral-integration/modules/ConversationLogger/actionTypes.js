5c29e3ebebd95cadaf5aad95396aeb3c1916fae5
Harry.Hou <harry.hou@ringcentral.com>
2018-08-07 14:47:14 +0800

    ConversationLogger/Add action to track event for analytics

--- packages/ringcentral-integration/modules/ConversationLogger/actionTypes.js
+++ packages/ringcentral-integration/modules/ConversationLogger/actionTypes.js
@@ -4,4 +4,5 @@ import baseActionTypes from '../../lib/LoggerBase/baseActionTypes';
 export default new Enum([
   ...Object.keys(baseActionTypes),
   'setAutoLog',
+  'logConversation',
 ], 'conversationLogger');
