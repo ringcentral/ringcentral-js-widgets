f9633b655e8c163d736a2a1eeba05e1787744127
Lex. Huang <531971909@qq.com>
2018-08-08 14:42:31 +0800

    docs(ConferenceCall): Add comments for `getMergingPairReducer`
    Clarify the design purpose

--- packages/ringcentral-integration/modules/ConferenceCall/getConferenceCallReducer.js
+++ packages/ringcentral-integration/modules/ConferenceCall/getConferenceCallReducer.js
@@ -89,6 +89,12 @@ export function getMergingStatusReducer(types) {
   };
 }
 
+/**
+ * interface MergingPairState = {fromSessionId:string, toSessionId:string}
+ *
+ * The `from` and `to` is relative to the [adding call](https://app.zeplin.io/project/59df2e4346294d03f96d15a9/screen/5b2c64f7db2860b90ddd5939) flow
+ * which is in [RCINT-7378](https://jira.ringcentral.com/browse/RCINT-7378)
+ */
 export function getMergingPairReducer(types) {
   return (state = {}, { type, fromSessionId, toSessionId }) => {
     switch (type) {
