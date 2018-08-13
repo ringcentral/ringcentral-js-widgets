b38bd2a6237407983c0c23e1834b748249231059
Lex. Huang <531971909@qq.com>
2018-08-13 14:42:45 +0800

    feat(components): Add <MediaObject/> (#1058)
    * feat(components): Add <MediaObject/>
    
    https://getbootstrap.com/docs/3.3/components/#media
    
    BREAKING CHANGE: refactoring ParticipantItem.js using new component
    
    * refactor(ParticipantItem.js): code alignment
    
    Line 26 exceeds the maximum line length of 100. (max-len)
    
    * feat(ConferenceParticipantPanel, MediaObject): add `flexible` prop
    
    Using flex layout or not

--- packages/ringcentral-widgets/components/ConferenceParticipantPanel/ParticipantItem.js
+++ packages/ringcentral-widgets/components/ConferenceParticipantPanel/ParticipantItem.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import i18n from './i18n';
 import styles from './styles.scss';
 import CallAvatar from '../CallAvatar';
+import MediaObject from '../MediaObject';
 import CircleButton from '../CircleButton';
 import EndIcon from '../../assets/images/End.svg';
 
@@ -19,25 +20,33 @@ function ParticipantItem({
   currentLocale,
 }) {
   return (
-    <div className={styles.participantItem}>
-      <div className={styles.avatar}>
-        <CallAvatar isOnConferenceCall={false} avatarUrl={avatarUrl} />
-      </div>
-      <div className={styles.detail}>{detail}</div>
-      <span title={i18n.getString('removeParticipant', currentLocale)} className={styles.webphoneButton}>
-        <CircleButton
-          className={styles.rejectButton}
-          onClick={(e) => {
-            e.stopPropagation();
-            onRemove();
-          }}
-          iconWidth={260}
-          iconX={120}
-          icon={EndIcon}
-          showBorder={false}
-        />
-      </span>
-    </div>
+    <MediaObject
+      leftAlignment="middle"
+      containerCls={styles.participantItem}
+      mediaLeft={
+        <div className={styles.avatar}>
+          <CallAvatar isOnConferenceCall={false} avatarUrl={avatarUrl} />
+        </div>
+      }
+      mediaBody={detail}
+      bodyCls={styles.detail}
+      rightAlignment="middle"
+      mediaRight={
+        <span title={i18n.getString('removeParticipant', currentLocale)} className={styles.webphoneButton}>
+          <CircleButton
+            className={styles.rejectButton}
+            onClick={(e) => {
+              e.stopPropagation();
+              onRemove();
+            }}
+            iconWidth={260}
+            iconX={120}
+            icon={EndIcon}
+            showBorder={false}
+          />
+        </span>
+     }
+    />
   );
 }
 
