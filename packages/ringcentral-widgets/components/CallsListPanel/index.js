4b9e4eb894c26a78c357188a9b7c4b91ff929047
Bruce Li <bruce.li@lmxmn208.rcoffice.ringcentral.com>
2018-08-14 10:11:28 +0800

    remove Ramda import

--- packages/ringcentral-widgets/components/CallsListPanel/index.js
+++ packages/ringcentral-widgets/components/CallsListPanel/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import * as R from 'ramda';
 import SpinnerOverlay from '../SpinnerOverlay';
 import ActiveCallItem from '../ActiveCallItem';
 import CallList from '../CallList';
