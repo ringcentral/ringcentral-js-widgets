c73a001e4ffdd7d448201896608739202f106270
MicleMing <fzulanming@gmail.com>
2018-08-15 12:48:47 +0800

    feat: add decorator and webpack loader

--- packages/ringcentral-widgets-library/app/.storybook/config.js
+++ packages/ringcentral-widgets-library/app/.storybook/config.js
@@ -1,7 +1,20 @@
-import { configure } from '@storybook/react';
+/**
+ * @file storybook config
+ */
+
+import { configure, addDecorator, getStorybook } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import centered from '@storybook/addon-centered'
+import { withKnobs } from '@storybook/addon-knobs'
 
 const req = require.context('../stories', true, /.story.js$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  addDecorator((story, context) => withInfo()(story)(context))
+  addDecorator(centered)
+  addDecorator(withKnobs)
+  req.keys().forEach((filename) => req(filename));
 }
-configure(loadStories, module);
+
+configure(loadStories, module)
+
+export { getStorybook }
