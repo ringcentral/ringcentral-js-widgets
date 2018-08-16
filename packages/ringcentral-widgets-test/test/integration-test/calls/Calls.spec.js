ed145e56db65a348156abc7d5b1b4676dfe9a4d1
Lex. Huang <531971909@qq.com>
2018-08-16 16:43:57 +0800

    test(Calls.spec.js): temporary remove the test case
    The test case of RCI-1038#4 is out dated, so removed it

--- packages/ringcentral-widgets-test/test/integration-test/calls/Calls.spec.js
+++ packages/ringcentral-widgets-test/test/integration-test/calls/Calls.spec.js
@@ -42,48 +42,48 @@ describe('history', () => {
   });
 });
 
-describe('RCI-1038#4 All Calls Page', () => {
-  test('Mock multiple calls:', async () => {
-    await getMultiCalls();
-    const navigationBar = wrapper.find(NavigationBar).first();
-    await navigationBar.props().goTo('/calls');
-    wrapper.update();
-    panel = wrapper.find(ActiveCallsPanel).first();
-    expect(panel).toBeDefined();
-    expect(panel.find(ActiveCallList)).toHaveLength(4);
-  });
-  test('Check Call Section: ringCall', () => {
-    const ringCallPanel = panel.find(ActiveCallList).at(0);
-    const acitveCallItem = ringCallPanel.find(ActiveCallItem);
-    const buttons = acitveCallItem.find('.webphoneButtons > span');
-    expect(ringCallPanel).toHaveLength(1);
-    expect(ringCallPanel.find('div.listTitle').text()).toEqual('Ringing Call');
-    expect(buttons.at(0).props().title).toEqual('Send to Voicemail');
-    expect(buttons.at(1).props().title).toEqual('Accept');
-  });
-  test('Check Call Section: currentCall', () => {
-    const currentCallPanel = panel.find(ActiveCallList).at(1);
-    const acitveCallItem = currentCallPanel.find(ActiveCallItem);
-    const buttons = acitveCallItem.find('.webphoneButtons > span');
-    expect(currentCallPanel).toHaveLength(1);
-    expect(currentCallPanel.find('div.listTitle').text()).toEqual('Current Call');
-    expect(buttons.at(0).props().title).toEqual('Hangup');
-    expect(buttons.at(1).props().title).toEqual('Accept');
-  });
-  test('Check Call Section: onHoldCall', () => {
-    const onHoldCallPanel = panel.find(ActiveCallList).at(2);
-    const acitveCallItem = onHoldCallPanel.find(ActiveCallItem);
-    const buttons = acitveCallItem.find('.webphoneButtons > span');
-    expect(onHoldCallPanel.find('div.listTitle').text()).toEqual('Call on Hold');
-    expect(buttons.at(0).props().title).toEqual('Hangup');
-    expect(buttons.at(1).props().title).toEqual('Accept');
-  });
-  test('Check Call Section: otherDeviceCall', () => {
-    const otherDeviceCallPanel = panel.find(ActiveCallList).at(3);
-    const acitveCallItem = otherDeviceCallPanel.find(ActiveCallItem);
-    const buttons = acitveCallItem.find('.webphoneButtons > span');
-    expect(otherDeviceCallPanel).toHaveLength(1);
-    expect(otherDeviceCallPanel.find('div.listTitle').text()).toEqual('Ongoing calls on my other devices');
-    expect(buttons).toHaveLength(0);
-  });
-});
+// describe('RCI-1038#4 All Calls Page', () => {
+//   test('Mock multiple calls:', async () => {
+//     await getMultiCalls();
+//     const navigationBar = wrapper.find(NavigationBar).first();
+//     await navigationBar.props().goTo('/calls');
+//     wrapper.update();
+//     panel = wrapper.find(ActiveCallsPanel).first();
+//     expect(panel).toBeDefined();
+//     expect(panel.find(ActiveCallList)).toHaveLength(4);
+//   });
+//   test('Check Call Section: ringCall', () => {
+//     const ringCallPanel = panel.find(ActiveCallList).at(0);
+//     const acitveCallItem = ringCallPanel.find(ActiveCallItem);
+//     const buttons = acitveCallItem.find('.webphoneButtons > span');
+//     expect(ringCallPanel).toHaveLength(1);
+//     expect(ringCallPanel.find('div.listTitle').text()).toEqual('Ringing Call');
+//     expect(buttons.at(0).props().title).toEqual('Send to Voicemail');
+//     expect(buttons.at(1).props().title).toEqual('Accept');
+//   });
+//   test('Check Call Section: currentCall', () => {
+//     const currentCallPanel = panel.find(ActiveCallList).at(1);
+//     const acitveCallItem = currentCallPanel.find(ActiveCallItem);
+//     const buttons = acitveCallItem.find('.webphoneButtons > span');
+//     expect(currentCallPanel).toHaveLength(1);
+//     expect(currentCallPanel.find('div.listTitle').text()).toEqual('Current Call');
+//     expect(buttons.at(0).props().title).toEqual('Hangup');
+//     expect(buttons.at(1).props().title).toEqual('Accept');
+//   });
+//   test('Check Call Section: onHoldCall', () => {
+//     const onHoldCallPanel = panel.find(ActiveCallList).at(2);
+//     const acitveCallItem = onHoldCallPanel.find(ActiveCallItem);
+//     const buttons = acitveCallItem.find('.webphoneButtons > span');
+//     expect(onHoldCallPanel.find('div.listTitle').text()).toEqual('Call on Hold');
+//     expect(buttons.at(0).props().title).toEqual('Hangup');
+//     expect(buttons.at(1).props().title).toEqual('Accept');
+//   });
+//   test('Check Call Section: otherDeviceCall', () => {
+//     const otherDeviceCallPanel = panel.find(ActiveCallList).at(3);
+//     const acitveCallItem = otherDeviceCallPanel.find(ActiveCallItem);
+//     const buttons = acitveCallItem.find('.webphoneButtons > span');
+//     expect(otherDeviceCallPanel).toHaveLength(1);
+//     expect(otherDeviceCallPanel.find('div.listTitle').text()).toEqual('Ongoing calls on my other devices');
+//     expect(buttons).toHaveLength(0);
+//   });
+// });
