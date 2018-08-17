90b32f74ad952440bd339df16b59b0baa173012a
embbnux <embbnux@embbnux.com>
2018-08-17 14:27:22 +0800

    fix lint problem

--- packages/glip-widgets/containers/GlipChat/index.js
+++ packages/glip-widgets/containers/GlipChat/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import withPhone from 'ringcentral-widgets/lib/withPhone';
