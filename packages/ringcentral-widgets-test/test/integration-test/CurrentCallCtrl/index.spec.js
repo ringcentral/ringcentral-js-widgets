d563f6a29f5339481aeb1307dc96eb0f552d8a87
Joy Zhang <mint.soda@hotmail.com>
2018-08-22 17:25:04 +0800

    feat add test for RCI-1712647 Current Call Control Page - Hold/Unhold

--- packages/ringcentral-widgets-test/test/integration-test/CurrentCallCtrl/index.spec.js
+++ packages/ringcentral-widgets-test/test/integration-test/CurrentCallCtrl/index.spec.js
@@ -1,10 +1,11 @@
+import ActiveCallPad, { MoreActionItem } from 'ringcentral-widgets/components/ActiveCallPad';
 import ActiveCallDialPad from 'ringcentral-widgets/components/ActiveCallDialPad';
 import ActiveCallButton from 'ringcentral-widgets/components/ActiveCallButton';
 import IncomingCallPad from 'ringcentral-widgets/components/IncomingCallPad';
-import ActiveCallPad from 'ringcentral-widgets/components/ActiveCallPad';
 import CircleButton from 'ringcentral-widgets/components/CircleButton';
 import { HeaderButton } from 'ringcentral-widgets/components/Header';
 import DialButton from 'ringcentral-widgets/components/DialButton';
+import Tooltip from 'ringcentral-widgets/components/Tooltip';
 import * as mock from 'ringcentral-integration/integration-test/mock';
 import deviceBody from './data/device';
 import { getWrapper, timeout } from '../shared';
@@ -43,8 +44,9 @@ afterEach(() => {
 
 async function makeOutboundCall() {
   mock.device(deviceBody);
-  await makeCall(phone);
+  const session = await makeCall(phone);
   wrapper.update();
+  return session;
 }
 
 async function makeInbountCall(sessionId, answerIt = false) {
@@ -156,3 +158,153 @@ describe('RCI-1712646 Current Call Control Page - Keypad', () => {
   });
 });
 
+describe('RCI-1712647 Current Call Control Page - Hold/Unhold', () => {
+  let holdButton = null;
+
+  test('Answer an inbound call and keep in active call page, click Hold/Unhold', async () => {
+    await makeInbountCall(sid111, true);
+    holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+    expect(holdButton.find('.buttonTitle').text()).toEqual('Hold');
+    // Click Hold Button
+    holdButton.find(CircleButton).simulate('click');
+    holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+    // TODO: Flip button
+    expect(holdButton.find('.buttonTitle').text()).toEqual('On Hold');
+    expect(holdFn.mock.calls[0]).toEqual([sid111]);
+    // Unhold button
+    holdButton.find(CircleButton).simulate('click');
+    holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+    // TODO: Flip button
+    expect(holdButton.find('.buttonTitle').text()).toEqual('Hold');
+    expect(unholdFn.mock.calls[0]).toEqual([sid111]);
+  });
+  test('Make an outbound call and keep in active call page, click Hold/Unhold', async () => {
+    const outboundSession = await makeOutboundCall();
+    holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+    expect(holdButton.find('.buttonTitle').text()).toEqual('Hold');
+    // Click Hold Button
+    holdButton.find(CircleButton).simulate('click');
+    holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+    // TODO: Flip button
+    expect(holdButton.find('.buttonTitle').text()).toEqual('On Hold');
+    expect(holdFn.mock.calls[0]).toEqual([outboundSession.id]);
+    // Unhold button
+    holdButton.find(CircleButton).simulate('click');
+    holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+    // TODO: Flip button
+    expect(holdButton.find('.buttonTitle').text()).toEqual('Hold');
+    expect(unholdFn.mock.calls[0]).toEqual([outboundSession.id]);
+  });
+});
+
+describe('Current Call Control Page - Mute/Unmute', () => {
+  let holdButton = null;
+  let muteButton = null;
+  test('Answer an inbound call then user hold the call, Mute/Unmute should be disabled',
+    async () => {
+      await makeInbountCall(sid111, true);
+      // Click Hold Button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      muteButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(0);
+      expect(muteButton.props().disabled).toBe(true);
+      // Unhold button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      muteButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(0);
+      expect(muteButton.props().disabled).toBe(false);
+    }
+  );
+  test('Make an outbound call then user hold the call, Mute/Unmute should be disabled',
+    async () => {
+      await makeOutboundCall();
+      // Click Hold Button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      muteButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(0);
+      expect(muteButton.props().disabled).toBe(true);
+      // Unhold button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      muteButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(0);
+      expect(muteButton.props().disabled).toBe(false);
+    }
+  );
+});
+
+describe('Current Call Control Page - Record', () => {
+  let holdButton = null;
+  let recordButton = null;
+  test('RCI-1712647 Answer an inbound call then user hold the call, Record should be disabled',
+    async () => {
+      await makeInbountCall(sid111, true);
+      // Click Hold Button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      recordButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(4);
+      expect(recordButton.props().disabled).toBe(true);
+      // Unhold button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      recordButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(4);
+      expect(recordButton.props().disabled).toBe(false);
+    }
+  );
+  test('RCI-1712647 Make an outbound call then user hold the call, Record should be disabled',
+    async () => {
+      await makeOutboundCall();
+      // Click Hold Button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      recordButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(4);
+      expect(recordButton.props().disabled).toBe(true);
+      // Unhold button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      recordButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(4);
+      expect(recordButton.props().disabled).toBe(false);
+    }
+  );
+});
+
+describe('Current Call Control Page - Flip', () => {
+  let holdButton = null;
+  let flipButton = null;
+  function getFlipButton() {
+    const moreButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(5);
+    moreButton.find(CircleButton).simulate('click');
+    const flipButton = wrapper.find(ActiveCallPad).find(Tooltip).find(MoreActionItem).at(1);
+    return flipButton;
+  }
+
+  test('RCI-1712647 Answer an inbound call then user hold the call, Flip should be disabled',
+    async () => {
+      await makeInbountCall(sid111, true);
+      // Click Hold Button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      flipButton = getFlipButton();
+      expect(flipButton.props().disabled).toBe(true);
+      // Unhold button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      flipButton = getFlipButton();
+      expect(flipButton.props().disabled).toBe(false);
+    }
+  );
+  test('RCI-1712647 Make an outbound call then user hold the call, Flip should be disabled',
+    async () => {
+      await makeOutboundCall();
+      // Click Hold Button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      flipButton = getFlipButton();
+      expect(flipButton.props().disabled).toBe(true);
+      // Unhold button
+      holdButton = wrapper.find(ActiveCallPad).find(ActiveCallButton).at(2);
+      holdButton.find(CircleButton).simulate('click');
+      flipButton = getFlipButton();
+      expect(flipButton.props().disabled).toBe(false);
+    }
+  );
+});
