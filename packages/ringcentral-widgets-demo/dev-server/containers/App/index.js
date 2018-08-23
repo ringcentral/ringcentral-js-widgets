01596c0e06d976b809cf034eef966e3a4650511e
Rukai Yu <rukai.yu@ringcentral.com>
2018-08-23 16:54:09 +0800

    chore(App): remove no necessary component import

--- packages/ringcentral-widgets-demo/dev-server/containers/App/index.js
+++ packages/ringcentral-widgets-demo/dev-server/containers/App/index.js
@@ -28,7 +28,6 @@ import ContactDetailsPage from 'ringcentral-widgets/containers/ContactDetailsPag
 import FeedbackPage from 'ringcentral-widgets/containers/FeedbackPage';
 import UserGuidePage from 'ringcentral-widgets/containers/UserGuidePage';
 import ConferenceCallDialerPage from 'ringcentral-widgets/containers/ConferenceCallDialerPage';
-import ConferenceCallMergeCtrlPage from 'ringcentral-widgets/containers/ConferenceCallMergeCtrlPage';
 import CallsOnholdPage from 'ringcentral-widgets/containers/CallsOnholdPage';
 import DialerAndCallsTabContainer from 'ringcentral-widgets/containers/DialerAndCallsTabContainer';
 import ConferenceParticipantPage from 'ringcentral-widgets/containers/ConferenceParticipantPage';
