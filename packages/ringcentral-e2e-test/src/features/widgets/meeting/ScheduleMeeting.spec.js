36ecddb5fa6b04592fcad097a9c7ab2e96fc6c0c
Michael Lin <unadlib@gmail.com>
2018-08-24 12:13:36 +0800

    update

--- packages/ringcentral-e2e-test/src/features/widgets/meeting/ScheduleMeeting.spec.js
+++ packages/ringcentral-e2e-test/src/features/widgets/meeting/ScheduleMeeting.spec.js
@@ -14,4 +14,19 @@ describe('test: =====>', () => {
     const text = await context.text(option.selector);
     expect(text).toBe(option.expected);
   });
+  // test({
+  //   title: 'Login button ${selector} text is ${expected}',
+  //   tags: [
+  //     ['google', { brands: ['rc', 'att'] }]
+  //   ],
+  //   level: 'p1',
+  //   options: [
+  //     { selector: '[data-sign="loginButton"]', expected: 'Sign In' },
+  //   ],
+  // }, async ({
+  //   option, context, tag, level
+  // }) => {
+  //   const text = await context.text(option.selector);
+  //   expect(text).toBe(option.expected);
+  // });
 });
--- packages/ringcentral-e2e-test/temp.js
+++ packages/ringcentral-e2e-test/temp.js
@@ -2,16 +2,17 @@ import runner from './src/runner';
 
 runner({
   inputTesterConfig: {
+    verbose: true,
     // testMatch: ['<rootDir>/packages/ringcentral-e2e-test/src/features/**/*.js'],
     testRegex: './packages/ringcentral-e2e-test/src/features/widgets/meeting/ScheduleMeeting.spec.js',
   },
   options: [],
   drivers: [
-    // 'enzyme',
     'puppeteer',
     'seleniumWebdriverFirefox',
-    'seleniumWebdriverSafari',
-    'seleniumWebdriverChrome',
+    'enzyme',
+    // 'seleniumWebdriverChrome',
+    // 'seleniumWebdriverSafari',
     // 'seleniumWebdriverIE',
     // 'seleniumWebdriverEdge',
   ],
