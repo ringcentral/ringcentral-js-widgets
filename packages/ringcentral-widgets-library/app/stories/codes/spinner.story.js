332f1f5f531ae18f52c6d7e6872c49223fc5213d
MicleMing <fzulanming@gmail.com>
2018-08-31 14:37:24 +0800

    refactor: divide ux and code

--- packages/ringcentral-widgets-library/app/stories/codes/spinner.story.js
+++ packages/ringcentral-widgets-library/app/stories/codes/spinner.story.js
@@ -6,7 +6,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
-import Spinner from '../components/Spinner';
+import Spinner from '../../components/Spinner';
 
 storiesOf('Spinner', module)
   .add('basic', () => <div style={{ width: '50px', height: '50px' }}> <Spinner /> </div>);
