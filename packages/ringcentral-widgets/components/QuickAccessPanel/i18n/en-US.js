1e61aedcba9c4ca8d7894058660a1262ebbf65f9
Bella Lian <bella.lian@lmxmn176.rcoffice.ringcentral.com>
2018-09-03 18:20:56 +0800

    edit

---
+++ packages/ringcentral-widgets/components/QuickAccessPanel/i18n/en-US.js
@@ -0,0 +1,7 @@
+export default {
+  textGoogle: 'Only show in Google apps',
+  textAll: 'Show on all websites except my blacklist',
+  textUser: 'Show on the websites I specify',
+  titleFirst: 'Choose where ',
+  titleSecond: 'Quick Access appears:'
+};
