91b44ad81a437e17b93b67c04db70636f877e238
Lex. Huang <531971909@qq.com>
2018-09-06 17:43:04 +0800

    fix(IncomingCallPage): fix `hasOtherActiveCall` (#1195)

--- packages/ringcentral-widgets/containers/IncomingCallPage/index.js
+++ packages/ringcentral-widgets/containers/IncomingCallPage/index.js
@@ -68,7 +68,11 @@ class IncomingCallPage extends Component {
     if (this.props.session.id !== nextProps.session.id) {
       this._updateAvatarAndMatchIndex(nextProps);
       this.setState({
-        hasOtherActiveCall: nextProps.activeSessionId !== nextProps.session.id,
+        hasOtherActiveCall: nextProps.activeSessionId
+          // when ringcall became active call
+          ? nextProps.activeSessionId !== nextProps.session.id
+          // otherwise when no active call
+          : false,
       });
     }
   }
