fa5d4eee4fe7bdb9569c7fa709fc8455a0773f0c
RingCentral Integrations Team <integrations@ringcentral.com>
2018-09-06 06:58:48 +0000

    released at Thu Sep  6 06:58:48 UTC 2018, commit: 35421212ddb1af38331094b748f8a939c7be5f91

--- modules/RecentMessages/index.js
+++ modules/RecentMessages/index.js
@@ -393,7 +393,7 @@ var RecentMessages = (_dec = (0, _di.Module)({
       var matches = void 0;
       for (var i = conversations.length - 1; i >= 0; i -= 1) {
         var conversation = conversations[i];
-        var messageList = this._messageStore.conversationStore[conversation.id] || [];
+        var messageList = this._messageStore.conversationStore[conversation.conversationId] || [];
         matches = phoneNumbers.find(this._filterPhoneNumber(conversation));
         // Check if message is within certain days
         if (!!matches && new Date(conversation.creationTime) > dateFrom) {
