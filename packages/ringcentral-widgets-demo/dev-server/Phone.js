21afb16886196459c49386c4093a9f3d0cbbd642
Bella Lian <bella.lian@LMXMN176.local>
2018-09-07 13:31:42 +0800

    update settingspage

--- packages/ringcentral-widgets-demo/dev-server/Phone.js
+++ packages/ringcentral-widgets-demo/dev-server/Phone.js
@@ -43,6 +43,7 @@ import ContactSearch from 'ringcentral-integration/modules/ContactSearch';
 import DateTimeFormat from 'ringcentral-integration/modules/DateTimeFormat';
 import Conference from 'ringcentral-integration/modules/Conference';
 import ConferenceCall from 'ringcentral-integration/modules/ConferenceCall';
+import QuickAccess from 'ringcentral-integration/modules/QuickAccess';
 
 import ActiveCalls from 'ringcentral-integration/modules/ActiveCalls';
 import DetailedPresence from 'ringcentral-integration/modules/DetailedPresence';
@@ -119,6 +120,7 @@ import { isRing } from 'ringcentral-integration/modules/Webphone/webphoneHelper'
     { provide: 'AccountContacts', useClass: AccountContacts },
     { provide: 'AddressBook', useClass: AddressBook },
     { provide: 'Contacts', useClass: Contacts },
+    { provide: 'QuickAccess', useClass: QuickAccess },
     {
       provide: 'ContactSources',
       deps: ['AddressBook', 'AccountContacts'],
