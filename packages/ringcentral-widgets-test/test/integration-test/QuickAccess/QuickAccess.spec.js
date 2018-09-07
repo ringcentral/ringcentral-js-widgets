299b9042b0e8f7f3879f91ae10acbdce9bc6daeb
Bella Lian <bella.lian@LMXMN176.local>
2018-09-07 15:21:25 +0800

    add incoming call test

--- packages/ringcentral-widgets-test/test/integration-test/QuickAccess/QuickAccess.spec.js
+++ packages/ringcentral-widgets-test/test/integration-test/QuickAccess/QuickAccess.spec.js
@@ -4,10 +4,22 @@ import QuickAccessPanel from 'ringcentral-widgets/components/QuickAccessPanel';
 import LinkLine from 'ringcentral-widgets/components/LinkLine';
 import Button from 'ringcentral-widgets/components/Button';
 import { getWrapper } from 'ringcentral-widgets-test/test/integration-test/shared';
+import { getInboundCall, makeCall } from '../../support/callHelper';
 
 let wrapper = null;
 let quickAccessPanel = null;
 let panel = null;
+const sid111 = '111';
+let phone = null;
+
+async function makeInbountCall(sessionId) {
+  await getInboundCall(phone, {
+    id: sessionId,
+    direction: 'Inbound'
+  });
+  wrapper.update();
+}
+
 beforeEach(async () => {
   wrapper = await getWrapper();
   const navigationBar = wrapper.find(NavigationBar).first();
@@ -22,6 +34,8 @@ beforeEach(async () => {
   wrapper.update();
   quickAccessPanel = wrapper.find(QuickAccessPanel).first();
 });
+
+
 
 
 describe('<QuickAccessPanel />', () => {
@@ -78,6 +92,17 @@ describe('<QuickAccessPanel />', () => {
     wrapper.update();
     const description = wrapper.find(QuickAccessPanel).first().find('.description').first();
     expect(description.find('span').length).toEqual(0);
+  });
+
+  test('when incoming call , page disappear', async () => {
+    expect(wrapper.find('.group').length).toEqual(1);
+    phone = wrapper.props().phone;
+    phone.webphone._createWebphone();
+    phone.webphone._removeWebphone = () => { };
+    phone.webphone._connect = () => { };
+    await makeInbountCall(sid111);
+    wrapper.update();
+    expect(wrapper.find('.group').length).toEqual(0);
   });
 
 })
