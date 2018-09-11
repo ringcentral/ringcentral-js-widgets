61ab0a9c026bd185eec6e87d87ecc92dcfb6b2fd
Rukai Yu <rukai.yu@ringcentral.com>
2018-09-11 14:04:11 +0800

    improve(ConferenceInfo): improve the ConferenceInfo component to display the avatars when the window resizing.
    #RCINT-8622

--- packages/ringcentral-widgets/components/ActiveCallPanel/ConferenceInfo.js
+++ packages/ringcentral-widgets/components/ActiveCallPanel/ConferenceInfo.js
@@ -1,14 +1,79 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import debounce from 'ringcentral-integration/lib/debounce';
 
 import CallAvatar from '../CallAvatar';
 import dynamicsFont from '../../assets/DynamicsFont/DynamicsFont.scss';
 import styles from './styles.scss';
 import i18n from './i18n';
 
+const MAXIMUM_AVATARS = 4;
+const WIDTH_PER_AVATAR = 51;
+const PEDDING_WIDTH = 15;
+const MINIUM_WIDTH = 140;
+
 export class ConferenceInfo extends Component {
-  shouldComponentUpdate(nextProps) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      avatarCount: this._computeAvatarCountByWindowWidth(),
+    };
+  }
+
+  _computeAvatarCountByWindowWidth() {
+    const w = window;
+    const width = w.innerWidth;
+
+    const { partyProfiles } = this.props;
+
+    const avatarProfilesCount = (partyProfiles && partyProfiles.length) || 0;
+
+    let avatarCount = avatarProfilesCount;
+
+    const minWith4Avatars = WIDTH_PER_AVATAR * (MAXIMUM_AVATARS) + PEDDING_WIDTH;
+
+    if (
+      width <= MINIUM_WIDTH
+    ) {
+      avatarCount = 1; // meets the minium window width, shows 1 avart + num
+    } else if (
+      width <= minWith4Avatars
+    ) {
+      avatarCount = 2; // meets the minium width of 2 avatars, show 2 avart + num
+    } else if (
+      avatarCount >= MAXIMUM_AVATARS
+    ) {
+      avatarCount = MAXIMUM_AVATARS;
+    }
+
+    return avatarCount;
+  }
+
+  _updateAvatarAmounts = debounce(() => {
+    if (!this._mounted) {
+      return;
+    }
+
+    const avatarCount = this._computeAvatarCountByWindowWidth();
+
+    this.setState({
+      avatarCount,
+    });
+  }, 100);
+
+
+  componentDidMount() {
+    this._mounted = true;
+    window.addEventListener('resize', this._updateAvatarAmounts);
+  }
+
+  componentWillUnmount() {
+    this._mounted = false;
+    window.removeEventListener('resize', this._updateAvatarAmounts);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
     const { partyProfiles } = nextProps;
     const oldpartyProfiles = this.props.partyProfiles;
     let showUpdate = true;
@@ -25,11 +90,30 @@ export class ConferenceInfo extends Component {
           }
         }
       }
+    } else if (nextState.avatarCount !== this.state.avatarCount) {
+      showUpdate = true;
     } else {
       showUpdate = false;
     }
 
     return showUpdate;
+  }
+
+  computeDisplayedProfiles({
+    profiles,
+    avatarCount,
+  }) {
+    const displayedProfiles =
+      profiles.length >= avatarCount
+        ? profiles.slice(0, avatarCount)
+        : profiles;
+
+    const remains = profiles.length - avatarCount;
+
+    return {
+      displayedProfiles,
+      remains,
+    };
   }
 
   render() {
@@ -38,17 +122,14 @@ export class ConferenceInfo extends Component {
       partyProfiles,
       onClick,
     } = this.props;
-    const MAXIMUM_AVATARS = 4;
     const profiles = partyProfiles || [];
 
-    const displayedProfiles =
-      profiles.length >= MAXIMUM_AVATARS
-        ? profiles.slice(0, MAXIMUM_AVATARS)
-        : profiles;
+    const { avatarCount } = this.state;
 
-    const remains = profiles.length > MAXIMUM_AVATARS
-      ? profiles.length - MAXIMUM_AVATARS
-      : 0;
+    const {
+      displayedProfiles,
+      remains
+    } = this.computeDisplayedProfiles({ profiles, avatarCount });
 
     return (
       <div
@@ -62,16 +143,17 @@ export class ConferenceInfo extends Component {
                   onClick={(e) => { e.preventDefault(); onClick(); }}
                 >
                   {
-                    displayedProfiles.map(({ avatarUrl, toUserName }, idx) => (
-                      <div
-                        key={`${toUserName}_${idx}`}
-                        className={styles.avatar}>
-                        <CallAvatar
-                          avatarUrl={avatarUrl}
-                        />
-                      </div>
+                  displayedProfiles.map(({ avatarUrl, toUserName }, idx) =>
+                  (
+                    <div
+                      key={`${toUserName}_${idx}`}
+                      className={styles.avatar}>
+                      <CallAvatar
+                        avatarUrl={avatarUrl}
+                      />
+                    </div>
                     )
-                    )
+                  )
                   }{
                     remains > 0
                       ? (
