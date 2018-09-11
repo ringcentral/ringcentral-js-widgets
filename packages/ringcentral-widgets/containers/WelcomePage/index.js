ba175929a05314e20d3d6fddafd0cd45a5a0f8b4
Bruce Li <bruce.li@lmxmn208.rcoffice.ringcentral.com>
2018-09-11 15:23:13 +0800

    feat: 🎸 refactor sign up function
    move google chrome related code to where it should be, remove vscode
    settings related changes

--- packages/ringcentral-widgets/containers/WelcomePage/index.js
+++ packages/ringcentral-widgets/containers/WelcomePage/index.js
@@ -15,6 +15,7 @@ function mapToProps(_, {
   },
   version,
   showSignUp = false,
+  onSignUpButtonClick
 }) {
   return {
     currentLocale: locale.currentLocale,
@@ -32,6 +33,7 @@ function mapToProps(_, {
       auth.loginStatus === loginStatus.loggedIn
     ),
     showSignUp,
+    onSignUpButtonClick,
   };
 }
 
