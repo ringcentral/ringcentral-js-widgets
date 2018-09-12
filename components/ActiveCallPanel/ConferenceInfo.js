0a81bdf9eb57b04d15d12d0c4414c6579bcebfe6
RingCentral Integrations Team <integrations@ringcentral.com>
2018-09-12 08:25:30 +0000

    released at Wed Sep 12 08:25:30 UTC 2018, commit: d0223cb76b9a77203b355d8bb0e00dfc9d815b44

--- components/ActiveCallPanel/ConferenceInfo.js
+++ components/ActiveCallPanel/ConferenceInfo.js
@@ -37,6 +37,10 @@ var _classnames = require('classnames');
 
 var _classnames2 = _interopRequireDefault(_classnames);
 
+var _debounce = require('ringcentral-integration/lib/debounce');
+
+var _debounce2 = _interopRequireDefault(_debounce);
+
 var _CallAvatar = require('../CallAvatar');
 
 var _CallAvatar2 = _interopRequireDefault(_CallAvatar);
@@ -55,17 +59,109 @@ var _i18n2 = _interopRequireDefault(_i18n);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var MAXIMUM_AVATARS = 4;
+
+var WIDTH_PER_AVATAR = parseInt(_styles2.default.conferenceAvatarSize, 0); // 51
+
+var AVATAR_MERGIN_LEFT = parseInt(_styles2.default.avatarMerginLeftSize, 0); // -20
+var PEDDING_WIDTH = parseInt(_styles2.default.avatarPaddingSize, 0); // 15
+
+var minWidthCalculator = function minWidthCalculator(count) {
+  return WIDTH_PER_AVATAR * count + AVATAR_MERGIN_LEFT * (count - 1) + PEDDING_WIDTH * 2 + 1 + 2;
+};
+
+// when the container width reachs below item of width, display the avatar amount of count.
+var KINDS_OF_WIDTH_THAT_NEED_ADAPATER = [{ avartarCount: 0, width: minWidthCalculator(1) }, { avartarCount: 1, width: minWidthCalculator(3) }, { avartarCount: 2, width: minWidthCalculator(MAXIMUM_AVATARS) }, { avartarCount: 3, width: minWidthCalculator(MAXIMUM_AVATARS + 1) }];
+
 var ConferenceInfo = exports.ConferenceInfo = function (_Component) {
   (0, _inherits3.default)(ConferenceInfo, _Component);
 
-  function ConferenceInfo() {
+  function ConferenceInfo(props) {
     (0, _classCallCheck3.default)(this, ConferenceInfo);
-    return (0, _possibleConstructorReturn3.default)(this, (ConferenceInfo.__proto__ || (0, _getPrototypeOf2.default)(ConferenceInfo)).apply(this, arguments));
+
+    var _this = (0, _possibleConstructorReturn3.default)(this, (ConferenceInfo.__proto__ || (0, _getPrototypeOf2.default)(ConferenceInfo)).call(this, props));
+
+    _this.onWindowResize = (0, _debounce2.default)(function () {
+      _this.updateAvatarAmounts(_this.props);
+    }, 100);
+
+    _this.state = {
+      avatarCount: MAXIMUM_AVATARS
+    };
+
+    _this._container = _react2.default.createRef();
+    return _this;
   }
 
   (0, _createClass3.default)(ConferenceInfo, [{
+    key: '_computeAvatarCountByWindowWidth',
+    value: function _computeAvatarCountByWindowWidth(props) {
+      var partyProfiles = props.partyProfiles;
+
+      var avatarProfilesCount = partyProfiles && partyProfiles.length || 0;
+
+      if (!this._mounted) {
+        if (avatarProfilesCount >= MAXIMUM_AVATARS) {
+          return MAXIMUM_AVATARS;
+        }
+        return avatarProfilesCount;
+      }
+
+      var width = this._container && this._container.current && this._container.current.clientWidth;
+
+      var avatarCount = avatarProfilesCount;
+
+      var firstMatchWidth = KINDS_OF_WIDTH_THAT_NEED_ADAPATER.find(function (it) {
+        return width < it.width;
+      });
+
+      if (firstMatchWidth) {
+        avatarCount = firstMatchWidth.avartarCount;
+        if (avatarCount + 1 === avatarProfilesCount) {
+          avatarCount = avatarProfilesCount;
+        }
+      } else if (avatarCount >= MAXIMUM_AVATARS) {
+        avatarCount = MAXIMUM_AVATARS;
+      }
+
+      return avatarCount;
+    }
+  }, {
+    key: 'updateAvatarAmounts',
+    value: function updateAvatarAmounts(props) {
+      if (!this._mounted) {
+        return;
+      }
+
+      var avatarCount = this._computeAvatarCountByWindowWidth(props);
+
+      if (avatarCount !== this.state.avatarCount) {
+        this.setState({
+          avatarCount: avatarCount
+        });
+      }
+    }
+  }, {
+    key: 'componentWillReceiveProps',
+    value: function componentWillReceiveProps(nextProps) {
+      this.updateAvatarAmounts(nextProps);
+    }
+  }, {
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      this._mounted = true;
+      window.addEventListener('resize', this.onWindowResize);
+      this.updateAvatarAmounts(this.props);
+    }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      this._mounted = false;
+      window.removeEventListener('resize', this.onWindowResize);
+    }
+  }, {
     key: 'shouldComponentUpdate',
-    value: function shouldComponentUpdate(nextProps) {
+    value: function shouldComponentUpdate(nextProps, nextState) {
       var partyProfiles = nextProps.partyProfiles;
 
       var oldpartyProfiles = this.props.partyProfiles;
@@ -80,11 +176,28 @@ var ConferenceInfo = exports.ConferenceInfo = function (_Component) {
             }
           }
         }
+      } else if (nextState.avatarCount !== this.state.avatarCount) {
+        showUpdate = true;
       } else {
         showUpdate = false;
       }
 
       return showUpdate;
+    }
+  }, {
+    key: 'computeDisplayedProfiles',
+    value: function computeDisplayedProfiles(_ref) {
+      var profiles = _ref.profiles,
+          avatarCount = _ref.avatarCount;
+
+      var displayedProfiles = profiles.length >= avatarCount ? profiles.slice(0, avatarCount) : profiles;
+
+      var remains = profiles.length - avatarCount;
+
+      return {
+        displayedProfiles: displayedProfiles,
+        remains: remains
+      };
     }
   }, {
     key: 'render',
@@ -94,19 +207,21 @@ var ConferenceInfo = exports.ConferenceInfo = function (_Component) {
           partyProfiles = _props.partyProfiles,
           _onClick = _props.onClick;
 
-      var MAXIMUM_AVATARS = 4;
       var profiles = partyProfiles || [];
 
-      var displayedProfiles = profiles.length >= MAXIMUM_AVATARS ? profiles.slice(0, MAXIMUM_AVATARS) : profiles;
+      var avatarCount = this.state.avatarCount;
 
-      var remains = profiles.length > MAXIMUM_AVATARS ? profiles.length - MAXIMUM_AVATARS : 0;
+      var _computeDisplayedProf = this.computeDisplayedProfiles({ profiles: profiles, avatarCount: avatarCount }),
+          displayedProfiles = _computeDisplayedProf.displayedProfiles,
+          remains = _computeDisplayedProf.remains;
 
       return _react2.default.createElement(
         'div',
         {
-          className: _styles2.default.conferenceCallInfoContainer
+          className: _styles2.default.conferenceCallInfoContainer,
+          ref: this._container
         },
-        displayedProfiles.length ? _react2.default.createElement(
+        displayedProfiles.length || avatarCount === 0 && remains > 0 ? _react2.default.createElement(
           'div',
           {
             className: (0, _classnames2.default)(_styles2.default.avatarContainer, _styles2.default.clickable),
@@ -114,9 +229,9 @@ var ConferenceInfo = exports.ConferenceInfo = function (_Component) {
               e.preventDefault();_onClick();
             }
           },
-          displayedProfiles.map(function (_ref, idx) {
-            var avatarUrl = _ref.avatarUrl,
-                toUserName = _ref.toUserName;
+          displayedProfiles.map(function (_ref2, idx) {
+            var avatarUrl = _ref2.avatarUrl,
+                toUserName = _ref2.toUserName;
             return _react2.default.createElement(
               'div',
               {
