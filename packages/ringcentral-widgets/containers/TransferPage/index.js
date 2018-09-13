3a1929a52ca7d26cf577d3317c1398c28167f83b
Lucy Tu <lucy.tu@ringcentral.com>
2018-09-13 15:41:05 +0800

    add TransferPage into Widget.

---
+++ packages/ringcentral-widgets/containers/TransferPage/index.js
@@ -0,0 +1,60 @@
+
+import { connect } from 'react-redux';
+import formatNumber from 'ringcentral-integration/lib/formatNumber';
+import TransferPanel from '../../components/TransferPanel';
+import { withPhone } from '../../lib/phoneContext';
+
+function mapToProps(_, {
+  phone
+}) {
+  const {
+    locale
+  } = phone;
+  const {
+    currentLocale
+  } = locale;
+  return {
+    currentLocale,
+    searchContactList: [],
+    isOnTransfer: false,
+  };
+}
+
+function mapToFunctions(_, {
+  phone,
+}) {
+  const {
+    regionSettings,
+    routerInteraction,
+    activeCallControl
+  } = phone;
+  return {
+    onTransfer(transferNumber) {
+      const sessionIdRgx = /\d+/g;
+      const sessionId = routerInteraction.currentPath.match(sessionIdRgx);
+      if (sessionId) {
+        activeCallControl.transfer(transferNumber, sessionId[0]);
+      }
+    },
+    toggleTransferPanel() {
+      phone.routerInteraction.push('/calls');
+    },
+    formatPhone: phoneNumber => formatNumber({
+      phoneNumber,
+      areaCode: regionSettings.areaCode,
+      countryCode: regionSettings.countryCode,
+    }),
+    searchContact: () => null
+  };
+}
+
+const TransferPage = withPhone(connect(
+  mapToProps,
+  mapToFunctions,
+)(TransferPanel));
+
+export {
+  mapToProps,
+  mapToFunctions,
+  TransferPage as default,
+};
