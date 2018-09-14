f081733ecf91758009c4683c35c39a8affa54dde
MicleMing <fzulanming@gmail.com>
2018-09-14 16:41:09 +0800

    fix: copywriting in call control alert

--- packages/ringcentral-widgets/components/CallControlAlert/i18n/en-US.js
+++ packages/ringcentral-widgets/components/CallControlAlert/i18n/en-US.js
@@ -6,8 +6,7 @@ const {
   unHoldConflictError,
   muteConflictError,
   unMuteConflictError,
-  hangUpError,
-  transferError
+  generalError,
 } = callControlError;
 
 export default {
@@ -15,6 +14,5 @@ export default {
   [holdConflictError]: 'This call had been held on other device. Please unhold the call before you control in this App.',
   [unMuteConflictError]: 'This call had been unmuted on other device. Please mute the call before you control in this App.',
   [unHoldConflictError]: 'This call had been unheld on other device. Please hold the call before you control in this App.',
-  [transferError]: 'transfer error',
-  [hangUpError]: 'hang up error',
+  [generalError]: 'Unexpected server error. Please try again later.',
 };
