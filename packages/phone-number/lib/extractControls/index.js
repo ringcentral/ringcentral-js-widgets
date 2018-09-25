91dea33d9d0dcf2474680a762a42120151170e94
Jack Tzu-Chieh Huang <u9520107@gmail.com>
2018-09-25 16:31:48 +0800

    [bug] Handle case when input is null

--- packages/phone-number/lib/extractControls/index.js
+++ packages/phone-number/lib/extractControls/index.js
@@ -3,11 +3,11 @@ import { reduce } from 'ramda';
 const pauseRegex = /,/g;
 const controlRegex = /[^0-9*#]/g;
 
-export default function extractExtendedControls(input = '') {
+export default function extractExtendedControls(input) {
   const [
     phoneNumber,
     ...tokens
-  ] = input.split(pauseRegex);
+  ] = (input || '').split(pauseRegex);
   return {
     input,
     phoneNumber,
