d8a3b8efe86c29ac96b4327c6aed57393d373d13
Rulee Chen <rulee@live.cn>
2018-10-01 22:37:26 +0800

    fix bug: activeCalls is empty in a flash when fetch success (#1341)

--- packages/ringcentral-integration/modules/DetailedPresence/actionTypes.js
+++ packages/ringcentral-integration/modules/DetailedPresence/actionTypes.js
@@ -3,5 +3,4 @@ import presenceActionTypes from '../Presence/actionTypes';
 
 export default new Enum([
   ...Object.keys(presenceActionTypes),
-  'updateActiveCalls',
 ], 'detailedPresence');
--- packages/ringcentral-integration/modules/DetailedPresence/getDetailedPresenceReducer.js
+++ packages/ringcentral-integration/modules/DetailedPresence/getDetailedPresenceReducer.js
@@ -27,7 +27,10 @@ export function getDataReducer(types) {
   return (state = [], { type, activeCalls = [], timestamp }) => {
     switch (type) {
       case types.fetchSuccess:
-      case types.updateActiveCalls: {
+      case types.notification: {
+        if (!timestamp) {
+          return state;
+        }
         return map((activeCall) => {
           const existingCall = state.find(call => (
             call.sessionId === activeCall.sessionId
