4e7db77d27e5ca2a7339a391b097cf85c80b9150
Jack Tzu-Chieh Huang <u9520107@gmail.com>
2018-10-05 16:56:25 +0800

    [refactor] refactor parse function to be more readable

--- packages/phone-number/lib/parse/index.js
+++ packages/phone-number/lib/parse/index.js
@@ -6,6 +6,107 @@ const invalidCharsRegExp = /[^\d*+#\-(). ]/;
 const plusRegex = /\+/g;
 const extensionDelimiter = /[*#]/g;
 
+/**
+ * @typedef {object} ParsedResult
+ * @property {string} input
+ * @property {string} parsedCountry
+ * @property {string} parsedNumber
+ * @property {boolean} isValid
+ * @property {boolean} hasInvalidChars
+ * @property {boolean} isExtension
+ * @property {boolean} isServiceNumber
+ * @property {boolean} hasPlus
+ * @property {string} phoneNumber
+ * @property {string} extension
+ * @property {string[]} extendedControls
+ */
+
+/**
+ * @description helper function to attatch parsed country and phone number with libphonenumber
+ * @param {ParsedResult} result
+ * @param {string} countryCode
+ */
+function attachParsedCountryInfo(result, countryCode) {
+  const {
+    country = null,
+    phone = null,
+  } = parseNumber(result.phoneNumber, countryCode);
+  result.parsedCountry = country;
+  result.parsedNumber = phone;
+}
+
+/**
+ * @description process the tokens as a service number
+ * @param {ParsedResult} result
+ * @param {string[]} tokens
+ * @returns {ParsedResult}
+ */
+function processServiceNumber(result, tokens) {
+  if (tokens[1] && tokens[1].length) {
+    result.isServiceNumber = true;
+    result.phoneNumber = `*${tokens[1]}`;
+    result.isValid = true;
+  }
+  return result;
+}
+
+/**
+ * @description process the tokens as an E164 formatted number
+ * @param {ParsedResult} result
+ * @param {string[]} tokens
+ * @param {string} countryCode
+ * @returns {ParsedResult}
+ */
+function processInternational(result, tokens, countryCode) {
+  if (tokens[0] && tokens[0].length) {
+    result.hasPlus = true;
+    result.phoneNumber = `+${tokens[0]}`;
+    // use libphonenumber to parse country code in the number
+    attachParsedCountryInfo(result, countryCode);
+    if (tokens[1] && tokens[1].length) {
+      result.extension = tokens[1];
+    }
+    result.isValid = true;
+  }
+  return result;
+}
+
+/**
+ * @description process the tokens as local numbers including extensions
+ * @param {ParsedResult} result
+ * @param {string[]} tokens
+ * @param {string} countryCode
+ * @returns {ParsedResult}
+ */
+function processLocalNumber(result, tokens, countryCode) {
+  if (tokens[0] && tokens[0].length) {
+    // not extension
+    if (tokens[0].length > 6) {
+      result.phoneNumber = tokens[0];
+      attachParsedCountryInfo(result, countryCode);
+      if (tokens[1] && tokens[1].length) {
+        result.extension = tokens[1];
+      }
+    } else {
+      result.isExtension = true;
+      result.phoneNumber = tokens[0];
+    }
+    result.isValid = true;
+    return result;
+  }
+  return result;
+}
+
+/**
+ * @typedef {object} ParseInput
+ * @property {string} input
+ * @property {string} countryCode
+ */
+
+/**
+ * @param {ParseInput}
+ * @returns {ParsedResult}
+ */
 export default function parse({ input, countryCode = 'US' }) {
   const {
     phoneNumber,
@@ -16,7 +117,7 @@ export default function parse({ input, countryCode = 'US' }) {
     input,
     parsedCountry: null,
     parsedNumber: null,
-    isValid: true,
+    isValid: false,
     hasInvalidChars: invalidCharsRegExp.test(input),
     isExtension: false,
     isServiceNumber: false,
@@ -30,52 +131,22 @@ export default function parse({ input, countryCode = 'US' }) {
   const withoutPlus = cleanInput.replace(plusRegex, '');
   const startWithStar = withoutPlus[0] === '*';
 
+  // cleanInput = '+*xxxx'; // is invalid
   if (startWithPlus && startWithStar) {
-    result.isValid = false;
-  } else {
-    const tokens = withoutPlus.split(extensionDelimiter);
-    if (startWithStar) {
-      if (tokens[1] && tokens[1].length) {
-        result.isServiceNumber = true;
-        result.phoneNumber = `*${tokens[1]}`;
-      } else {
-        result.isValid = false;
-      }
-    } else if (startWithPlus) {
-      if (tokens[0] && tokens[0].length) {
-        result.hasPlus = true;
-        result.phoneNumber = `+${tokens[0]}`;
-        const {
-          country = null,
-          phone = null,
-        } = parseNumber(result.phoneNumber, countryCode);
-        result.parsedCountry = country;
-        result.parsedNumber = phone;
-        if (tokens[1] && tokens[1].length) {
-          result.extension = tokens[1];
-        }
-      } else {
-        result.isValid = false;
-      }
-    } else if (tokens[0] && tokens[0].length) {
-      if (tokens[0].length > 6) {
-        result.phoneNumber = tokens[0];
-        const {
-          country = null,
-          phone = null,
-        } = parseNumber(result.phoneNumber, countryCode);
-        result.parsedCountry = country;
-        result.parsedNumber = phone;
-        if (tokens[1] && tokens[1].length) {
-          result.extension = tokens[1];
-        }
-      } else {
-        result.isExtension = true;
-        result.phoneNumber = tokens[0];
-      }
-    } else {
-      result.isValid = false;
-    }
+    return result;
   }
-  return result;
+  const tokens = withoutPlus.split(extensionDelimiter);
+
+  // cleanInput = '*xxxx'; // service number
+  if (startWithStar) {
+    return processServiceNumber(result, tokens);
+  }
+
+  // cleanInput = '+xxx'; // should contain country code
+  if (startWithPlus) {
+    return processInternational(result, tokens, countryCode);
+  }
+
+  // cleanNumber = 'xxxxx'; // is local number
+  return processLocalNumber(result, tokens, countryCode);
 }
