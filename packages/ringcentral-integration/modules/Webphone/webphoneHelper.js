739277a94fa41880bb4c38d467d0e4b2da994497
embbnux <embbnux@embbnux.com>
2018-10-10 09:10:33 +0800

    suppport firefox

--- packages/ringcentral-integration/modules/Webphone/webphoneHelper.js
+++ packages/ringcentral-integration/modules/Webphone/webphoneHelper.js
@@ -3,17 +3,31 @@ import sessionStatus from './sessionStatus';
 import { camelize } from '../../lib/di/utils/utils';
 import callDirections from '../../enums/callDirections';
 
-export function isBrowserSupport() {
-  const isChrome = !!(navigator.userAgent.match(/Chrom(e|ium)/));
-  if (!isChrome) {
+let environment;
+if (typeof window !== 'undefined') {
+  environment = window;
+}
+if (typeof global !== 'undefined') {
+  environment = global.window || global;
+}
+
+export function isWebSocketSupport() {
+  return !!(environment && environment.WebSocket);
+}
+
+export function isWebRTCSupport() {
+  if (!environment.navigator) {
     return false;
   }
-  const chromeVersion =
-    parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2], 10);
-  if (chromeVersion >= 51) {
-    return true;
-  }
-  return false;
+  return !!(
+    environment.MediaStream &&
+    environment.RTCPeerConnection &&
+    environment.navigator.mediaDevices.getUserMedia
+  )
+}
+
+export function isBrowserSupport() {
+  return isWebSocketSupport() && isWebRTCSupport();
 }
 
 export function extractHeadersData(session, headers) {
