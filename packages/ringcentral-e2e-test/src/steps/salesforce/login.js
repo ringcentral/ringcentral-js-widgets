5e05040a8f94663191c30c1d76a673f7ca58c424
summerGancf <summer.gan@LMXMN204.local>
2018-10-17 11:28:14 +0800

    feat: rebase

--- packages/ringcentral-e2e-test/src/steps/salesforce/login.js
+++ packages/ringcentral-e2e-test/src/steps/salesforce/login.js
@@ -4,7 +4,7 @@ import ToggleEnv from './toggleEnv';
 /* global $ */
 export default class Login {
   static async prepare(context) {
-    await $(context.driver.app).waitFor('[class*=loginButton]',{ selector: 'css' });
+    await $(context.driver.app).waitFor('[class*=loginButton]', { selector: 'css' });
     if (context.options.isVirtual) return;
     const process = createProcess(
       ToggleEnv,
@@ -14,8 +14,7 @@ export default class Login {
 
   static async getAccount(context) {
     const accountHelper = new AccountHelper();
-<<<<<<< HEAD
-    if(3 ===context.options.option.tagNum){
+    if (3 === context.options.option.tagNum) {
       context.options.option.account1 = await accountHelper.getAccount(context.options.option.accountTag1);
       context.options.option.account2 = await accountHelper.getAccount(context.options.option.accountTag2);
       context.options.option.account3 = await accountHelper.getAccount(context.options.option.accountTag3);
@@ -27,27 +26,15 @@ export default class Login {
     } else {
       console.log('tagNum not 3,please set accountTag & tagNum');
     }
-=======
-    context.options.option.account = await accountHelper.getAccount(context.options.option.accountTag);
-    context.driver.addAfterHook(async () => {
-      await accountHelper.recycleAccount(context.options.option.account[0]['uuid']);
-    });
->>>>>>> e2e-discusstion
   }
 
   static async login({ options: { option, isVirtual }, driver: { app } }) {
     if (isVirtual) {
-<<<<<<< HEAD
       app.props().phone.auth.login({ username: option.account1[0]['mainNumber'], password: option.account1[0]['password'] });
     } else {
       await $(app).execute(`phone.auth.login({username: '${option.account1[0]['mainNumber']}', password: '${option.account1[0]['password']}'})`);
-=======
-      app.props().phone.auth.login({ username: option.account[0]['mainNumber'], password: option.account[0]['password'] });
-    } else {
-      await $(app).execute(`phone.auth.login({username: '${option.account[0]['mainNumber']}', password: '${option.account[0]['password']}'})`);
->>>>>>> e2e-discusstion
     }
-    await $(app).waitForSelector('[title="More Menu"]',{ selector: 'css' });
+    await $(app).waitForSelector('[title="More Menu"]', { selector: 'css' });
   }
 
   // static async login({ options: { option, isVirtual }, driver: { app } }) {
