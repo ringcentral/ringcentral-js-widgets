e2a06d574d649a5a56b2e4b4d2943344af8a63a6
Joy Zhang <mint.soda@hotmail.com>
2018-10-19 10:38:38 +0800

    chore(file path): update file path and catalog

--- packages/ringcentral-e2e-test/src/features/office/oauth/index.js
+++ packages/ringcentral-e2e-test/src/features/office/oauth/index.js
@@ -4,7 +4,6 @@ import { createProcess } from 'marten';
 import AuthorizeOffice, { goThrough3rdPartyLogin } from '../../../steps/office/authorizeOffice';
 import Login from '../../../steps/office/login';
 import Entry from '../../../steps/entry';
-import sleep from 'ringcentral-integration/lib/sleep';
 
 beforeEach(() => {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
@@ -37,7 +36,7 @@ async function detectAuthPanel(isAuth, option) {
     await $(page).waitFor('[class*="IconField"] > i', { selector: 'css' });
     await $(page).click('[class*="IconField"] > i', { selector: 'css' });
     const authTooltip = await $(page).getText('[class*="rc-tooltip-inner"]', { selector: 'css' });
-    expect(authTooltip).toContain(`You have authorized RingCentral for Office365 to access your account ${option.officeAccout}.`);
+    expect(authTooltip).toEqual(`You have authorized RingCentral for Office365 to access your account ${option.officeAccout}.`);
     // Authorise button
     const authButton = await $(page).getText('button[class*="AuthorizeSettingsPanel"]', { selector: 'css' });
     expect(authButton).toEqual('Unauthorize');
@@ -66,7 +65,7 @@ describe('RCI-330 - Authorize and Unauthorize RingCentral for Office 365', () =>
         scheduleButton: 'Invite with Outlook Calendar',
       },
     ],
-  }, async (option) => {
+  }, async ({ option }) => {
     // Login CTI
     const params = context.options.config;
     const process = createProcess(
@@ -104,7 +103,7 @@ describe('RCI-330 - Authorize and Unauthorize RingCentral for Office 365', () =>
      */
     await $(page).waitFor('[class*="loginNumber"]', { selector: 'css' });
     await $(page).click('[class*="loginNumber"]', { selector: 'css' });
-    await Login.login();
+    Login.login();
     await detectAuthPanel(true, option);
     /** Click the Unauthorise button
      * User should be able to view below authorized message
@@ -161,7 +160,7 @@ describe('RCI-330 - Authorize and Unauthorize RingCentral for Office 365', () =>
   });
 });
 
-export async function caseAuthorizePanel(option) {
+export async function caseAuthorizePanel({ option }) {
   // Login CTI
   const params = context.options.config;
   const process = createProcess(
@@ -171,16 +170,13 @@ export async function caseAuthorizePanel(option) {
   )(context);
   // Authroize Office365
   await process.exec();
-  await detectAuthPanel(true, option.config);
+  await detectAuthPanel(true, option);
   /** Click logout and relogin, user still Authorized **/
   await $(page).waitFor('[class*="loginNumber"]', { selector: 'css' });
   await $(page).click('[class*="loginNumber"]', { selector: 'css' });
-  sleep(2000);
-  await Login.login({
-    noSkipUserGuide: true
-  });
-  await detectAuthPanel(false, option.config);
+  Login.login();
+  await detectAuthPanel(true, option);
   /** Click the Unauthorise button **/
   await $(page).click('button[class*="AuthorizeSettingsPanel"]', { selector: 'css' });
-  await detectAuthPanel(false, option.config);
+  await detectAuthPanel(false, option);
 }
