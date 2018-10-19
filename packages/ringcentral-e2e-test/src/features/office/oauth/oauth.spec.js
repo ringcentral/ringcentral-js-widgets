55c2a3d13ba581f891fd436fa25cc560ecf89f82
Joy Zhang <mint.soda@hotmail.com>
2018-10-19 10:14:18 +0800

    refactor(office auth): update auth flow

--- packages/ringcentral-e2e-test/src/features/office/oauth/oauth.spec.js
+++ packages/ringcentral-e2e-test/src/features/office/oauth/oauth.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 /* global $, page, browser, driver, context */
 import { createProcess } from 'marten';
-import AuthorizeOffice from '../../../steps/office/authorizeOffice';
+import AuthorizeOffice, { goThrough3rdPartyLogin } from '../../../steps/office/authorizeOffice';
 import Login from '../../../steps/office/login';
 import Entry from '../../../steps/entry';
 
@@ -21,81 +21,100 @@ afterEach(async() => {
  * - if is auth: Unauthorise button, Button should be highlight and clickable
  * - if is unauth: Authorise button, Button should be highlight and clickable
  */
-async function detectAuthPanel(isAuth) {
-  debugger;
+async function detectAuthPanel(isAuth, option) {
   await $(page).waitFor('div[title="More Menu"]', { selector: 'css' });
   await $(page).click('div[title="More Menu"]', { selector: 'css' });
   await $(page).waitFor('div[title="Settings"]', { selector: 'css' });
   await $(page).click('div[title="Settings"]', { selector: 'css' });
   if (isAuth) {
-    const authTitle = await $(page).getText("[class*='AuthorizeSettingsPanel-_styles_title']", { selector: 'css' });
+    await $(page).waitFor(2000);
+    // title: Authorised Account
+    $(page).waitFor('span[class*="src-components-AuthorizeSettingsPanel-_styles_title"]', { selector: 'css' });
+    const authTitle = await $(page).getText('span[class*="AuthorizeSettingsPanel-_styles_title"]', { selector: 'css' });
     expect(authTitle).toEqual('Authorized Account');
-    const authTooltip = await $(page).getText("[class*='AuthorizeSettingsPanel-_styles_tooltip']", { selector: 'css' });
-    expect(authTooltip).toEqual(`You have authorized RingCentral for Office365 to access your account ${option.officeAccout}`);
+    // i button
+    await $(page).waitFor('[class*="IconField"] > i', { selector: 'css' });
+    await $(page).click('[class*="IconField"] > i', { selector: 'css' });
+    const authTooltip = await $(page).getText('[class*="rc-tooltip-inner"]', { selector: 'css' });
+    expect(authTooltip).toEqual(`You have authorized RingCentral for Office365 to access your account ${option.officeAccout}.`);
+    // Authorise button
     const authButton = await $(page).getText('button[class*="AuthorizeSettingsPanel"]', { selector: 'css' });
     expect(authButton).toEqual('Unauthorize');
   } else {
+    // title: empty
     const authTitle = await $(page).getText("[class*='AuthorizeSettingsPanel-_styles_title']", { selector: 'css' });
     expect(authTitle).toEqual('');
+    // Authorise button
     const authButton = await $(page).getText('button[class*="AuthorizeSettingsPanel"]', { selector: 'css' });
     expect(authButton).toEqual('Authorize');
   }
 }
 
 describe('RCI-330 - Authorize and Unauthorize RingCentral for Office 365', () => {
-  // test({
-  //   title: 'Go to Schedule Meeting',
-  //   tags: [
-  //     ['office', { brands: ['rc'] }],
-  //   ],
-  //   brands: ['rc'],
-  //   levels: ['p0'],
-  //   options: [
-  //     {
-  //       buttonText: 'Invite with Outlook Calendar'
-  //     },
-  //   ],
-  // }, async ({ option }) => {
-  //   // Login CTI
-  //   const params = context.options.config;
-  //   const process = createProcess(
-  //     Entry,
-  //     Login,
-  //   )(context);
-  //   await process.exec();
-  //   await $(page).waitFor('div[title="More Menu"]', { selector: 'css' });
-  //   await $(page).click('div[title="More Menu"]', { selector: 'css' });
-  //   await $(page).waitFor('div[title="Schedule Meeting"]', { selector: 'css' });
-  //   await $(page).click('div[title="Schedule Meeting"]', { selector: 'css' });
-  //   /** The layout of this page should be shown as below
-  //    * 1. Ghost text "Please authorise RingCentral to access your Office account information."
-  //    * 2. Authorize button
-  //   **/
-  //   const buttonText = await $(page).getText('button[class*="widgets-components-MeetingScheduleButton-_styles_button"]', { selector: 'css' });
-  //   expect(buttonText).toEqual(option.buttonText);
-  //   /** Click the Authorize button, Enter Office account and password
-  //    * go through the auth flow
-  //    * check auth panel
-  //    */
-  //   await AuthorizeOffice.login();
-  //   detectAuthPanel(true);
-  //   /** Click logout and relogin
-  //    *  User still Authorized
-  //    */
-  //   await $(page).waitFor('[class*="SettingsPanel-_styles_logoutIcon"]', { selector: 'css' });
-  //   await $(page).click('[class*="SettingsPanel-_styles_logoutIcon"]', { selector: 'css' });
-  //   Login.login();
-  //   detectAuthPanel(true);
-  //   /** Click the Unauthorise button
-  //    * User should be able to view below authorized message
-  //    * Office 365 icon
-  //    * Authorise button
-  //    * Button should be highlight and clickable
-  //    */
-  //   await $(page).click('button[class*="AuthorizeSettingsPanel"]', { selector: 'css' });
-  //   detectAuthPanel(false);
-  // });
-  test({
+  test.skip({
+    title: 'Go to Schedule Meeting',
+    tags: [
+      ['office', { brands: ['rc'] }],
+    ],
+    brands: ['rc'],
+    levels: ['p0'],
+    options: [
+      {
+        buttonText: 'Authorize',
+        actionPrompt: 'Please authorize RingCentral to access your Google account information.',
+        scheduleButton: 'Invite with Outlook Calendar',
+      },
+    ],
+  }, async ({ option }) => {
+    // Login CTI
+    const params = context.options.config;
+    const process = createProcess(
+      Entry,
+      Login,
+    )(context);
+    await process.exec();
+    await $(page).waitFor('div[title="More Menu"]', { selector: 'css' });
+    await $(page).click('div[title="More Menu"]', { selector: 'css' });
+    await $(page).waitFor('div[title="Schedule Meeting"]', { selector: 'css' });
+    await $(page).click('div[title="Schedule Meeting"]', { selector: 'css' });
+    /** The layout of this page should be shown as below
+     * 1. Ghost text "Please authorise RingCentral to access your Office account information."
+     * 2. Authorize button
+    **/
+    const buttonText = await $(page).getText('button[class*="widgets-components-MeetingScheduleButton-_styles_button"]', { selector: 'css' });
+    expect(buttonText).toEqual(option.buttonText);
+    const actionPrompt = await $(page).getText("div[class*='styles_actionPrompt']", { selector: 'css' });
+    expect(actionPrompt).toEqual(option.actionPrompt);
+    /** Click the Authorize button, Enter Office account and password
+     * go through the auth flow
+     * check auth panel
+     */
+    await $(page).click("button[class*='widgets-components-MeetingScheduleButton-_styles_button']",{ selector: 'css' });
+    await goThrough3rdPartyLogin();
+    /** New meeting should be displayed */
+    await $(page).waitFor(2000);
+    const inviteText = await $(page).getText("div[class*='styles_inviteBox'] > button", { selector: 'css' });
+    expect(inviteText).toEqual(option.scheduleButton);
+    /** Go to setting page
+     *  */
+    await detectAuthPanel(true, option);
+    /** Click logout and relogin
+     *  User still Authorized
+     */
+    await $(page).waitFor('[class*="loginNumber"]', { selector: 'css' });
+    await $(page).click('[class*="loginNumber"]', { selector: 'css' });
+    Login.login();
+    await detectAuthPanel(true, option);
+    /** Click the Unauthorise button
+     * User should be able to view below authorized message
+     * Office 365 icon
+     * Authorise button
+     * Button should be highlight and clickable
+     */
+    await $(page).click('button[class*="AuthorizeSettingsPanel"]', { selector: 'css' });
+    await detectAuthPanel(false, option);
+  });
+  test.skip({
     title: 'Go to Schedule Conference, Click button {buttonText}, app should show alert message, lead user to auth account',
     tags: [
       ['office', { brands: ['rc'] }],
@@ -104,7 +123,7 @@ describe('RCI-330 - Authorize and Unauthorize RingCentral for Office 365', () =>
     levels: ['p0'],
     options: [
       {
-        buttonText: 'Invite with Outlook Calendar',
+        buttonText: 'Invite with Office Calendar',
         alertMessage: 'We noticed you haven\'t authorized RingCentral for Office365 to access your Office365 account. Please click here to authorize'
       },
     ],
@@ -120,47 +139,87 @@ describe('RCI-330 - Authorize and Unauthorize RingCentral for Office 365', () =>
     await $(page).click('div[title="More Menu"]', { selector: 'css' });
     await $(page).waitFor('div[title="Schedule Conference"]', { selector: 'css' });
     await $(page).click('div[title="Schedule Conference"]', { selector: 'css' });
-    await $(page).waitFor('div[class*="styles_bottom"] div[class*="ringcentral-widgets-components-Button"]', { selector: 'css' });
-    await $(page).click('div[class*="styles_bottom"] div[class*="ringcentral-widgets-components-Button"]', { selector: 'css' });
+    await $(page).waitFor('div[class*="styles_bottom"]>div[class*="ringcentral-widgets-components-Button"]', { selector: 'css' });
+    await $(page).click('div[class*="styles_bottom"]>div[class*="ringcentral-widgets-components-Button"]', { selector: 'css' });
+    await $(page).click('div[class*="styles_bottom"]>div[class*="ringcentral-widgets-components-Button"]', { selector: 'css' });
+    await $(page).click("a[class*='styles_clickHere']", { selector: 'css' });
+    const targets = await browser.targets();
+    const officeAuthPage = await targets[targets.length - 1].page();
+    await goThrough3rdPartyLogin(officeAuthPage);
     const alertMessage = await $(page).getText('div[class*="ringcentral-widgets-components-AlertDisplay"]', { selector: 'css' });
-    expect(alertMessage).toEqual(expect.stringContaining(option.alertMessage));
-    /** Click the Authorize button **/
-    await AuthorizeOffice.login();
-    detectAuthPanel(true);
+    /** Click the the `click here` to auth on alert message**/
+    await detectAuthPanel(true, option);
     /** Click logout and relogin, user still Authorized **/
-    await $(page).waitFor('[class*="SettingsPanel-_styles_logoutIcon"]', { selector: 'css' });
-    await $(page).click('[class*="SettingsPanel-_styles_logoutIcon"]', { selector: 'css' });
-    Login.login();
-    detectAuthPanel(true);
+    await $(page).waitFor('[class*="loginNumber"]', { selector: 'css' });
+    await $(page).click('[class*="loginNumber"]', { selector: 'css' });
+    await Login.login();
+    detectAuthPanel(true, option);
     /** Click the Unauthorise button **/
     await $(page).click('button[class*="AuthorizeSettingsPanel"]', { selector: 'css' });
-    detectAuthPanel(false);
+    detectAuthPanel(false, option);
   });
-  // test({
-  //   title: 'O365 authorization flow(click authorization on setting panel)',
-  //   tags: [
-  //     ['office', { brands: ['rc'] }],
-  //   ],
-  //   brands: ['rc'],
-  //   levels: ['p0'],
-  //   options: [
-  //     {
-  //       authSuccess: 'Authorized Account',
-  //     },
-  //   ],
-  // }, async ({ option }) => {
-  //   // Login CTI
-  //   const params = context.options.config;
-  //   const process = createProcess(
-  //     Entry,
-  //     Login,
-  //     AuthorizeOffice,
-  //   )(context);
-  //   await process.exec();
-  //   // Authroize Office365
-  //   const text = await $(page).getText("[class*='AuthorizeSettingsPanel-_styles_title']",
-  //     { selector: 'css' }
-  //   );
-  //   expect(text).toEqual(option.authSuccess);
-  // });
+  test({
+    title: 'O365 authorization flow(click authorization on setting panel)',
+    tags: [
+      ['office', { brands: ['rc'] }],
+    ],
+    brands: ['rc'],
+    levels: ['p0'],
+    options: [
+      {
+        authSuccess: 'Authorized Account',
+      },
+    ],
+  }, async ({ option }) => {
+    // Login CTI
+    const params = context.options.config;
+    const process = createProcess(
+      Entry,
+      Login,
+      AuthorizeOffice
+    )(context);
+    // Authroize Office365
+    await process.exec();
+    await detectAuthPanel(true, option);
+    /** Click logout and relogin, user still Authorized **/
+    await $(page).waitFor('[class*="loginNumber"]', { selector: 'css' });
+    await $(page).click('[class*="loginNumber"]', { selector: 'css' });
+    Login.login();
+    await detectAuthPanel(true, option);
+    /** Click the Unauthorise button **/
+    await $(page).click('button[class*="AuthorizeSettingsPanel"]', { selector: 'css' });
+    await detectAuthPanel(false, option);
+  });
+  // for test
+  test.skip({
+    title: '123',
+    tags: [
+      ['office', { brands: ['rc'] }],
+    ],
+    brands: ['rc'],
+    levels: ['p0'],
+    options: [
+      {
+        authSuccess: 'Authorized Account',
+      },
+    ],
+  }, async ({ option }) => {
+    // Login CTI
+    const process = createProcess(
+      Entry,
+      Login,
+    )(context);
+    // Authroize Office365
+    await process.exec();
+    // await $(page).waitFor(2000);
+    await $(page).waitFor('div[title="More Menu"]', { selector: 'css' });
+    await $(page).click('div[title="More Menu"]', { selector: 'css' });
+    await $(page).waitFor('div[title="Settings"]', { selector: 'css' });
+    await $(page).click('div[title="Settings"]', { selector: 'css' });
+    /** Click logout and relogin, user still Authorized **/
+    await $(page).waitFor('[class*="loginNumber"]', { selector: 'css' });
+    await $(page).click('[class*="loginNumber"]', { selector: 'css' });
+    Login.login();
+    await detectAuthPanel(true, option);
+  });
 });
