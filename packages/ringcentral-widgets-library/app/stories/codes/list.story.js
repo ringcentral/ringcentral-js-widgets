ea3a173851a58fb47583b3ce080b9d5d2e2f59f9
jackzong <jinzhanglin@hotmail.com>
2018-10-24 13:45:38 +0800

    update

--- packages/ringcentral-widgets-library/app/stories/codes/list.story.js
+++ packages/ringcentral-widgets-library/app/stories/codes/list.story.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { storiesOf, addDecorator } from '@storybook/react';
-// import { withBackgrounds } from '@storybook/addon-backgrounds';
+import backgrounds, { withBackgrounds } from '@storybook/addon-backgrounds';
 import List from '../../components/List';
 
 storiesOf('List', module)
-  // .addDecorator(withBackgrounds([
-  //   { name: 'twitter', value: '#000', default: true }
-  // ]))
+  .addDecorator(withBackgrounds([
+    { name: 'twitter', value: '#000', default: true }
+  ]))
   .add('basic', () => <div style={{ width: '300px' }}><List /></div>);
