a28342a4020a8609b3b1e47cb0c130bbfe2927c0
jackzong <jinzhanglin@hotmail.com>
2018-10-24 13:05:00 +0800

    update

---
+++ packages/ringcentral-e2e-test/loginInfo.js
@@ -0,0 +1,7 @@
+module.exports = {
+  username: '18332032011',
+  password: 'Test!123',
+  officeAccout: 'demo2@RingCentral639.onmicrosoft.com',
+  officePwd: 'Rcisfun27',
+  extension: '../../../googlechrome/build/extension/office/rc'
+};
