5fc7c6dd64f8b5a3d3648d3e7ea83e13e6e2d16b
Lex Huang <531971909@qq.com>
2018-10-26 17:04:06 +0800

    fix(hasActiveCalls.js): fix bool logic (#1431)
    * fix(hasActiveCalls.js): fix bool logic
    
    Since the  callLogSection.showNotification is slower than the webRTC signals, tht tab would
    disappear and reappear and disappear again.
    
    https://jira.ringcentral.com/browse/RCINT-9003
    
    * refactor(hasActiveCalls.js): add callMonitor
    
    * refactor(hasActiveCalls): add calling mode criteria

--- packages/ringcentral-widgets/lib/hasActiveCalls.js
+++ packages/ringcentral-widgets/lib/hasActiveCalls.js
@@ -6,14 +6,16 @@ export default function hasActiveCalls(phone) {
     webphone,
     callLogSection,
     callMonitorUI,
+    callingSettings,
   } = phone;
+  if (webphone && callingSettings.callingMode === callingModes.webphone) {
+    // need to show the ringout calls
+    return !!(webphone.sessions.length) || (callMonitor && callMonitor.calls.length);
+  }
   return !!(
-    // (callMonitor && callMonitor.calls.length) &&
-    (webphone && webphone.sessions.length)
-  ) || !!(
     (callMonitor && callMonitor.calls.length)
-    || (callLogSection && callLogSection.showNotification)
-    // || (callLogSection && callLogSection.show)
-    || (callMonitorUI && callMonitorUI.cachedActive)
+      || (callLogSection && callLogSection.showNotification)
+      // || (callLogSection && callLogSection.show)
+      || (callMonitorUI && callMonitorUI.cachedActive)
   );
 }
