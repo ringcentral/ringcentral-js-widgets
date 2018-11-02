7aafea520470e095512ae44b3a9173d7f90ea596
aermin <aermin@foxmail.com>
2018-11-02 10:43:06 +0800

    refactor(ActiveCallsPanel && ConfirmMergeModal && ContactDetails): update zh-HK files

--- packages/ringcentral-widgets/components/ContactDetails/i18n/zh-HK.js
+++ packages/ringcentral-widgets/components/ContactDetails/i18n/zh-HK.js
@@ -3,14 +3,14 @@ import dndStatus from 'ringcentral-integration/modules/Presence/dndStatus';
 import phoneTypes from '../../../enums/phoneTypes';
 
 export default {
-  emailLabel: '電子郵件',
-  call: '通話',
-  text: '簡訊',
-  [presenceStatus.available]: '可用',
-  [presenceStatus.offline]: '隱形',
-  [presenceStatus.busy]: '忙碌',
-  [dndStatus.doNotAcceptAnyCalls]: '勿打擾',
-  notActivated: '非使用中'
+  emailLabel: "電子郵件",
+  call: "通話",
+  text: "簡訊",
+  [presenceStatus.available]: "可用",
+  [presenceStatus.offline]: "隱形",
+  [presenceStatus.busy]: "忙碌",
+  [dndStatus.doNotAcceptAnyCalls]: "勿打擾",
+  notActivated: "非使用中"
 };
 
 // @key: @#@"emailLabel"@#@ @source: @#@"Email"@#@
