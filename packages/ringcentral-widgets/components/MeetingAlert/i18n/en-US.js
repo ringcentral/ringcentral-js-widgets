ed75292657f1209cf69b14f1be4bb3863cebed2b
Rulee Chen <rulee@live.cn>
2018-11-08 16:34:43 +0800

    fix(Meeting): show alert message when app doesn't have meeting permission (#1495)

--- packages/ringcentral-widgets/components/MeetingAlert/i18n/en-US.js
+++ packages/ringcentral-widgets/components/MeetingAlert/i18n/en-US.js
@@ -3,5 +3,6 @@ import meetingStatus from 'ringcentral-integration/modules/Meeting/meetingStatus
 export default {
   [meetingStatus.emptyTopic]: 'Please enter meeting topic.',
   [meetingStatus.noPassword]: 'Please provide meeting password.',
+  [meetingStatus.insufficientPermissions]: '{application} do not have {permissionName} permission.',
   [meetingStatus.scheduledSuccess]: 'Meeting is scheduled.',
 };
