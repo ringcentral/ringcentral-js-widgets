abdeeba183f2f1be222933f50f89b27db0c3db63
jackzong <jinzhanglin@hotmail.com>
2018-11-12 14:30:14 +0800

    update

--- packages/ringcentral-widgets-library/app/components/List/index.js
+++ packages/ringcentral-widgets-library/app/components/List/index.js
@@ -3,13 +3,14 @@ import PropsTypes from 'prop-types';
 import LinkLine from '../LinkLine';
 import Switch from '../Switch';
 import Line from '../Line';
+import IconLine from '../IconLine';
 
-function List({ type }) {
+function List({ type, switchProps }) {
   console.log(type);
   if (type === 'L01') {
     return (<LinkLine>Label</LinkLine>);
   } else if (type === 'L02') {
-    return (<Line icon={Switch}>Label</Line>);
+    return (<IconLine icon={<Switch {...switchProps} />}>Label</IconLine>);
   }
 }
 
--- packages/ringcentral-widgets-library/app/stories/codes/list.story.js
+++ packages/ringcentral-widgets-library/app/stories/codes/list.story.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { storiesOf, addDecorator } from '@storybook/react';
 import withBackgrounds from '@storybook/addon-backgrounds';
 import List from '../../components/List';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('List', module)
   .addDecorator(withBackgrounds([
     { name: 'listBackground', value: '#f9f9f9', default: true }
   ]))
-  .add('basic', () => <div style={{ width: '300px' }}><List type="L01" /><List type="L02" /></div>);
+  .add('basic', () => {
+    const onChange = action('onChange');
+    const switchProps = {
+      title: text('Title', 'Switch Title'),
+      checked: boolean('Checked', false),
+      onChange,
+      disable: boolean('Disabled', false),
+    };
+    return (<div style={{ width: '300px' }}>
+      <List type="L01" />
+      <List type="L02" switchProps={switchProps} />
+    </div>);
+  }
+  );
