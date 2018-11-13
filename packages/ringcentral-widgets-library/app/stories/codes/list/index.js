345986368af8d18ff240752fe26ab3cb8059fe9b
MicleMing <fzulanming@gmail.com>
2018-11-13 17:36:32 +0800

    feat: call item component

---
+++ packages/ringcentral-widgets-library/app/stories/codes/list/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, boolean, number } from '@storybook/addon-knobs/react';
+import { action } from '@storybook/addon-actions';
+import CallItem from '../../../components/List/CallItem';
+
+storiesOf('List', module)
+  .add('callItem', () => {
+    const props = {
+      avatarUrl: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1542096539&di=1b15909917afacfde67df9f1ec893d2c&src=http://b-ssl.duitang.com/uploads/item/201501/25/20150125100619_waZTn.jpeg',
+      userName: text('userName', 'kyle'),
+      isOnline: boolean('isOnline', false),
+      isConference: boolean('isConference', false),
+      converenceNumber: number('converenceNumber', 1),
+      showEndCall: boolean('showEndCall', true),
+      onEndCall: action('onEndCall clicked'),
+      showAnswerCall: boolean('showAnswerCall', true),
+      onAnswerCall: action('onAnswerCall clicked'),
+      showExtraInfo: boolean('showExtraInfo', true),
+      extraInfo: text('extraInfo', 'hello'),
+    };
+    return <CallItem {...props} />;
+  });
