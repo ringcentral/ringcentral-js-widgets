7a02979c204e21141d29613692a1eb97973d0580
MingMing Lan <fzuming@qq.com>
2018-11-14 14:28:09 +0800

    feat: online status in avatar (#1526)

--- packages/ringcentral-widgets-library/app/stories/codes/avatar/index.js
+++ packages/ringcentral-widgets-library/app/stories/codes/avatar/index.js
@@ -10,6 +10,7 @@ storiesOf('Avatar', module)
     const props = {
       avatarUrl: text('avatarUrl', 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1542096539&di=1b15909917afacfde67df9f1ec893d2c&src=http://b-ssl.duitang.com/uploads/item/201501/25/20150125100619_waZTn.jpeg'),
       isOnConferenceCall: boolean('isOnConferenceCall', true),
+      isOnline: boolean('isOnline', false),
       extraNum: number('extraNum', 2),
       onClick: action('clicked'),
       spinnerMode: boolean('spinnerMode', false)
