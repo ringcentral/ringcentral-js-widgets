a51eddebb897f76676211c9bb9e73592b76390fc
Grace He <grace.he@lmxmn246.rcoffice.ringcentral.com>
2018-11-16 14:59:01 +0800

    fix RCI1136 checkpoint

---
+++ packages/ringcentral-e2e-test/src/features/Office365/RCI-1136_New_Event_page_with_Meeting_injection.spec.js
@@ -0,0 +1,133 @@
+/* eslint-disable */ 
+/* RCI-1136: New Event page with Meeting injection
+http://einstein.int.ringcentral.com/?project=1309&suite=11037&case=17244
+
+Summary:
+  Goal: Checking New Event page injection
+
+  Priority: P1
+
+  User Story: 
+    RCINT-8582: New Event Page with RC Meeting Injection
+    https://jira.ringcentral.com/browse/RCINT-8582
+  
+  Keywords:
+    Automation Ready, Functionality, Office365
+
+  Preconditions:
+    1. RC CTI app is installed and enabled
+    2. User has logged in to 3rd party
+    3. User has logged in to  CTI app
+    4. User has authorized CTI
+    
+    Account type(/s): RC US, CA, UK, EU, AT&T, TELUS, BT
+    Extension type(/s): 
+    Entry point(/s):  1. In Calendar, Time Format is 24 hours
+                      2. In Calendar, Time Format is 12 hours
+                      Setting in :https://portal.office.com//account/#settings
+
+Created By		cerrie.shen
+Date Created	Tue, 06 Nov 2018 10:26:36
+Updated By	cerrie.shen
+Date Updated	Tue, 06 Nov 2018 14:47:44
+ */
+ /* eslint-enable */
+
+ import { createProcess } from 'marten';
+ import { LoginCTI } from '../../steps/commons/login';
+ import AuthorizeOffice from '../../steps/office/__temp__/authorizeOffice';
+ import Entry from '../../steps/entry';
+ import addMeeting from '../../steps/office/__temp__/addMeeting';
+  
+ beforeEach(() => {
+   jasmine.DEFAULT_TIMEOUT_INTERVAL = 1200000;
+ });
+ 
+  describe('Office365 : New Event page =====>', () => {
+    test({
+      title: 'New Event page with Meeting injection',
+      tags: [
+        ['office'],
+      ],
+      levels: ['p1'],
+      //brands: ['rc', 'bt', 'att', 'telus'],
+      brands: [
+        ['rc', { accounts: ['CM_RC_US'] }],
+        ['bt', { accounts: ['CM_BT'] }],
+        ['att', { username:'18665133464',password:'Test!123' }],
+        ['telus', { accounts: ['CM_TELUS'] }],
+      ],
+      //accounts: ['CM_RC_US'],
+      options: [
+       {         
+         usrname: '18665133464' ,
+         password: 'Test!123',
+         o365URL: 'https://outlook.office365.com/owa/?path=/calendar/view/Day',
+         authSuccess: 'Authorized Account',
+         appNames: {
+           rc: 'RingCentral for Office365',
+           att: 'Office@Hand for Office365',
+           bt: 'BT Cloud Phone for Office365',
+           telus: 'TELUS Business Connect for Office365',
+         },
+       }
+      ],
+    }, async (context) => {
+      const process = createProcess(
+        Entry,
+        LoginCTI,
+        //skipGuide,
+        AuthorizeOffice,
+        addMeeting,
+        
+      )(context);
+      /*
+      __Step1__: Go to Outlook->Calendar->click Time slot ->more detail, and user is on New Even page
+ 
+      [Expected Result]: 
+      There is a selector:
+      <brand icon>+ Select Conference with more drop down
+      <brand icon>+ Video Meeting
+      <brand icon>+ Conference Meeting
+      None
+      */
+   
+      await process.execTo(addMeeting.openDetail);
+      expect(await addMeeting.getSelePlaceholder(context)).toEqual('Select Conference');
+      expect(await addMeeting.getSeleItems(context)).toEqual(['Video Meeting','Conference Meeting']);
+ 
+      
+      /*
+      __Step2__: Select "Video Meeting"option
+      [Expected Result]:
+      Pop up a meeting setting page
+      */
+     
+      /*
+      __Step3__: Closed the meeting setting popup and then click "Schedule" button
+      [Expected Result]:
+      Inject meeting info to "Description", and meeting link to "Location" by default meeting setting
+      */
+     await process.execTo(addMeeting.createVideoMeeting);
+     //const locationRes = await addMeeting.checkVideoLocaExist(context);
+     
+     expect(await addMeeting.checkVideoLocaExist(context)).toBe(true);
+     expect(await addMeeting.checkVideoDescExist(context)).toBe(true);
+ 
+      /*
+      __Step4__: Select "Conference Meeting"
+      [Expected Result]: Pop up a conference setting page
+      */
+ 
+      /*
+      __Step5__: Closed the conference setting popup and then click "Schedule" button
+      [Expected Result]: 
+      1. Meeting info will replace, the Video meeting info will be cleared;
+      2. Inject conference info to "Description", and text "Conference Meeting" to "Location"
+      */
+     //await process.execTo(addMeeting.createConfMeeting);
+     //expect(addMeeting.checkConfLocaExist(context).toBeTruthy();
+     //expect(addMeeting.checkConfDescExist(context).toBeTruthy();
+    });
+  });
+  
---
+++ packages/ringcentral-e2e-test/src/steps/office/__temp__/addMeeting.js
@@ -0,0 +1,162 @@
+import sleep from 'ringcentral-integration/lib/sleep';
+
+const video_zoom_reg = {
+  'rc': /(http|https):\/\/(rcdev\.zoom\.us)(\/\w+)?(\/\d+)/,
+  'bt': /(http|https):\/\/(rcdev\.zoom\.us)(\/\w+)?(\/\d+)/,
+  'att': /(http|https):\/\/(attdev\.zoom\.us)(\/\w+)?(\/\d+)/,
+  'telus': /(http|https):\/\/(rcdev\.zoom\.us)(\/\w+)?(\/\d+)/
+}
+const video_meeting_reg = {
+  'rc': /(http|https):\/\/meetings\.ringcentral\.com(\/\w+)?/,
+  'bt': /(http|https):\/\/meetings.btcloudphone\.bt\.com(\/\w+)?/,
+  'att': /(http|https):\/\/meetings-officeathand\.att\.com(\/\w+)?/,
+  'telus': /(http|https):\/\/meetings.businessconnect\.telus\.com(\/\w+)?/
+}
+const conf_zoom_reg = /^(http|https):\/\/rcconf.net\/\w+$/;
+const conf_meeting_reg = /^(http|https):\/\/rcconf.net\/\w+$/;
+
+
+
+/* global $ */
+export default class addMeeting{
+  static async openDetail(context) {    
+    console.log('Opening o365 specific page.');
+    const page = context.page;
+    await $(page).goto(context.options.option.o365URL, { // This page could be slower than you think
+      waitUntil: 'networkidle2',
+      timeout: 600000,
+    });
+    await $(page).click("button[autoid='_fce_1']");
+  }
+
+  static async createVideoMeeting(context){
+
+    console.log('create video meeting');
+    const page = context.page;
+    await $(page).click("@selectedItem");  
+    //select video meeting
+    await $(page).click("[value=meeting]");
+    //savebtn
+    console.log('save schedule');
+    await $(page).waitForSelector("#rc-meeting-schedule", {
+      visible: true,
+      timeout:30*1000
+    });
+    await $(page).click("#rc-meeting-schedule");
+  }
+
+  static async createConfMeeting(context){
+    console.log('create conference meeting');
+    const page = context.page;
+    await $(page).click("@selectedItem");  
+    //select video meeting
+    await $(page).click("[value=conference]");
+    //savebtn
+    console.log('save schedule');
+    await $(page).waitForSelector("#rc-meeting-schedule", {
+      visible: true,
+      timeout:30*1000
+    });
+    await $(page).click("#rc-meeting-schedule");
+  }
+
+  static async checkVideoDescExist(context){
+    console.log("=========check video description===========");
+    const page = context.page;
+    await sleep(3000);
+    const text = await page.evaluate(()=>{
+      var a = document.querySelector('[contenteditable="true"][aria-multiline="true"][role="textbox"]');
+      return a.innerHTML;
+    });
+    const brand = context.options.tag.brands;
+    const link_com = video_zoom_reg[brand].test(text);
+    const link_spe = video_meeting_reg[brand].test(text);  
+    debugger;
+    return link_com&&link_spe;
+  }
+
+  static async checkConfDescExist(context){
+    console.log("=========check conference description===========");
+    const page = context.page;
+    await sleep(3000);
+    const text = await page.evaluate(()=>{
+      var a = document.querySelector('[contenteditable="true"][aria-multiline="true"][role="textbox"]');
+      return a.innerHTML;
+    });
+    const brand = context.options.tag.brands;
+    const link_com = conf_zoom_reg[brand].test(text);
+    const link_spe = conf_meeting_reg[brand].test(text);  
+    return link_com&&link_spe;
+  }
+
+  static async checkVideoLocaExist(context){
+    console.log('==========check video location==========');
+    const page = context.page;
+
+    // close the native dialogs
+    page.on('dialog',async dialog => {
+      dialog.dismiss()
+    });
+    await sleep(3000);
+
+    const text = await page.evaluate(()=>{
+      var a = document.getElementById('MeetingCompose.LocationInputLabel');
+      return a && a.parentElement.innerHTML;
+    });
+    const brand = context.options.tag.brands;
+    const result = video_zoom_reg[brand].test(text);
+    debugger;
+    return result;
+  }
+
+  static async checkConfLocaExist(context){
+    console.log('==========check conference location==========');
+    const page = context.page;
+
+    // close the native dialogs
+    page.on('dialog',async dialog => {
+      dialog.dismiss()
+    });
+    await sleep(3000);
+
+    const text = await page.evaluate(()=>{
+      var a = document.getElementById('MeetingCompose.LocationInputLabel');
+      return a && a.parentElement.innerHTML;
+    });
+    const brand = context.options.tag.brands;
+    const result = conf_zoom_reg[brand].test(text);
+    return result;
+  }
+  static async getSeleItems(context){
+    console.log("=======get selector items=========");
+    const page = context.page;    
+    await $(page).click("@selectedItem");     
+    //get select iem
+    const results = await page.evaluate(()=>{
+      let seleItems = Array.prototype.slice.call(document.querySelectorAll("span[class*='src-lib-MeetingHelper-MeetingSelector-_styles_menuItem']"));
+      return seleItems.map( seleItems => { return seleItems.innerHTML });
+    });
+    
+    console.info('=======selector items ======',results);
+    //it's a good idea to collapse drop down for don't affect another steps
+    await $(page).click("@selectedItem"); 
+    return results;
+  }
+
+  static async getSelePlaceholder(context){
+    console.log("=======get selector placeholder=========");
+    const page = context.page;
+    await $(page).waitForSelector("@selectedItem");
+    const text = await $(page).getAttribute("@selectedItem","placeholder");
+    console.info("===placehoder text===",text);
+    return text;
+  }
+
+  static get steps() {
+    return [
+      this.openDetail,
+      this.createVideoMeeting,
+      this.createConfMeeting
+    ];
+  }
+}
