73ca57abf5aff381dfb3208274c66076c09ada34
MingMing Lan <fzuming@qq.com>
2018-12-12 17:45:03 +0800

    feat: button technical spec (#1574)

---
+++ packages/ringcentral-widgets-library/app/elements/Button/enum.js
@@ -0,0 +1,20 @@
+export const BUTTON_TYPE = {
+  primay: 'primay',
+  warning: 'warning',
+  outline: 'outline',
+  danger: 'danger',
+  default: 'default'
+};
+
+export const BUTTON_SHAP = {
+  round: 'round',
+  circle: 'circle',
+  rectangle: 'rectangle',
+  default: 'default'
+};
+
+export const BUTTON_ICON = {
+  download: 'download',
+  plus: 'plus',
+  checked: 'checked'
+};
--- packages/ringcentral-widgets-library/app/elements/Button/index.js
+++ packages/ringcentral-widgets-library/app/elements/Button/index.js
@@ -3,27 +3,68 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import styles from './styles.scss';
 
+import Icon from '../Icon';
+
+import {
+  BUTTON_SHAP,
+  BUTTON_TYPE,
+  BUTTON_ICON
+} from './enum';
+
+const shapStyleMap = {
+  [BUTTON_SHAP.circle]: styles.circle,
+  [BUTTON_SHAP.round]: styles.round,
+  [BUTTON_SHAP.rectangle]: styles.rectangle,
+};
+
+const typeStyleMap = {
+  [BUTTON_TYPE.primay]: styles.primay,
+  [BUTTON_TYPE.warning]: styles.warning,
+  [BUTTON_TYPE.outline]: styles.outline,
+  [BUTTON_TYPE.danger]: styles.danger,
+};
+
+const IconMap = {
+  // [BUTTON_ICON.download]: // <Icon type='download'></Icon>,
+  // [BUTTON_ICON.plus]: // Icon.Plus,
+  // [BUTTON_ICON.checked]: // Icon.Checked
+};
+
 export default function Button({
+  loading,
+  shape,
+  type,
+  icon,
   className,
   disabled,
   onClick,
   children,
   tooltip,
 }) {
+  const classNames = classnames(
+    styles.button,
+    shapStyleMap[shape],
+    typeStyleMap[type],
+    className,
+    disabled && styles.disabled
+  );
   return (
-    <div
-      className={classnames(
-        className,
-        styles.root,
-        disabled && styles.disabled,
-      )}
+    <button
+      className={classNames}
       onClick={disabled ? null : onClick}
       title={tooltip}>
       {children}
-    </div>
+    </button>
   );
 }
 Button.propTypes = {
+  loading: PropTypes.bool,
+  // ['round', 'circle', 'rectangle', 'default']
+  shape: PropTypes.string,
+  // ['primary', 'warning', 'outline', 'danger', 'default']
+  type: PropTypes.string,
+  // ['download', 'plus', 'checked']
+  icon: PropTypes.string,
   className: PropTypes.string,
   tooltip: PropTypes.string,
   disabled: PropTypes.bool,
@@ -32,6 +73,10 @@ Button.propTypes = {
 };
 
 Button.defaultProps = {
+  loading: false,
+  shape: 'default',
+  type: 'default',
+  icon: '',
   className: undefined,
   tooltip: '',
   disabled: false,
