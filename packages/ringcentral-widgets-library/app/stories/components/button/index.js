48d861c7014d95fd7bd86bcd53593f9a1a2792bc
MicleMing <fzulanming@gmail.com>
2018-12-12 17:43:52 +0800

    feat: button technical spec

--- packages/ringcentral-widgets-library/app/stories/components/button/index.js
+++ packages/ringcentral-widgets-library/app/stories/components/button/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+import { text, boolean, number, select } from '@storybook/addon-knobs/react';
 import { linkTo } from '@storybook/addon-links'
 
 import Button from '../../../elements/Button';
@@ -15,8 +15,24 @@ storiesOf('Elements/Button', module)
   .add('with dynamic', () => {
     const children = text('children', 'dynamic');
     const tooltip = text('tooltip', 'hello');
+    const label1 = 'types';
+    const options = [
+      'outline',
+      'warning',
+    ];
+    const type = select(label1, options, 'outline');
+
+    const label2 = 'shaps';
+    const shapOptions = [
+      'default',
+      'circle'
+    ];
+    const shape = select(label2, shapOptions, 'circle');
+
     return (
       <Button
+        type={type}
+        shape={shape}
         tooltip={tooltip}
         onClick={linkTo('Components/List', 'callItem')}
       >
