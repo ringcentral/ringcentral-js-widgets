648b27ef91528414401a243dfba6c63cc5c2a493
JackZong <jinzhanglin@hotmail.com>
2018-12-14 10:52:19 +0800

    feat(button): add button design from jupiter (#1575)

--- packages/ringcentral-widgets-library/app/.storybook/webpack.config.js
+++ packages/ringcentral-widgets-library/app/.storybook/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = (baseConf, type) => {
       test: /\.scss$/,
       loaders: [
         "style-loader",
-        "css-loader?modules&localIdentName=[path]_[name]_[local]_[hash:base64:5]",
+        "css-loader?modules&localIdentName=[local]",
         "sass-loader"],
       include: path.resolve(__dirname, "../")
     },
--- packages/ringcentral-widgets-library/app/stories/components/button/index.js
+++ packages/ringcentral-widgets-library/app/stories/components/button/index.js
@@ -6,35 +6,41 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { text, boolean, number, select } from '@storybook/addon-knobs/react';
-import { linkTo } from '@storybook/addon-links'
+import { linkTo } from '@storybook/addon-links';
 
 import Button from '../../../elements/Button';
 
 storiesOf('Elements/Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>RC Button</Button>)
   .add('with dynamic', () => {
-    const children = text('children', 'dynamic');
+    const children = text('children', 'Button');
     const tooltip = text('tooltip', 'hello');
     const label1 = 'types';
     const options = [
       'outline',
       'warning',
+      'primary',
+      'default',
+      'danger',
     ];
-    const type = select(label1, options, 'outline');
+    const type = select(label1, options, 'primary');
 
     const label2 = 'shaps';
     const shapOptions = [
       'default',
       'circle'
     ];
-    const shape = select(label2, shapOptions, 'circle');
+    const shape = select(label2, shapOptions, 'default');
+    const label3 = 'disabled';
+    const disabled = boolean(label3, false);
 
     return (
       <Button
         type={type}
         shape={shape}
         tooltip={tooltip}
-        onClick={linkTo('Components/List', 'callItem')}
+        onClick={() => {}}
+        disabled={disabled}
       >
         {children}
       </Button>
