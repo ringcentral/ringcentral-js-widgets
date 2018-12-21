2d520f4305455b29982d7e5364bb0ad59ead9193
RingCentral Integrations Team <integrations@ringcentral.com>
2018-12-21 09:03:13 +0000

    released at Fri Dec 21 09:03:13 UTC 2018, commit: 68c6432d800e8bcaf660f233f15ff86335f82304

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.isSameLocalNumber = exports.isValidNumber = exports.formatTypes = exports.parse = exports.detect = exports.format = undefined;
+exports.parseIncompletePhoneNumber = exports.isSameLocalNumber = exports.isValidNumber = exports.formatTypes = exports.isE164 = exports.parse = exports.detect = exports.format = undefined;
 
 var _libphonenumberJs = require('libphonenumber-js');
 
@@ -19,6 +19,10 @@ var _parse = require('./lib/parse');
 
 var _parse2 = _interopRequireDefault(_parse);
 
+var _isE = require('./lib/isE164');
+
+var _isE2 = _interopRequireDefault(_isE);
+
 var _isSameLocalNumber = require('./lib/isSameLocalNumber');
 
 var _isSameLocalNumber2 = _interopRequireDefault(_isSameLocalNumber);
@@ -28,7 +32,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 exports.format = _format2.default;
 exports.detect = _detect2.default;
 exports.parse = _parse2.default;
+exports.isE164 = _isE2.default;
 exports.formatTypes = _format.formatTypes;
 exports.isValidNumber = _libphonenumberJs.isValidNumber;
 exports.isSameLocalNumber = _isSameLocalNumber2.default;
+exports.parseIncompletePhoneNumber = _libphonenumberJs.parseIncompletePhoneNumber;
 //# sourceMappingURL=index.js.map
