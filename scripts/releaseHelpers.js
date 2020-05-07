d7b5095e6f5d3e26edfde9af6bf7925fe9ef4aa3
embbnux <embbnux@embbnux.com>
2020-05-07 17:57:41 +0800

    fix scripts

--- scripts/releaseHelpers.js
+++ scripts/releaseHelpers.js
@@ -20,13 +20,16 @@ export function prepareRelease(packageName, releaseBranchName) {
   }
   if (!fs.existsSync(`release/${packageFolder}`)) {
     console.log('Clone release branch...');
-    execa.shellSync(`git clone "https://$RELEASE_USER:$RELEASE_TOKEN@github.com/$REPO" -b ${releaseBranchName} ${packageFolder} &> /dev/null`, {
-      stdio: 'inherit',
-      cwd: 'release'
-    });
+    execa.shellSync(
+      `git clone "https://$RELEASE_USER:$RELEASE_TOKEN@github.com/$REPO" -b ${releaseBranchName} ${packageFolder} &> /dev/null`,
+      {
+        stdio: 'inherit',
+        cwd: 'release',
+      },
+    );
   }
   console.log('Start release build');
-  execa.shellSync(`lerna run release --scope ${packageName} --stream`, {
+  execa.shellSync(`yarn workspace ${packageName} release`, {
     stdio: 'inherit',
   });
 }
@@ -34,26 +37,35 @@ export function prepareRelease(packageName, releaseBranchName) {
 export function startRelease(packageName, releaseBranchName) {
   const packageFolder = getPackageFolderName(packageName);
   const gitStatus = execa.shellSync('git status -s', {
-    cwd: `release/${packageFolder}`
+    cwd: `release/${packageFolder}`,
   });
   if (gitStatus.stdout === '') {
     return;
   }
   console.log(`start to push ${packageName} release to ${releaseBranchName}`);
-  execa.shellSync('git config user.email "integrations@ringcentral.com" &> /dev/null', {
-    cwd: `release/${packageFolder}`
-  });
-  execa.shellSync('git config user.name "RingCentral Integrations Team" &> /dev/null', {
-    cwd: `release/${packageFolder}`
-  });
+  execa.shellSync(
+    'git config user.email "integrations@ringcentral.com" &> /dev/null',
+    {
+      cwd: `release/${packageFolder}`,
+    },
+  );
+  execa.shellSync(
+    'git config user.name "RingCentral Integrations Team" &> /dev/null',
+    {
+      cwd: `release/${packageFolder}`,
+    },
+  );
   execa.shellSync('git add --all . &> /dev/null', {
-    cwd: `release/${packageFolder}`
+    cwd: `release/${packageFolder}`,
   });
-  execa.shellSync('git commit -m "released at $(date), commit: $TRAVIS_COMMIT" &> /dev/null', {
-    cwd: `release/${packageFolder}`
-  });
+  execa.shellSync(
+    'git commit -m "released at $(date), commit: $TRAVIS_COMMIT" &> /dev/null',
+    {
+      cwd: `release/${packageFolder}`,
+    },
+  );
   execa.shellSync(`git push origin ${releaseBranchName} -f &> /dev/null`, {
-    cwd: `release/${packageFolder}`
+    cwd: `release/${packageFolder}`,
   });
   console.log(`release to ${releaseBranchName} successfully.`);
 }
