c08105daeef75bab7958f8695e883c2f53990df5
embbnux <embbnux@embbnux.com>
2022-11-10 15:14:45 +0000

    Deploying to commons-release from  @ 66c0c9f31c32da1f8bc3ec28d64e90248bea9e3d 🚀

--- modules/Theme/defaultCssVariable.js
+++ modules/Theme/defaultCssVariable.js
@@ -7,82 +7,78 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.defaultCssVariable = void 0;
 
-var _colorManipulator = require("@ringcentral/juno/es6/foundation/styles/colorManipulator.js");
+var _juno = require("@ringcentral/juno");
 
-var _newPalette = require("@ringcentral/juno/es6/foundation/styles/newPalette.js");
-
-var _palette = require("@ringcentral/juno/es6/foundation/styles/palette.js");
-
-var mainColor = (0, _newPalette.palette2)('nav', 'menuBg');
+var mainColor = (0, _juno.palette2)('nav', 'menuBg');
 var defaultCssVariable = {
   rcBlue: mainColor,
   // '#066fac'
-  bigRed: (0, _newPalette.palette2)('danger', 'b03'),
+  bigRed: (0, _juno.palette2)('danger', 'b03'),
   // '#dc0202'
-  darkRed: (0, _newPalette.palette2)('danger', 'b04'),
+  darkRed: (0, _juno.palette2)('danger', 'b04'),
   // '#c90213'
-  tomato: (0, _newPalette.palette2)('avatar', 'tomato'),
+  tomato: (0, _juno.palette2)('avatar', 'tomato'),
   // '#f95b5c'
-  orange: (0, _newPalette.palette2)('nav', 'mention'),
+  orange: (0, _juno.palette2)('nav', 'mention'),
   // '#ff8800'
-  leaf: (0, _newPalette.palette2)('success', 'b03'),
+  leaf: (0, _juno.palette2)('success', 'b03'),
   // '#5fb95c'
-  dark: (0, _newPalette.palette2)('neutral', 'f06'),
+  dark: (0, _juno.palette2)('neutral', 'f06'),
   // '#333'
-  night: (0, _newPalette.palette2)('neutral', 'f05'),
+  night: (0, _juno.palette2)('neutral', 'f05'),
   // '#2f2f2f'
-  ash: (0, _newPalette.palette2)('neutral', 'f04'),
+  ash: (0, _juno.palette2)('neutral', 'f04'),
   // '#666666'
-  coin: (0, _newPalette.palette2)('neutral', 'f03'),
+  coin: (0, _juno.palette2)('neutral', 'f03'),
   // '#999999'
-  smoke: (0, _newPalette.palette2)('neutral', 'f02'),
+  smoke: (0, _juno.palette2)('neutral', 'f02'),
   // '#c7c7c7'
-  silver: (0, _newPalette.palette2)('neutral', 'l02'),
+  silver: (0, _juno.palette2)('neutral', 'l02'),
   // '#e2e2e2'
-  egg: (0, _newPalette.palette2)('neutral', 'b02'),
+  egg: (0, _juno.palette2)('neutral', 'b02'),
   // '#f9f9f9'
-  snow: (0, _newPalette.palette2)('neutral', 'f01'),
+  snow: (0, _juno.palette2)('neutral', 'f01'),
   // '#ffffff'
-  rcOrange: (0, _newPalette.palette2)('label', 'orange01'),
+  rcOrange: (0, _juno.palette2)('label', 'orange01'),
   // '#e07800'
-  darkergray: (0, _newPalette.palette2)('neutral', 'f03'),
+  darkergray: (0, _juno.palette2)('neutral', 'f03'),
   // '#797979'
-  darkgray: (0, _newPalette.palette2)('neutral', 'f03'),
+  darkgray: (0, _juno.palette2)('neutral', 'f03'),
   // '#808080'
-  gray: (0, _newPalette.palette2)('neutral', 'b03'),
+  gray: (0, _juno.palette2)('neutral', 'b03'),
   // '#eeeeee'
-  bggray: (0, _newPalette.palette2)('neutral', 'b03'),
+  bggray: (0, _juno.palette2)('neutral', 'b03'),
   // '#e0e0e0'
-  lightgray: (0, _newPalette.palette2)('neutral', 'b02'),
+  lightgray: (0, _juno.palette2)('neutral', 'b02'),
   // '#fcfcfc'
-  greyLight: (0, _newPalette.palette2)('neutral', 'f04'),
+  greyLight: (0, _juno.palette2)('neutral', 'f04'),
   // '#797979'
-  missed: (0, _newPalette.palette2)('danger', 'b03'),
+  missed: (0, _juno.palette2)('danger', 'b03'),
   // '#d8232a'
-  active: (0, _newPalette.palette2)('success', 'f11'),
+  active: (0, _juno.palette2)('success', 'f11'),
   // '#23b23d'
   primaryColor: mainColor,
   // '#066fac'
-  primaryColorHighlight: (0, _palette.setOpacity)(mainColor, '40'),
+  primaryColorHighlight: (0, _juno.setOpacity)(mainColor, '40'),
   // 'rgba(6, 132, 189, 0.4)'
-  primaryColorHighlightSolid: (0, _colorManipulator.lighten)(mainColor, 0.6),
+  primaryColorHighlightSolid: (0, _juno.lighten)(mainColor, 0.6),
   // 'rgba(173, 210, 227, 1)'
-  linePanelBackgroundColor: (0, _newPalette.palette2)('neutral', 'b02'),
+  linePanelBackgroundColor: (0, _juno.palette2)('neutral', 'b02'),
   // '#f6f6f6'
-  callBtnColor: (0, _newPalette.palette2)('success', 'b03'),
+  callBtnColor: (0, _juno.palette2)('success', 'b03'),
   // '#5fb95c'
-  extensionBackgroundColor: (0, _newPalette.palette2)('success', 'f11'),
+  extensionBackgroundColor: (0, _juno.palette2)('success', 'f11'),
   // '#66d17b'
-  smsBubbleBackgroundColor: (0, _newPalette.palette2)('interactive', 'b01'),
+  smsBubbleBackgroundColor: (0, _juno.palette2)('interactive', 'b01'),
   // '#d5f3fd'
   brandFontColor: mainColor,
   // '#066fac'
-  brandFontColorHighlight: (0, _palette.setOpacity)(mainColor, '40'),
+  brandFontColorHighlight: (0, _juno.setOpacity)(mainColor, '40'),
   // 'rgba(6, 132, 189, 0.4)'
-  jupiterBackgroundColor: (0, _newPalette.palette2)('neutral', 'b02'),
+  jupiterBackgroundColor: (0, _juno.palette2)('neutral', 'b02'),
   // '#f5f6fb'
   addMeetingBtnColor: mainColor,
-  addMeetingBtnTextColor: (0, _newPalette.palette2)('neutral', 'f01'),
+  addMeetingBtnTextColor: (0, _juno.palette2)('neutral', 'f01'),
   // '#ffffff'
   extOptionBtnColor: mainColor
 };
