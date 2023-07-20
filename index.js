60da3a72bf0e51a58db935fce2f71866ce3cc139
embbnux <embbnux@embbnux.com>
2023-07-20 06:18:19 +0000

    Deploying to core-release from  @ 443adc0cbe2eb01b2490f132df4e605835c61552 🚀

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
 var _lib = require("./lib");
-
 Object.keys(_lib).forEach(function (key) {
   if (key === "default" || key === "__esModule") return;
   if (key in exports && exports[key] === _lib[key]) return;
