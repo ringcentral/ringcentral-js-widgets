220774bc87450f790d0b4ab94a728935e6023a18
embbnux <embbnux@embbnux.com>
2026-01-15 09:57:04 +0000

    Deploying to react-hooks-release from  @ 783ba07654eb96d637931ca523dceb16b4493733 🚀

--- src/useViewTransitionState.d.ts
+++ src/useViewTransitionState.d.ts
@@ -2,4 +2,10 @@
  * when browser support view transition, this hook will temp input state and call `document.startViewTransition` before the state change.
  *
  */
-export declare const useViewTransitionState: <T = unknown>(currentState: T, onStartViewTransition?: (currentState: T) => void) => T;
+export declare const useViewTransitionState: <T = unknown>(currentState: T, 
+/**
+ * This callback will be called before the view transition starts.
+ *
+ * which is useful when you want to add some animation before the view transition starts.
+ */
+onStartViewTransition?: (currentState: T) => void) => T;
