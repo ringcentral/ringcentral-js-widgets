0bf06b6ce164ec565d00053e053bc3f8a132a366
Embbnux Ji <embbnux.ji@ringcentral.com>
2026-01-29 14:18:34 +0800

    misc: i18n

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./src"), exports);
+
+require("core-js/modules/es.array.for-each.js");
+require("core-js/modules/es.object.define-property.js");
+require("core-js/modules/es.object.keys.js");
+require("core-js/modules/es.object.to-string.js");
+require("core-js/modules/web.dom-collections.for-each.js");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var _src = require("./src");
+Object.keys(_src).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _src[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _src[key];
+    }
+  });
+});
 //# sourceMappingURL=index.js.map
